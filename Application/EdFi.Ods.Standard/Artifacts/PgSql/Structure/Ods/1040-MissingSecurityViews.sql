fe9682456ee20902b7a337d96a7adbfbf98bcecb
Benjamin Meyers <blmeyers@users.noreply.github.com>
2020-06-05 15:39:54 -0500

    Adding scripts to create missing security views in core.

---
+++ Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/1040-MissingSecurityViews.sql
@@ -0,0 +1,31 @@
+CREATE VIEW auth.CommunityProviderIdToStaffUSI
+AS
+    SELECT emp.EducationOrganizationId AS CommunityProviderId
+          ,emp.StaffUSI
+    FROM edfi.CommunityProvider cp
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp ON
+        cp.CommunityProviderId = emp.EducationOrganizationId
+
+    UNION ALL
+
+    SELECT assgn.EducationOrganizationId AS CommunityProviderId
+          ,assgn.StaffUSI
+    FROM edfi.CommunityProvider cp
+             INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn ON
+        cp.CommunityProviderId = assgn.EducationOrganizationId;
+
+CREATE VIEW auth.PostSecondaryInstitutionIdToStaffUSI
+AS
+    SELECT emp.EducationOrganizationId AS PostSecondaryInstitutionId
+          ,emp.StaffUSI
+    FROM edfi.PostSecondaryInstitution psi
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp ON
+        psi.PostSecondaryInstitutionId = emp.EducationOrganizationId
+
+    UNION ALL
+
+    SELECT assgn.EducationOrganizationId AS PostSecondaryInstitutionId
+          ,assgn.StaffUSI
+    FROM edfi.PostSecondaryInstitution psi
+             INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn ON
+        psi.PostSecondaryInstitutionId = assgn.EducationOrganizationId;
