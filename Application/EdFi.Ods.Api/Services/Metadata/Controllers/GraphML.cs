d10f0ca130affedb76d33c581ad40728deb332f8
Geoff McElhanon <geoffrey@mcelhanon.com>
2020-06-04 21:44:06 -0500

    Formatting changes only.

--- Application/EdFi.Ods.Api/Services/Metadata/Controllers/GraphML.cs
+++ Application/EdFi.Ods.Api/Services/Metadata/Controllers/GraphML.cs
@@ -2,13 +2,12 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
- 
+
 using System.Collections.Generic;
 using System.Linq;
 using EdFi.Ods.Common.Extensions;
 using EdFi.Ods.Common.Models.Domain;
 using EdFi.Ods.Common.Models.Graphs;
-using EdFi.Ods.Common.Specifications;
 using QuickGraph;
 using EntitySpecification = EdFi.Ods.Common.Specifications.EntitySpecification;
 
@@ -25,41 +24,34 @@ namespace EdFi.Ods.Api.Services.Metadata.Controllers
         public static GraphML Create(BidirectionalGraph<EntityWithDataOperation, DataOperationEdge> graph)
         {
             return new GraphML
-                   {
-                       Id = "EdFi Dependencies", Nodes = graph.Vertices
-                                                              .Where(
-                                                                   x => EntitySpecification.HasResourceRepresentation(x.Entity))
-                                                              .Select(
-                                                                   x =>
-                                                                   {
-                                                                       var entity = x.ContextualConcreteEntity ?? x.Entity;
+            {
+                Id = "EdFi Dependencies",
+                Nodes = graph.Vertices.Where(x => EntitySpecification.HasResourceRepresentation(x.Entity))
+                    .Select(
+                        x =>
+                        {
+                            var entity = x.ContextualConcreteEntity ?? x.Entity;
 
-                                                                       return new Node
-                                                                              {
-                                                                                  Id =
-                                                                                      $"/{entity.SchemaUriSegment()}/{entity.PluralName.ToCamelCase()}"
-                                                                              };
-                                                                   })
-                                                              .Distinct(Node.NodeComparer)
-                                                              .ToList(),
-                       Edges = graph.Edges
-                                    .Where(
-                                         x => EntitySpecification.HasResourceRepresentation(x.Source.Entity))
-                                    .Select(
-                                         x =>
-                                         {
-                                             var source = x.Source.ContextualConcreteEntity ?? x.Source.Entity;
-                                             var target = x.Target.ContextualConcreteEntity ?? x.Target.Entity;
+                            return new Node {Id = $"/{entity.SchemaUriSegment()}/{entity.PluralName.ToCamelCase()}"};
+                        })
+                    .Distinct(Node.NodeComparer)
+                    .ToList(),
+                Edges = graph.Edges.Where(x => EntitySpecification.HasResourceRepresentation(x.Source.Entity))
+                    .Select(
+                        x =>
+                        {
+                            var source = x.Source.ContextualConcreteEntity ?? x.Source.Entity;
+                            var target = x.Target.ContextualConcreteEntity ?? x.Target.Entity;
 
-                                             return new Edge
-                                                    {
-                                                        Source = $"/{source.SchemaUriSegment()}/{source.PluralName.ToCamelCase()}",
-                                                        Target = $"/{target.SchemaUriSegment()}/{target.PluralName.ToCamelCase()}"
-                                                    };
-                                         })
-                                    .Distinct(Edge.SourceTargetComparer)
-                                    .ToList()
-                   };
+                            return new Edge
+                            {
+                                Source = $"/{source.SchemaUriSegment()}/{source.PluralName.ToCamelCase()}",
+                                Target = $"/{target.SchemaUriSegment()}/{target.PluralName.ToCamelCase()}"
+                            };
+                        })
+                    .Distinct(Edge.SourceTargetComparer)
+                    .ToList()
+            };
         }
     }
 }
