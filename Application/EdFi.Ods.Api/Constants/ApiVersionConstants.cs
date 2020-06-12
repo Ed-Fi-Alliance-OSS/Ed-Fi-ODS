268e82738bd96e3de3fe98635aa9aa4196d54a18
Travis Meador <t.meador@wearedoubleline.com>
2020-06-12 12:18:35 -0500

    update versions (#26)

--- Application/EdFi.Ods.Api/Constants/ApiVersionConstants.cs
+++ Application/EdFi.Ods.Api/Constants/ApiVersionConstants.cs
@@ -2,7 +2,7 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
- 
+
 using System.Reflection;
 
 namespace EdFi.Ods.Api.Constants
@@ -32,12 +32,12 @@ namespace EdFi.Ods.Api.Constants
         /// <summary>
         /// Informational version of the ods api.
         /// </summary>
-        public const string InformationalVersion = "3.4.0";
+        public const string InformationalVersion = "4.0.0";
 
         /// <summary>
         /// Semantic version of the ods api.
         /// </summary>
-        public const string Version = "3.4.0";
+        public const string Version = "4.0.0";
 
         /// <summary>
         /// Suite version of the ods api.
--- Application/EdFi.Ods.Common/XmlNamespace.cs
+++ Application/EdFi.Ods.Common/XmlNamespace.cs
@@ -2,14 +2,14 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
- 
+
 namespace EdFi.Ods.Common
 {
     public static class XmlNamespace
     {
         static XmlNamespace()
         {
-            Namespace = "http://ed-fi.org/0320b";
+            Namespace = "http://ed-fi.org/3.2.0-c";
             ExtensionPrefix = "EXTENSION";
         }
 
