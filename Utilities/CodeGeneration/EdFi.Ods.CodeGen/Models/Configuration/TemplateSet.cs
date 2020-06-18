3a49d345b88e5a7ce00ab4604daa836d077a3c0d
Jim McKay <jmckay@certicasolutions.com>
2020-06-18 13:37:51 -0500

    updated code gen to use the custom database engine

--- Utilities/CodeGeneration/EdFi.Ods.CodeGen/Models/Configuration/TemplateSet.cs
+++ Utilities/CodeGeneration/EdFi.Ods.CodeGen/Models/Configuration/TemplateSet.cs
@@ -3,6 +3,11 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using EdFi.Ods.Common.Configuration;
+using EdFi.Ods.Common.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace EdFi.Ods.CodeGen.Models.Configuration
 {
     public class TemplateSet
@@ -13,13 +18,14 @@ namespace EdFi.Ods.CodeGen.Models.Configuration
 
         public string OutputPath { get; set; }
 
-        public string DatabaseEngine { get; set; }
+        [JsonConverter(typeof(DatabaseEngineConverter))]
+        public DatabaseEngine DatabaseEngine { get; set; }
 
         public override string ToString()
             => $@"
 Name:        {Name}
 Driver:      {Driver}
-DatabaseEngine: {DatabaseEngine}
+DatabaseEngine: {DatabaseEngine.Value}
 OutputPath:  {OutputPath}
 ";
     }
