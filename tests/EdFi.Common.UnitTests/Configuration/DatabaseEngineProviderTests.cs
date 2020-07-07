b6bc20bd0d200206a1e535e9d832a5ce87730153
Jim McKay <jmckay@certicasolutions.com>
2020-07-07 15:36:17 -0500

    fixed broken build

--- tests/EdFi.Common.UnitTests/Configuration/DatabaseEngineProviderTests.cs
+++ tests/EdFi.Common.UnitTests/Configuration/DatabaseEngineProviderTests.cs
@@ -2,7 +2,7 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
- 
+
 using System;
 using System.Collections.Generic;
 using EdFi.Ods.Common.Configuration;
@@ -153,7 +153,7 @@ namespace EdFi.Ods.Common.UnitTests.Configuration
             [Test]
             public void Should_throw_NotSupportedException()
             {
-                ActualException.ShouldBeOfType<NotSupportedException>();
+                ActualException.ShouldBeOfType<ArgumentException>();
             }
         }
     }
