a53e3e1b0f763c4e9185f26d960a1b192d7b34d9
Jim McKay <jmckay@certicasolutions.com>
2020-07-15 14:26:59 -0500

    added sql server specific provisioner

--- Application/EdFi.Ods.Sandbox/Provisioners/SqlSandboxProvisioner.cs
+++ Application/EdFi.Ods.Sandbox/Provisioners/SqlSandboxProvisioner.cs
@@ -1,141 +1,198 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
- 
-using System.Data;
+
+using System;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using EdFi.Admin.DataAccess.Utils;
+using EdFi.Ods.Common.Configuration;
+using log4net;
 
 namespace EdFi.Ods.Sandbox.Provisioners
 {
-    public class SqlSandboxProvisioner : AzureSandboxProvisioner
+    public class SqlSandboxProvisioner : SandboxProvisionerBase
     {
-        protected override void CopySandbox(string originalDatabaseName, string newDatabaseName)
+        private readonly ILog _logger = LogManager.GetLogger(nameof(SqlSandboxProvisioner));
+
+        public SqlSandboxProvisioner(IConfigValueProvider configValueProvider,
+            IConfigConnectionStringsProvider connectionStringsProvider)
+            : base(configValueProvider, connectionStringsProvider) { }
+
+        protected override DbConnection CreateConnection() => new SqlConnection(ConnectionString);
+
+        public override async Task<string[]> GetSandboxDatabasesAsync()
         {
-            string datafile, logfile;
-            var backup = Path.Combine(GetBackupDirectory(), originalDatabaseName + ".bak");
-            GetDatabaseFiles(originalDatabaseName, newDatabaseName, out datafile, out logfile);
-
-            using (var conn = new SqlConnection(ConnectionString))
+            using (var conn = CreateConnection())
             {
-                conn.Open();
+                var results = await conn.QueryAsync<string>(
+                        $"SELECT name FROM sys.databases WHERE name like @DbName;",
+                        new {DbName = DatabaseNameBuilder.SandboxNameForKey("%")}, commandTimeout: CommandTimeout)
+                    .ConfigureAwait(false);
 
-                var sql = string.Format(
-                    @"BACKUP DATABASE [{0}] TO DISK = '{1}' WITH INIT;",
-                    originalDatabaseName,
-                    backup);
+                return results.ToArray();
+            }
+        }
 
-                using (var cmd = conn.CreateCommand())
+        public override async Task RenameSandboxAsync(string oldName, string newName)
+        {
+            using (var conn = CreateConnection())
+            {
+                await conn.ExecuteAsync($"ALTER DATABASE {oldName} MODIFY Name = {newName};")
+                    .ConfigureAwait(false);
+            }
+        }
+
+        public override async Task<SandboxStatus> GetSandboxStatusAsync(string clientKey)
+        {
+            try
+            {
+                using (var conn = CreateConnection())
                 {
-                    cmd.CommandText = sql;
-                    cmd.CommandTimeout = CommandTimeout;
-                    cmd.ExecuteNonQuery();
+                    var results = await conn.QueryAsync<SandboxStatus>(
+                            $"SELECT name as Name, state as Code, state_desc as Description FROM sys.databases WHERE name = @DbName;",
+                            new {DbName = DatabaseNameBuilder.SandboxNameForKey(clientKey)},
+                            commandTimeout: CommandTimeout)
+                        .ConfigureAwait(false);
+
+                    return results.SingleOrDefault() ?? SandboxStatus.ErrorStatus();
                 }
+            }
+            catch (Exception e)
+            {
+                _logger.Debug(e);
+                throw;
+            }
+        }
 
-                sql = string.Format(@"RESTORE FILELISTONLY FROM DISK = '{0}';", backup);
-                string logicalName = null;
-
-                using (var cmd = conn.CreateCommand())
+        public override async Task CopySandboxAsync(string originalDatabaseName, string newDatabaseName)
+        {
+            using (var conn = CreateConnection())
+            {
+                try
                 {
-                    cmd.CommandText = sql;
-                    cmd.CommandTimeout = CommandTimeout;
+                    await conn.OpenAsync();
 
-                    using (var reader = cmd.ExecuteReader())
+                    string backupDirectory = await GetBackupDirectoryAsync()
+                        .ConfigureAwait(false);
+
+                    _logger.Debug($"backup directory = {backupDirectory}");
+
+                    string backup = Path.Combine(backupDirectory, originalDatabaseName + ".bak");
+                    _logger.Debug($"backup file = {backup}");
+
+                    var sqlFileInfo = await GetDatabaseFilesAsync(originalDatabaseName, newDatabaseName)
+                        .ConfigureAwait(false);
+
+                    await BackUpAndRestoreSandbox()
+                        .ConfigureAwait(false);
+
+                    // NOTE: these helper functions are using the same connection now.
+                    async Task BackUpAndRestoreSandbox()
                     {
-                        if (reader.Read())
+                        _logger.Debug($"backing up {originalDatabaseName} to file {backup}");
+
+                        await conn.ExecuteAsync(
+                            $@"BACKUP DATABASE [{originalDatabaseName}] TO DISK = '{backup}' WITH INIT;",
+                            commandTimeout: CommandTimeout).ConfigureAwait(false);
+
+                        string logicalName = null;
+
+                        _logger.Debug($"restoring files from {backup}.");
+
+                        using (var reader = await conn.ExecuteReaderAsync($@"RESTORE FILELISTONLY FROM DISK = '{backup}';")
+                            .ConfigureAwait(false))
                         {
-                            logicalName = reader.GetString(0);
+                            if (await reader.ReadAsync().ConfigureAwait(false))
+                            {
+                                logicalName = reader.GetString(0);
+                                _logger.Debug($"logical name = {logicalName}");
+                            }
                         }
+
+                        _logger.Debug($"Restoring database {newDatabaseName} from {backup}");
+
+                        await conn.ExecuteAsync(
+                                $@"RESTORE DATABASE [{newDatabaseName}] FROM DISK = '{backup}' WITH REPLACE, MOVE '{logicalName}' TO '{sqlFileInfo.Data}', MOVE '{logicalName}_log' TO '{sqlFileInfo.Log}';")
+                            .ConfigureAwait(false);
+                    }
+
+                    async Task<string> GetBackupDirectoryAsync()
+                    {
+                        return await GetSqlRegistryValueAsync(
+                                @"HKEY_LOCAL_MACHINE", @"Software\Microsoft\MSSQLServer\MSSQLServer", @"BackupDirectory")
+                            .ConfigureAwait(false);
+                    }
+
+                    async Task<string> GetSqlRegistryValueAsync(string subtree, string folder, string key)
+                    {
+                        var sql = $@"EXEC master.dbo.xp_instance_regread N'{subtree}', N'{folder}',N'{key}'";
+                        string path = null;
+
+                        using (var cmd = conn.CreateCommand())
+                        {
+                            cmd.CommandText = sql;
+                            cmd.CommandTimeout = CommandTimeout;
+
+                            _logger.Debug($"running stored procedure = {sql}");
+
+                            using (var reader = await cmd.ExecuteReaderAsync().ConfigureAwait(false))
+                            {
+                                if (await reader.ReadAsync().ConfigureAwait(false))
+                                {
+                                    path = reader.GetString(1);
+                                }
+                            }
+                        }
+
+                        _logger.Debug($"path from registry = {path}");
+                        return path;
+                    }
+
+                    async Task<SqlFileInfo> GetDatabaseFilesAsync(string originalName, string newName)
+                    {
+                        string dataPath = await GetSqlDataPathAsync(originalName, DataPathType.Data).ConfigureAwait(false);
+                        string logPath = await GetSqlDataPathAsync(originalName, DataPathType.Log).ConfigureAwait(false);
+
+                        return new SqlFileInfo
+                        {
+                            Data = Path.Combine(dataPath, $"{newName}.mdf"),
+                            Log = Path.Combine(logPath, $"{newName}.ldf")
+                        };
+                    }
+
+                    async Task<string> GetSqlDataPathAsync(string originalName, DataPathType dataPathType)
+                    {
+                        var type = (int) dataPathType;
+
+                        // Since we know we have an existing database, use its data file location to figure out where to put new databases
+                        var sql =
+                            $"use [{originalName}];DECLARE @default_data_path nvarchar(1000);DECLARE @sqlexec nvarchar(200);SET @sqlexec = N'select TOP 1 @data_path=physical_name from sys.database_files where type={type};';EXEC sp_executesql @sqlexec, N'@data_path nvarchar(max) OUTPUT',@data_path = @default_data_path OUTPUT;SELECT @default_data_path;";
+
+                        string fullPath = await conn.ExecuteScalarAsync<string>(sql, commandTimeout: CommandTimeout)
+                            .ConfigureAwait(false);
+
+                        return Path.GetDirectoryName(fullPath);
                     }
                 }
-
-                sql =
-                    string.Format(
-                        @"RESTORE DATABASE [{0}] FROM DISK = '{1}' WITH REPLACE, MOVE '{2}' TO '{4}', MOVE '{3}_log' TO '{5}';",
-                        newDatabaseName,
-                        backup,
-                        logicalName,
-                        logicalName,
-                        datafile,
-                        logfile);
-
-                using (var cmd = conn.CreateCommand())
+                catch (Exception e)
                 {
-                    cmd.CommandText = sql;
-                    cmd.CommandTimeout = CommandTimeout;
-                    cmd.ExecuteNonQuery();
+                    _logger.Error(e);
+                    throw;
                 }
             }
         }
 
-        private string GetBackupDirectory()
+        private class SqlFileInfo
         {
-            return GetSqlRegistryValue(@"HKEY_LOCAL_MACHINE", @"Software\Microsoft\MSSQLServer\MSSQLServer", @"BackupDirectory");
-        }
+            public string Data { get; set; }
 
-        private string GetSqlRegistryValue(string subtree, string folder, string key)
-        {
-            var sql = string.Format(@"EXEC master.dbo.xp_instance_regread N'{0}', N'{1}',N'{2}'", subtree, folder, key);
-            string path = null;
-
-            using (var conn = new SqlConnection(ConnectionString))
-            {
-                using (var cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = sql;
-                    cmd.CommandTimeout = CommandTimeout;
-                    conn.Open();
-
-                    using (var reader = cmd.ExecuteReader(CommandBehavior.CloseConnection))
-                    {
-                        if (reader.Read())
-                        {
-                            path = reader.GetString(1);
-                        }
-                    }
-                }
-            }
-
-            return path;
-        }
-
-        private string GetSqlDataPath(string originalDatabaseName, DataPathType dataPathType)
-        {
-            var type = (int) dataPathType;
-
-            // Since we know we have an existing database, use its data file location to figure out where to put new databases
-            var sql =
-                "use [" + originalDatabaseName + "];" +
-                "DECLARE @default_data_path nvarchar(1000);" +
-                "DECLARE @sqlexec nvarchar(200);" +
-                "SET @sqlexec = N'select TOP 1 @data_path=physical_name from sys.database_files where type=" + type + ";';" +
-                "EXEC sp_executesql @sqlexec, N'@data_path nvarchar(max) OUTPUT',@data_path = @default_data_path OUTPUT;" +
-                "SELECT @default_data_path;";
-
-            string path = null;
-
-            using (var conn = new SqlConnection(ConnectionString))
-            {
-                using (var cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = sql;
-                    cmd.CommandTimeout = CommandTimeout;
-                    conn.Open();
-                    var fullPath = (string) cmd.ExecuteScalar();
-                    path = Path.GetDirectoryName(fullPath);
-                }
-            }
-
-            return path;
-        }
-
-        private void GetDatabaseFiles(string originalDatabaseName, string newDatabaseName, out string data, out string log)
-        {
-            var dataPath = GetSqlDataPath(originalDatabaseName, DataPathType.Data);
-            var logPath = GetSqlDataPath(originalDatabaseName, DataPathType.Log);
-            data = Path.Combine(dataPath, string.Format("{0}.mdf", newDatabaseName));
-            log = Path.Combine(logPath, string.Format("{0}.ldf", newDatabaseName));
+            public string Log { get; set; }
         }
 
         private enum DataPathType
