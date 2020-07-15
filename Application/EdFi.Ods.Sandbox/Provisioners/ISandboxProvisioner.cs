80d283b79b4d9088efb6a0d06e6eebc565a03561
Jim McKay <jmckay@certicasolutions.com>
2020-07-15 14:08:27 -0500

    updated the interface to include async methods added new method for removal

--- Application/EdFi.Ods.Sandbox/Provisioners/ISandboxProvisioner.cs
+++ Application/EdFi.Ods.Sandbox/Provisioners/ISandboxProvisioner.cs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Apache-2.0
+﻿// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -15,10 +15,26 @@ namespace EdFi.Ods.Sandbox.Provisioners
 
         void DeleteSandboxes(params string[] deletedClientKeys);
 
+        void RenameSandbox(string oldName, string newName);
+
         SandboxStatus GetSandboxStatus(string clientKey);
 
         void ResetDemoSandbox();
 
         string[] GetSandboxDatabases();
+
+        Task AddSandboxAsync(string sandboxKey, SandboxType sandboxType);
+
+        Task DeleteSandboxesAsync(params string[] deletedClientKeys);
+
+        Task RenameSandboxAsync(string oldName, string newName);
+
+        Task<SandboxStatus> GetSandboxStatusAsync(string clientKey);
+
+        Task ResetDemoSandboxAsync();
+
+        Task<string[]> GetSandboxDatabasesAsync();
+
+        Task CopySandboxAsync(string originalDatabaseName, string newDatabaseName);
     }
 }
