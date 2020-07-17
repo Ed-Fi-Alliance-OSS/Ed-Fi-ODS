0f789dba47ef9761c4705eb58a4c170c0f949753
Jim McKay <jmckay@certicasolutions.com>
2020-07-17 16:56:00 -0500

    refactored to remove the dependency on the config settings

--- Application/EdFi.Admin.DataAccess/Utils/DatabaseNameBuilder.cs
+++ Application/EdFi.Admin.DataAccess/Utils/DatabaseNameBuilder.cs
@@ -2,73 +2,37 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
- 
-using System.Configuration;
-using System.Data.SqlClient;
 
 namespace EdFi.Admin.DataAccess.Utils
 {
-    public class DatabaseNameBuilder
+    public static class DatabaseNameBuilder
     {
+        private const string EdFi = "EdFi_";
         private const string TemplatePrefix = "Ods_";
         private const string SandboxPrefix = TemplatePrefix + "Sandbox_";
 
-        private const string TemplateEmptyDatabase = TemplatePrefix + "Empty_Template";
-        private const string TemplateMinimalDatabase = TemplatePrefix + "Minimal_Template";
-        private const string TemplateSampleDatabase = TemplatePrefix + "Populated_Template";
+        public const string TemplateEmptyDatabase = TemplatePrefix + "Empty_Template";
+        public const string TemplateMinimalDatabase = TemplatePrefix + "Minimal_Template";
+        public const string TemplateSampleDatabase = TemplatePrefix + "Populated_Template";
         public const string DemoSandboxDatabase = "EdFi_Ods";
-
-        private static string _databaseNameTemplate;
-
-        private static string DatabaseNameTemplate
-        {
-            get
-            {
-                return _databaseNameTemplate ??
-                       (_databaseNameTemplate =
-                           ConfigurationManager.ConnectionStrings["EdFi_Ods"] != null
-                               ? new SqlConnectionStringBuilder(
-                                   ConfigurationManager.ConnectionStrings["EdFi_Ods"]
-                                                       .ConnectionString).InitialCatalog
-                               : string.Empty);
-            }
-        }
 
         public static string EmptyDatabase
         {
-            get { return DatabaseName(TemplateEmptyDatabase); }
+            get => EdFi + TemplateEmptyDatabase;
         }
 
         public static string MinimalDatabase
         {
-            get { return DatabaseName(TemplateMinimalDatabase); }
+            get => EdFi + TemplateMinimalDatabase;
         }
 
         public static string SampleDatabase
         {
-            get { return DatabaseName(TemplateSampleDatabase); }
+            get => EdFi + TemplateSampleDatabase;
         }
 
-        private static string DatabaseName(string databaseName)
-        {
-            return DatabaseNameTemplate.Contains("{0}")
-                ? string.Format(DatabaseNameTemplate, databaseName)
-                : DatabaseNameTemplate;
-        }
+        public static string SandboxNameForKey(string key) => EdFi + SandboxPrefix + key;
 
-        public static string SandboxNameForKey(string key)
-        {
-            return DatabaseName(SandboxPrefix + key);
-        }
-
-        public static string YearSpecificOdsDatabaseName(int year)
-        {
-            return DatabaseName(TemplatePrefix + year);
-        }
-
-        public static string KeyFromSandboxName(string sandboxName)
-        {
-            return sandboxName.Replace(DatabaseName(SandboxPrefix), string.Empty);
-        }
+        public static string KeyFromSandboxName(string sandboxName) => sandboxName.Replace(SandboxPrefix, string.Empty);
     }
 }
