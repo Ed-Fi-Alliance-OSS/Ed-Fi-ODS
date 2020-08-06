fa7425c9a55746997e7099e498d9b58c7ce30cfc
BhanuprakashThunga <bthunga@inniveinc.com>
2020-08-06 21:00:43 +0530

    Converted EdFi.Ods.Tests.TestExtension to .NET Core

--- Application/EdFi.Ods.Tests.TestExtension/Controllers/Controllers.net.cs
+++ Application/EdFi.Ods.Tests.TestExtension/Controllers/Controllers.net.cs
@@ -3,9 +3,11 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+#if NETFRAMEWORK
 using System.Web.Http;
 
 namespace EdFi.Ods.Tests.TestExtension.Controllers
 {
     public class TestController : ApiController { }
 }
+#endif
