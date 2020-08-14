71e929152cf34672b1f5f94bf43d32cbced37245
Thomas Nunnery <t.nunnery@wearedoubleline.com>
2020-08-14 08:07:42 -0500

    Exposd Common Urls via the version endpoint.

--- Application/EdFi.Ods.Api/Services/Controllers/VersionController.cs
+++ Application/EdFi.Ods.Api/Services/Controllers/VersionController.cs
@@ -3,11 +3,14 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System;
 using System.Linq;
+using System.Security.Policy;
 using System.Web.Http;
 using EdFi.Ods.Api.Constants;
 using EdFi.Ods.Common;
 using EdFi.Ods.Common.Configuration;
+using EdFi.Ods.Common.Context;
 using EdFi.Ods.Common.Models;
 
 namespace EdFi.Ods.Api.Services.Controllers
@@ -17,24 +20,32 @@ namespace EdFi.Ods.Api.Services.Controllers
         private readonly IApiConfigurationProvider _apiConfigurationProvider;
         private readonly IDomainModelProvider _domainModelProvider;
         private readonly IApiVersionProvider _apiVersionProvider;
+        private readonly ISchoolYearContextProvider _schoolYearContextProvider;
 
         public VersionController(
             IApiConfigurationProvider apiConfigurationProvider,
             IDomainModelProvider domainModelProvider,
-            IApiVersionProvider apiVersionProvider)
+            IApiVersionProvider apiVersionProvider,
+            ISchoolYearContextProvider schoolYearContextProvider)
         {
             Preconditions.ThrowIfNull(apiConfigurationProvider, nameof(apiConfigurationProvider));
             Preconditions.ThrowIfNull(domainModelProvider, nameof(domainModelProvider));
             Preconditions.ThrowIfNull(apiVersionProvider, nameof(apiVersionProvider));
+            Preconditions.ThrowIfNull(schoolYearContextProvider, nameof(schoolYearContextProvider));
 
             _apiConfigurationProvider = apiConfigurationProvider;
             _domainModelProvider = domainModelProvider;
             _apiVersionProvider = apiVersionProvider;
+            _schoolYearContextProvider = schoolYearContextProvider;
         }
 
         [Route("")]
         public IHttpActionResult Get()
         {
+            var metaDataUrl = string.Empty;
+            var dependenciesUrl = string.Empty;
+            var oauthUrl = string.Empty;
+
             var dataModels = _domainModelProvider
                 .GetDomainModel()
                 .Schemas
@@ -46,6 +57,21 @@ namespace EdFi.Ods.Api.Services.Controllers
                     })
                 .ToArray();
 
+            if (_apiConfigurationProvider.IsYearSpecific())
+            {
+                var schoolYear = _schoolYearContextProvider.GetSchoolYear();
+                metaDataUrl = Url.Link("MetadataSections", new { controller = "openapimetadata", action = "getsections", schoolYearFromRoute = schoolYear });
+                dependenciesUrl = Url.Link("AggregateDependencies", new { controller = "aggregatedependency", action = "get", schoolYearFromRoute = schoolYear });
+                oauthUrl = Url.Link("OAuthToken", new { controller = "Token" });
+            }
+            else
+            {
+                metaDataUrl = Url.Link("MetadataSections", new { controller = "openapimetadata", action = "getsections" });
+                dependenciesUrl = Url.Link("AggregateDependencies", new { controller = "aggregatedependency", action = "get" });
+                oauthUrl = Url.Link("OAuthToken", new { controller = "Token" });
+            }
+
+
             var content = new
             {
                 version = _apiVersionProvider.Version,
@@ -53,8 +79,12 @@ namespace EdFi.Ods.Api.Services.Controllers
                 suite = _apiVersionProvider.Suite,
                 build = _apiVersionProvider.Build,
                 apiMode = _apiConfigurationProvider.Mode.DisplayName,
-                dataModels = dataModels
-            };
+                dataModels = dataModels,
+                metaDataUrl = metaDataUrl,
+                dependenciesUrl = dependenciesUrl,
+                oauthUrl = oauthUrl,
+                apiUrl = new Uri(new Uri(Url.Request.RequestUri.AbsoluteUri), $"/data/v{_apiVersionProvider.InformationalVersion}/").ToString()
+        };
 
             return Ok(content);
         }
