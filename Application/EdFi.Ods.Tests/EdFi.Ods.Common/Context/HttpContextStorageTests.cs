58bd91396c57e3a1b87d6b59c3fd9865b3a156c9
Jim McKay <jamessmckay@users.noreply.github.com>
2020-08-21 14:23:51 -0500

    Update Application/EdFi.Ods.Tests/EdFi.Ods.Common/Context/HttpContextStorageTests.cs
    Co-authored-by: Travis Meador <t.meador@wearedoubleline.com>

--- Application/EdFi.Ods.Tests/EdFi.Ods.Common/Context/HttpContextStorageTests.cs
+++ Application/EdFi.Ods.Tests/EdFi.Ods.Common/Context/HttpContextStorageTests.cs
@@ -66,7 +66,6 @@ namespace EdFi.Ods.Tests.EdFi.Common.Context
 
             var actual = httpContextStorage.GetValue<TestObject>(key);
             actual.Text.ShouldBe(expected);
-
         }
     }
 }
