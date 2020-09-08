757ed95b600097831c98bea4f36dee7efd9d34f8
tparandiev <tparandiev@inniveinc.com>
2020-09-08 06:04:01 -0700

    remove IApiConfigurationProvider

--- Application/EdFi.Common/Configuration/IApiConfigurationProvider.net.cs
+++ Application/EdFi.Common/Configuration/IApiConfigurationProvider.net.cs
@@ -2,6 +2,7 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
+#if NETFRAMEWORK
 
 namespace EdFi.Ods.Common.Configuration
 {
@@ -14,3 +15,4 @@ namespace EdFi.Ods.Common.Configuration
         bool IsYearSpecific();
     }
 }
+#endif
