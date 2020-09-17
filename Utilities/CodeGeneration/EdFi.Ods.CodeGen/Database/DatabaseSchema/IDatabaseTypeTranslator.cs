be51958f3e8e0316330a68b2e7895cbac437699d
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2020-09-16 23:52:26 -0500

    Added XML documentation to the IDatabaseTypeTranslator.

--- Utilities/CodeGeneration/EdFi.Ods.CodeGen/Database/DatabaseSchema/IDatabaseTypeTranslator.cs
+++ Utilities/CodeGeneration/EdFi.Ods.CodeGen/Database/DatabaseSchema/IDatabaseTypeTranslator.cs
@@ -7,12 +7,37 @@ using System.Data;
 
 namespace EdFi.Ods.CodeGen.Database.DatabaseSchema
 {
+    /// <summary>
+    /// Defines methods for converting from database physical data types to various code generation output formats
+    /// for artifacts based on database views.
+    /// </summary>
     public interface IDatabaseTypeTranslator
     {
+        /// <summary>
+        /// Gets the C# .NET type associated with the supplied database data type.
+        /// </summary>
+        /// <param name="sqlType">The raw data type used by the reference database.</param>
+        /// <returns>The corresponding C# .NET type.</returns>
+        /// <remarks>This supports the code generation process for database views read by the DatabaseSchemaReader library which
+        /// only provides the data type from the data view in the reference database.
+        /// </remarks>
         string GetSysType(string sqlType);
 
+        /// <summary>
+        /// Gets the NHibernate mapping type associated with the supplied database data type.
+        /// </summary>
+        /// <param name="sqlType">The raw data type used by the reference database.</param>
+        /// <returns>The corresponding NHibernate mapping type.</returns>
+        /// <remarks>This supports the code generation process for database views read by the DatabaseSchemaReader library which
+        /// only provides the data type from the data view in the reference database.
+        /// </remarks>
         string GetNHType(string sqlType);
 
+        /// <summary>
+        /// Gets the <see cref="DbType" /> enumeration value associated with the supplied database data type.
+        /// </summary>
+        /// <param name="sqlType">The raw data type used by the reference database.</param>
+        /// <returns>The corresponding <see cref="DbType" /> enumeration value.</returns>
         DbType GetDbType(string sqlType);
     }
 }
