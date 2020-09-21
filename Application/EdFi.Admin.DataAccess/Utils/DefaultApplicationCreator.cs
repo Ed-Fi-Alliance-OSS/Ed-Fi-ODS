601121eab247a6302830dd22093e15fbe62f4cb8
raghuramakrishna <68504753+raghuramakrishna@users.noreply.github.com>
2020-09-21 22:42:27 +0530

    [ODS-3505] Sandbox Admin Web to .NET Core (#173)
    * fixed connection string key
    
    * fixed sandbox name
    
    * added a default for null value on sandbox name
    
    Co-authored-by: Jim McKay <jmckay@certicasolutions.com>

--- Application/EdFi.Admin.DataAccess/Utils/DefaultApplicationCreator.cs
+++ Application/EdFi.Admin.DataAccess/Utils/DefaultApplicationCreator.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -11,7 +11,6 @@ using System.Linq;
 using EdFi.Admin.DataAccess.Contexts;
 using EdFi.Admin.DataAccess.Models;
 using EdFi.Ods.Common;
-using EdFi.Ods.Common.Configuration;
 using Microsoft.Extensions.Configuration;
 
 namespace EdFi.Admin.DataAccess.Utils
@@ -45,7 +44,7 @@ namespace EdFi.Admin.DataAccess.Utils
                                     .Include(x => x.Applications.Select<Application, ICollection<ApplicationEducationOrganization>>(a => a.ApplicationEducationOrganizations))
                                     .Single();
 
-                var defaultAppName = _configuration.GetSection("DefaultApplicationName");
+                var defaultAppName = _configuration.GetSection("DefaultApplicationName").Value ?? "Default Sandbox Application";
                 var applicationName = defaultAppName + " " + sandboxType;
                 var application = GetApplication(context, vendor, applicationName);
 
