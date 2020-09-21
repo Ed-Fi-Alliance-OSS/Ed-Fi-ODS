fcdb180b58be0fbaf58b622b409f0ce3671b5727
Jim McKay <jmckay@certicasolutions.com>
2020-09-21 10:40:29 -0500

    fixed sandbox name

--- Application/EdFi.Admin.DataAccess/Utils/DefaultApplicationCreator.cs
+++ Application/EdFi.Admin.DataAccess/Utils/DefaultApplicationCreator.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
@@ -11,7 +11,6 @@ using System.Linq;
 using EdFi.Admin.DataAccess.Contexts;
 using EdFi.Admin.DataAccess.Models;
 using EdFi.Ods.Common;
-using EdFi.Ods.Common.Configuration;
 using Microsoft.Extensions.Configuration;
 
 namespace EdFi.Admin.DataAccess.Utils
@@ -46,7 +45,7 @@ namespace EdFi.Admin.DataAccess.Utils
                                     .Single();
 
                 var defaultAppName = _configuration.GetSection("DefaultApplicationName");
-                var applicationName = defaultAppName + " " + sandboxType;
+                var applicationName = defaultAppName.Value + " " + sandboxType;
                 var application = GetApplication(context, vendor, applicationName);
 
                 context.SaveChanges();
