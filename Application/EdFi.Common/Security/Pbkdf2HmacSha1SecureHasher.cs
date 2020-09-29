b3df8f4a19bd44eae63ff08921f59fa47f23a908
Jim McKay <jmckay@certicasolutions.com>
2020-09-29 17:35:04 -0500

    reformat

--- Application/EdFi.Common/Security/Pbkdf2HmacSha1SecureHasher.cs
+++ Application/EdFi.Common/Security/Pbkdf2HmacSha1SecureHasher.cs
@@ -4,21 +4,24 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using System.Security.Cryptography;
-using EdFi.Ods.Common.ChainOfResponsibility;
 using EdFi.Ods.Common.Extensions;
 using EdFi.Ods.Common.Security.Helpers;
 
 namespace EdFi.Ods.Common.Security
 {
-    public class Pbkdf2HmacSha1SecureHasher : ChainOfResponsibilityBase<ISecureHasher, SecureHashRequest, PackedHash>,
-                                              ISecureHasher
+    public class Pbkdf2HmacSha1SecureHasher : ISecureHasher
     {
         public const string ConfigurationAlgorithmName = "PBKDF2-HMACSHA1";
 
-        public Pbkdf2HmacSha1SecureHasher(ISecureHasher next)
-            : base(next) { }
+        public string Algorithm
+        {
+            get => ConfigurationAlgorithmName;
+        }
 
-        public string Algorithm => ConfigurationAlgorithmName;
+        public int GetAlgorithmHashCode
+        {
+            get => HashHelper.GetSha256Hash(Algorithm).ToInt32();
+        }
 
         public PackedHash ComputeHash(string secret, int hashAlgorithm, int iterations, byte[] salt)
         {
@@ -30,9 +33,13 @@ namespace EdFi.Ods.Common.Security
             }
 
             return new PackedHash
-                   {
-                       Format = 0, HashAlgorithm = hashAlgorithm, HashBytes = bytes, Iterations = iterations, Salt = salt
-                   };
+            {
+                Format = 0,
+                HashAlgorithm = hashAlgorithm,
+                HashBytes = bytes,
+                Iterations = iterations,
+                Salt = salt
+            };
         }
 
         public PackedHash ComputeHash(string secret, int hashAlgorithm, int iterations, int saltSizeInBytes)
@@ -47,24 +54,13 @@ namespace EdFi.Ods.Common.Security
             }
 
             return new PackedHash
-                   {
-                       Format = 0, HashAlgorithm = hashAlgorithm, HashBytes = bytes, Iterations = iterations, Salt = salt
-                   };
-        }
-
-        protected override bool CanHandleRequest(SecureHashRequest request)
-        {
-            // to allow for auto upgrading of an existing hash routine that used the string version of GetHashCode()
-            // we need to keep it in this check. Note, the string version of GetHashCode() is not deterministic when outside of the app domain.
-            return request.HashAlgorithm == HashHelper.GetSha256Hash(Algorithm).ToInt32()
-                   || request.HashAlgorithm == Algorithm.GetHashCode();
-        }
-
-        protected override PackedHash HandleRequest(SecureHashRequest request)
-        {
-            return request.Salt != null
-                ? ComputeHash(request.Secret, request.HashAlgorithm, request.Iterations, request.Salt)
-                : ComputeHash(request.Secret, request.HashAlgorithm, request.Iterations, request.SaltSizeInBytes);
+            {
+                Format = 0,
+                HashAlgorithm = hashAlgorithm,
+                HashBytes = bytes,
+                Iterations = iterations,
+                Salt = salt
+            };
         }
     }
 }
