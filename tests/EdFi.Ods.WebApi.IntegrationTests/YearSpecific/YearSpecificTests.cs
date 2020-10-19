9b34abf32685e9dce0ddc0a655428606fb7764c6
Raghu Ramakrishna <rramakrishna@inniveinc.com>
2020-10-19 15:34:33 +0530

    cleaned up code

--- tests/EdFi.Ods.WebApi.IntegrationTests/YearSpecific/YearSpecificTests.cs
+++ tests/EdFi.Ods.WebApi.IntegrationTests/YearSpecific/YearSpecificTests.cs
@@ -54,83 +54,43 @@ namespace EdFi.Ods.WebApi.IntegrationTests.YearSpecific
             client2014.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Guid.NewGuid().ToString("n"));
             client2015.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Guid.NewGuid().ToString("n"));
 
-                // create 2014
-                // var uniqueId2014Response = await client.PostAsync(
-                //     UriHelper.BuildIdentityUri("identities", 2014),
-                //     new StringContent(
-                //         JsonConvert.SerializeObject(
-                //             new IdentityCreateRequest
-                //             {
-                //                 BirthDate = new DateTime(1995, 2, 3),
-                //                 SexType = "Male"
-                //             }),
-                //         Encoding.UTF8,
-                //         "application/json"), _cancellationToken);
+			var create2014Response = await client2014.PostAsync(
+				_uriHelper.BuildOdsUri("students", 2014),
+				new StringContent(
+					ResourceHelper.CreateStudent(
+						uniqueId2014,
+						DateTime.Now.Ticks.ToString(
+							CultureInfo.InvariantCulture),
+						DateTime.Now.Ticks.ToString(
+							CultureInfo.InvariantCulture)),
+					Encoding.UTF8,
+					"application/json"), _cancellationToken);
 
-                // uniqueId2014 = await ExtractIdFromHttpResponseAsync(uniqueId2014Response);
+			create2014Response.EnsureSuccessStatusCode();
 
-                var create2014Response = await client2014.PostAsync(
-                    _uriHelper.BuildOdsUri("students", 2014),
-                    new StringContent(
-                        ResourceHelper.CreateStudent(
-                            uniqueId2014,
-                            DateTime.Now.Ticks.ToString(
-                                CultureInfo.InvariantCulture),
-                            DateTime.Now.Ticks.ToString(
-                                CultureInfo.InvariantCulture)),
-                        Encoding.UTF8,
-                        "application/json"), _cancellationToken);
+			var create2015Response = await client2015.PostAsync(
+				_uriHelper.BuildOdsUri("students", 2015),
+				new StringContent(
+					ResourceHelper.CreateStudent(
+						uniqueId2015,
+						DateTime.Now.Ticks.ToString(
+							CultureInfo.InvariantCulture),
+						DateTime.Now.Ticks.ToString(
+							CultureInfo.InvariantCulture)),
+					Encoding.UTF8,
+					"application/json"), _cancellationToken);
 
-                create2014Response.EnsureSuccessStatusCode();
+			create2015Response.EnsureSuccessStatusCode();
 
-                // create 2015
-                var create2015Response = await client2015.PostAsync(
-                    _uriHelper.BuildOdsUri("students", 2015),
-                    new StringContent(
-                        ResourceHelper.CreateStudent(
-                            uniqueId2015,
-                            DateTime.Now.Ticks.ToString(
-                                CultureInfo.InvariantCulture),
-                            DateTime.Now.Ticks.ToString(
-                                CultureInfo.InvariantCulture)),
-                        Encoding.UTF8,
-                        "application/json"), _cancellationToken);
+			int? exists2014 = await StudentExistsAsync(2014, uniqueId2014);
+			int? notExists2014 = await StudentExistsAsync(2015, uniqueId2014);
+			int? exists2015 = await StudentExistsAsync(2015, uniqueId2015);
+			int? notExists2015 = await StudentExistsAsync(2014, uniqueId2015);
 
-                create2015Response.EnsureSuccessStatusCode();
-
-                int? exists2014 = await StudentExistsAsync(2014, uniqueId2014);
-                int? notExists2014 = await StudentExistsAsync(2015, uniqueId2014);
-                int? exists2015 = await StudentExistsAsync(2015, uniqueId2015);
-                int? notExists2015 = await StudentExistsAsync(2014, uniqueId2015);
-
-                exists2014.ShouldNotBeNull();
-                notExists2014.ShouldBeNull();
-                exists2015.ShouldNotBeNull();
-                notExists2015.ShouldBeNull();
-        }
-
-        private async Task<string> ExtractIdFromHttpResponseAsync(HttpResponseMessage responseMessage)
-        {
-            var uniqueId = Guid.NewGuid().ToString("n");
-
-            if (!responseMessage.IsSuccessStatusCode)
-            {
-                return uniqueId;
-            }
-
-            var json = await responseMessage.Content.ReadAsStringAsync();
-            var identitySearchResponses = JsonConvert.DeserializeObject<IdentitySearchResponse>(json).SearchResponses.ToList();
-
-            if (!identitySearchResponses.Any())
-            {
-                return uniqueId;
-            }
-
-            var identityResponse = identitySearchResponses.Where(x => x.Responses.Any())
-                .SelectMany(x => x.Responses)
-                .FirstOrDefault();
-
-            return identityResponse?.UniqueId ?? uniqueId;
+			exists2014.ShouldNotBeNull();
+			notExists2014.ShouldBeNull();
+			exists2015.ShouldNotBeNull();
+			notExists2015.ShouldBeNull();
         }
 
         private async Task<int?> StudentExistsAsync(int schoolYear, string uniqueId)
