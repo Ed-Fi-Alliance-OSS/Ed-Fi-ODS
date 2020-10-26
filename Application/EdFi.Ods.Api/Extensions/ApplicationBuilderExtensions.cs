5b46dbec3448fa725356e2fbb34f4b420fac80c5
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2020-10-25 22:30:08 -0500

    Incorporating review feedback.

--- Application/EdFi.Ods.Api/Extensions/ApplicationBuilderExtensions.cs
+++ Application/EdFi.Ods.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -10,6 +10,15 @@ namespace EdFi.Ods.Api.Extensions
 {
     public static class ApplicationBuilderExtensions
     {
-        public static IApplicationBuilder UseOpenApiMetadata(this IApplicationBuilder builder) => builder.UseMiddleware<OpenApiMetadataMiddleware>();
+        public static IApplicationBuilder UseOpenApiMetadata(this IApplicationBuilder builder)
+            => builder.UseMiddleware<OpenApiMetadataMiddleware>();
+
+        /// <summary>
+        /// Adds the <see cref="EdFiApiAuthenticationMiddleware"/> to the specified <see cref="IApplicationBuilder"/>, which enables authentication capabilities.
+        /// </summary>
+        /// <param name="builder">The <see cref="IApplicationBuilder"/> to add the middleware to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UseEdFiApiAuthentication(this IApplicationBuilder builder)
+            => builder.UseMiddleware<EdFiApiAuthenticationMiddleware>();
     }
 }
