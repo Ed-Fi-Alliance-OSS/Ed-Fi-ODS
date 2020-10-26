16dd85649a753b5e3734d31d7fd4106073589d10
Vinaya <vinaya.mayya@ed-fi.org>
2020-10-25 21:27:55 -0500

    Updated student transportation artifacts

--- Samples/Extensions/StudentTransportation/StudentTransportationMetaEd/MetaEdOutput/SampleStudentTransportation/Database/PostgreSQL/ODS/Structure/Changes/0020-CreateChangeVersionSequence.sql
+++ Samples/Extensions/StudentTransportation/StudentTransportationMetaEd/MetaEdOutput/SampleStudentTransportation/Database/PostgreSQL/ODS/Structure/Changes/0020-CreateChangeVersionSequence.sql
@@ -5,7 +5,7 @@
 
 CREATE SEQUENCE IF NOT EXISTS changes.ChangeVersionSequence START WITH 1;
 
-CREATE FUNCTION changes.updateChangeVersion()
+CREATE OR REPLACE FUNCTION changes.updateChangeVersion()
     RETURNS trigger AS
 $BODY$
 BEGIN
