df55f2dc2ca0d1e2972fa0ba0af9dd28f71a0b6e
Jim McKay <jmckay@certicasolutions.com>
2020-10-26 07:51:58 -0500

    fixed formatting -- added comments to the class

--- Application/EdFi.Ods.Api/Middleware/EdFiApiAuthenticationMiddleware.cs
+++ Application/EdFi.Ods.Api/Middleware/EdFiApiAuthenticationMiddleware.cs
@@ -5,7 +5,6 @@
 
 using System;
 using System.Threading.Tasks;
-using EdFi.Ods.Common.Context;
 using EdFi.Ods.Common.Security;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
@@ -15,44 +14,43 @@ namespace EdFi.Ods.Api.Middleware
 {
     public class EdFiApiAuthenticationMiddleware
     {
-        private readonly RequestDelegate _next;
-        
         private readonly IApiKeyContextProvider _apiKeyContextProvider;
+        private readonly RequestDelegate _next;
 
         public EdFiApiAuthenticationMiddleware(RequestDelegate next, IAuthenticationSchemeProvider schemes,
             IApiKeyContextProvider apiKeyContextProvider)
         {
-            if (next == null)
+            if (schemes != null)
             {
-                throw new ArgumentNullException(nameof(next));
+                _next = next ?? throw new ArgumentNullException(nameof(next));
+                _apiKeyContextProvider = apiKeyContextProvider;
+
+                Schemes = schemes;
             }
-            
-            if (schemes == null)
+            else
             {
                 throw new ArgumentNullException(nameof(schemes));
             }
-
-            _next = next;
-            _apiKeyContextProvider = apiKeyContextProvider;
-            
-            Schemes = schemes;
         }
 
         public IAuthenticationSchemeProvider Schemes { get; set; }
 
         public async Task Invoke(HttpContext context)
         {
-            context.Features.Set<IAuthenticationFeature>(new AuthenticationFeature
-            {
-                OriginalPath = context.Request.Path,
-                OriginalPathBase = context.Request.PathBase
-            });
+            context.Features.Set<IAuthenticationFeature>(
+                new AuthenticationFeature
+                {
+                    OriginalPath = context.Request.Path,
+                    OriginalPathBase = context.Request.PathBase
+                });
 
             // Give any IAuthenticationRequestHandler schemes a chance to handle the request
             var handlers = context.RequestServices.GetRequiredService<IAuthenticationHandlerProvider>();
+
             foreach (var scheme in await Schemes.GetRequestHandlerSchemesAsync())
             {
                 var handler = await handlers.GetHandlerAsync(context, scheme.Name) as IAuthenticationRequestHandler;
+
                 if (handler != null && await handler.HandleRequestAsync())
                 {
                     return;
@@ -60,13 +58,17 @@ namespace EdFi.Ods.Api.Middleware
             }
 
             var defaultAuthenticate = await Schemes.GetDefaultAuthenticateSchemeAsync();
+
             if (defaultAuthenticate != null)
             {
                 var result = await context.AuthenticateAsync(defaultAuthenticate.Name);
+
                 if (result?.Principal != null)
                 {
                     context.User = result.Principal;
-                    
+
+                    // NOTE: For our use case we set the api key context into the call context storage. The rest of this code
+                    // is the default implementation and may need to be update on version updates.
                     _apiKeyContextProvider.SetApiKeyContext((ApiKeyContext) result.Ticket.Properties.Parameters["ApiKeyContext"]);
                 }
             }
