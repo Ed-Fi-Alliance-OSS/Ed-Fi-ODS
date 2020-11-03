2a053e042b09389df529186fbb87d72c6422ec85
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2020-11-03 16:05:28 -0600

    Moved updated TPDM authorization views to the extensions repository.

--- Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/1150-UpdatedAuthViewsToPreventDuplicatesFromStaffEdOrgAssignmentsAndEmployments.sql
+++ Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/1150-UpdatedAuthViewsToPreventDuplicatesFromStaffEdOrgAssignmentsAndEmployments.sql
@@ -104,46 +104,6 @@ AS
         INNER JOIN edfi.StaffEducationOrganizationAssignmentAssociation seo_assgn
             ON sch.SchoolId = seo_assgn.EducationOrganizationId;
 
-DROP VIEW IF EXISTS auth.StaffUSIToTeacherPreparationProviderId;
-
-CREATE VIEW auth.StaffUSIToTeacherPreparationProviderId
-AS
-    -- TeacherPreparationProvider to Staff (through TeacherPreparationProvider employment)
-    SELECT emp.EducationOrganizationId AS TeacherPreparationProviderId
-        ,emp.StaffUSI
-    FROM tpdm.TeacherPreparationProvider tpp
-        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
-            ON tpp.TeacherPreparationProviderId = emp.EducationOrganizationId
-
-    UNION
-
-    -- TeacherPreparationProvider to Staff (through TeacherPreparationProvider assignment)
-    SELECT assgn.EducationOrganizationId AS TeacherPreparationProviderId
-        ,assgn.StaffUSI
-    FROM tpdm.TeacherPreparationProvider tpp
-        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
-            ON tpp.TeacherPreparationProviderId = assgn.EducationOrganizationId;
-
-DROP VIEW IF EXISTS auth.StaffUSIToUniversityId;
-
-CREATE VIEW auth.StaffUSIToUniversityId
-AS
-    -- University to Staff (through University employment)
-    SELECT emp.EducationOrganizationId AS UniversityId
-        ,emp.StaffUSI
-    FROM tpdm.University u
-        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
-            ON u.UniversityId = emp.EducationOrganizationId
-
-    UNION
-
-    -- University to Staff (through University assignment)
-    SELECT assgn.EducationOrganizationId AS UniversityId
-        ,assgn.StaffUSI
-    FROM tpdm.University u
-        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
-            ON u.UniversityId = assgn.EducationOrganizationId;
-
 -- Recreate dependent views
 CREATE VIEW auth.EducationOrganizationIdToStaffUSI
 AS
