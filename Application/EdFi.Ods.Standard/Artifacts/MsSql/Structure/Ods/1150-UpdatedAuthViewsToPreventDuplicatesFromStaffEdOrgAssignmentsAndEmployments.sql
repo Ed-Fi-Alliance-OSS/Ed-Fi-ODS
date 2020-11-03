1a2b48498a4316c0a19e3f70fc803d61adcb48cd
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2020-11-03 13:52:37 -0600

    Fixed SQL Server authorization views around duplicates introduced due to usage of Staff employment/assignment relationships.

---
+++ Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/1150-UpdatedAuthViewsToPreventDuplicatesFromStaffEdOrgAssignmentsAndEmployments.sql
@@ -0,0 +1,148 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+PRINT N'Altering [auth].[CommunityProviderIdToStaffUSI]...';
+
+ALTER VIEW [auth].[CommunityProviderIdToStaffUSI]
+AS
+    -- CommunityProvider to Staff (through CommunityProvider employment)
+    SELECT emp.EducationOrganizationId AS CommunityProviderId
+        ,emp.StaffUSI
+    FROM edfi.CommunityProvider cp
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
+            ON cp.CommunityProviderId = emp.EducationOrganizationId
+
+    UNION
+
+    -- CommunityProvider to Staff (through CommunityProvider assignment)
+    SELECT assgn.EducationOrganizationId AS CommunityProviderId
+        ,assgn.StaffUSI
+    FROM edfi.CommunityProvider cp
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
+            ON cp.CommunityProviderId = assgn.EducationOrganizationId;
+GO
+
+PRINT N'Altering auth.LocalEducationAgencyIdToStaffUSI...';
+
+ALTER VIEW auth.LocalEducationAgencyIdToStaffUSI
+AS
+    -- LEA to Staff (through LEA employment)
+    SELECT emp.EducationOrganizationId AS LocalEducationAgencyId
+        ,emp.StaffUSI
+    FROM edfi.LocalEducationAgency lea
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
+            ON lea.LocalEducationAgencyId = emp.EducationOrganizationId
+
+    UNION
+
+    -- LEA to Staff (through LEA assignment)
+    SELECT assgn.EducationOrganizationId AS LocalEducationAgencyId
+         ,assgn.StaffUSI
+    FROM edfi.LocalEducationAgency lea
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
+            ON lea.LocalEducationAgencyId = assgn.EducationOrganizationId
+
+    UNION
+
+    -- LEA to Staff (through School employment)
+    SELECT sch.LocalEducationAgencyId
+         ,emp.StaffUSI
+    FROM edfi.School sch
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
+            ON sch.SchoolId = emp.EducationOrganizationId
+
+    UNION
+
+    -- LEA to Staff (through School assignment)
+    SELECT sch.LocalEducationAgencyId
+         ,assgn.StaffUSI
+    FROM edfi.School sch
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
+            ON sch.SchoolId = assgn.EducationOrganizationId;
+GO
+
+PRINT N'Altering [auth].[PostSecondaryInstitutionIdToStaffUSI]...';
+
+ALTER VIEW [auth].[PostSecondaryInstitutionIdToStaffUSI]
+AS
+    -- PostSecondaryInstitution to Staff (through PostSecondaryInstitution employment)
+    SELECT emp.EducationOrganizationId AS PostSecondaryInstitutionId
+        ,emp.StaffUSI
+    FROM edfi.PostSecondaryInstitution psi
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
+            ON psi.PostSecondaryInstitutionId = emp.EducationOrganizationId
+
+    UNION
+
+    -- PostSecondaryInstitution to Staff (through PostSecondaryInstitution assignment)
+    SELECT assgn.EducationOrganizationId AS PostSecondaryInstitutionId
+        ,assgn.StaffUSI
+    FROM edfi.PostSecondaryInstitution psi
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
+            ON psi.PostSecondaryInstitutionId = assgn.EducationOrganizationId;
+GO
+
+PRINT N'Altering [auth].[SchoolIdToStaffUSI]...';
+
+ALTER VIEW [auth].[SchoolIdToStaffUSI]
+AS
+    -- School to Staff (through School employment)
+    SELECT sch.SchoolId
+        ,seo_empl.StaffUSI
+    FROM edfi.School sch
+        INNER JOIN edfi.StaffEducationOrganizationEmploymentAssociation seo_empl
+            ON sch.SchoolId = seo_empl.EducationOrganizationId
+
+    UNION
+
+    -- School to Staff (through School assignment)
+    SELECT sch.SchoolId
+        ,seo_assgn.StaffUSI
+    FROM edfi.School sch
+        INNER JOIN edfi.StaffEducationOrganizationAssignmentAssociation seo_assgn
+            ON sch.SchoolId = seo_assgn.EducationOrganizationId;
+GO
+
+PRINT N'Altering [auth].[StaffUSIToTeacherPreparationProviderId]...';
+
+ALTER VIEW [auth].[StaffUSIToTeacherPreparationProviderId]
+AS
+    -- TeacherPreparationProvider to Staff (through TeacherPreparationProvider employment)
+    SELECT emp.EducationOrganizationId AS TeacherPreparationProviderId
+        ,emp.StaffUSI
+    FROM tpdm.TeacherPreparationProvider tpp
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
+            ON tpp.TeacherPreparationProviderId = emp.EducationOrganizationId
+
+    UNION
+
+    -- TeacherPreparationProvider to Staff (through TeacherPreparationProvider assignment)
+    SELECT assgn.EducationOrganizationId AS TeacherPreparationProviderId
+        ,assgn.StaffUSI
+    FROM tpdm.TeacherPreparationProvider tpp
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
+            ON tpp.TeacherPreparationProviderId = assgn.EducationOrganizationId;
+GO
+
+PRINT N'Altering [auth].[StaffUSIToUniversityId]...';
+
+ALTER VIEW [auth].[StaffUSIToUniversityId]
+AS
+    -- University to Staff (through University employment)
+    SELECT emp.EducationOrganizationId AS UniversityId
+        ,emp.StaffUSI
+    FROM tpdm.University u
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
+            ON u.UniversityId = emp.EducationOrganizationId
+
+    UNION
+
+    -- University to Staff (through University assignment)
+    SELECT assgn.EducationOrganizationId AS UniversityId
+        ,assgn.StaffUSI
+    FROM tpdm.University u
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
+            ON u.UniversityId = assgn.EducationOrganizationId;
+GO
