e77ede884159f9f24a3f8f0a1444770302a79f4e
Jim McKay <jmckay@certicasolutions.com>
2020-11-04 12:05:48 -0600

    fixed idempotent

--- Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/1160-UpdateVersionTo510.sql
+++ Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/1160-UpdateVersionTo510.sql
@@ -3,7 +3,7 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-IF object_id('GetEdFiOdsVersion', 'FN') IS NOT NULL
+IF object_id('util.GetEdFiOdsVersion', 'FN') IS NOT NULL
 BEGIN
     DROP FUNCTION util.GetEdFiOdsVersion;
 END
