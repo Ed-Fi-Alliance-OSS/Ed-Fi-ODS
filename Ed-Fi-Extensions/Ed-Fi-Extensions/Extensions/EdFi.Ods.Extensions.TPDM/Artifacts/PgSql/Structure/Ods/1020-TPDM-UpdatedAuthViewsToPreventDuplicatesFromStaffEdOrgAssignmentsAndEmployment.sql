ea823e9ae1e0f8c4dba475fbcbe2ef9d453a6b81
Geoff McElhanon <geoffrey@mcelhanon.com>
2020-11-04 11:30:23 -0600

    [ODS-4592] Duplicates from Security Views (#4)
    * Updated authorization views to prevent possible duplicates resulting from staff employment or assignment.
    
    * Added GO statements after PRINT statements to ensure ALTER VIEW statements are the first statement of the block.

---
+++ Ed-Fi-Extensions/Ed-Fi-Extensions/Extensions/EdFi.Ods.Extensions.TPDM/Artifacts/PgSql/Structure/Ods/1020-TPDM-UpdatedAuthViewsToPreventDuplicatesFromStaffEdOrgAssignmentsAndEmployment.sql
@@ -0,0 +1,45 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+DROP VIEW IF EXISTS auth.StaffUSIToTeacherPreparationProviderId;
+
+CREATE VIEW auth.StaffUSIToTeacherPreparationProviderId
+AS
+    -- TeacherPreparationProvider to Staff (through TeacherPreparationProvider employment)
+    SELECT emp.EducationOrganizationId AS TeacherPreparationProviderId
+        ,emp.StaffUSI
+    FROM tpdm.TeacherPreparationProvider tpp
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
+            ON tpp.TeacherPreparationProviderId = emp.EducationOrganizationId
+
+    UNION
+
+    -- TeacherPreparationProvider to Staff (through TeacherPreparationProvider assignment)
+    SELECT assgn.EducationOrganizationId AS TeacherPreparationProviderId
+        ,assgn.StaffUSI
+    FROM tpdm.TeacherPreparationProvider tpp
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
+            ON tpp.TeacherPreparationProviderId = assgn.EducationOrganizationId;
+
+DROP VIEW IF EXISTS auth.StaffUSIToUniversityId;
+
+CREATE VIEW auth.StaffUSIToUniversityId
+AS
+    -- University to Staff (through University employment)
+    SELECT emp.EducationOrganizationId AS UniversityId
+        ,emp.StaffUSI
+    FROM tpdm.University u
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Employment emp
+            ON u.UniversityId = emp.EducationOrganizationId
+
+    UNION
+
+    -- University to Staff (through University assignment)
+    SELECT assgn.EducationOrganizationId AS UniversityId
+        ,assgn.StaffUSI
+    FROM tpdm.University u
+        INNER JOIN auth.EducationOrganizationToStaffUSI_Assignment assgn
+            ON u.UniversityId = assgn.EducationOrganizationId;
+
