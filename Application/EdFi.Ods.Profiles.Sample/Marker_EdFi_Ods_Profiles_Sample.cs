e93a58309e8ee73f63b7e12caf154d917968553c
semalaiappan <34613894+semalaiappan@users.noreply.github.com>
2020-11-06 10:21:55 -0600

    [ODS-4628] Manual Profiles package and Documentation (#235)
    * Profile Plugin Support
    
    * Review comment fix
    
    * Update EdFi.Ods.Profiles.Sample.csproj
    
    * Code fix

--- Application/EdFi.Ods.Profiles.Sample/Marker_EdFi_Ods_Profiles_Sample.cs
+++ Application/EdFi.Ods.Profiles.Sample/Marker_EdFi_Ods_Profiles_Sample.cs
@@ -3,7 +3,9 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using EdFi.Ods.Common.Extensibility;
+
 namespace EdFi.Ods.Profiles.Sample
 {
-    public interface Marker_EdFi_Ods_Profiles_Sample { }
+    public interface Marker_EdFi_Ods_Profiles_Sample : IPluginMarker { }
 }
