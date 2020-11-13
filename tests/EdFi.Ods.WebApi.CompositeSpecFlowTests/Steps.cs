2c9da57e2e5992d89edbcf94582b03976ea81d9f
semalai <m.semalaiappan@gmail.com>
2020-11-13 14:16:27 -0600

    nuget package updated

--- tests/EdFi.Ods.WebApi.CompositeSpecFlowTests/Steps.cs
+++ tests/EdFi.Ods.WebApi.CompositeSpecFlowTests/Steps.cs
@@ -12,6 +12,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using ApprovalTests;
 using ApprovalTests.Reporters;
+using Castle.Core.Internal;
 using EdFi.Common.Inflection;
 using EdFi.Ods.Common;
 using EdFi.Ods.Common.Database;
@@ -560,7 +561,7 @@ namespace EdFi.Ods.WebApi.CompositeSpecFlowTests
             var id = await StepsHelper.GetAsync<Guid>(
                 _connectionStringProvider.Value.GetConnectionString(), query, _cancellationToken.Value);
 
-            id.ShouldNotBeNull();
+            id.ToString().IsNullOrEmpty().ShouldBeFalse();
 
             _scenarioContext.Set(id, ScenarioContextKeys.CompositeSubjectId);
         }
