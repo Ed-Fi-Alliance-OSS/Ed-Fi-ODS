bc3f92e97ace6ab7b1be610d36187ca8ce749238
Jim McKay <jmckay@certicasolutions.com>
2020-12-05 12:33:21 -0600

    added autofac to xmllookup

--- Utilities/DataLoading/EdFi.XmlLookup.Console/Program.cs
+++ Utilities/DataLoading/EdFi.XmlLookup.Console/Program.cs
@@ -4,22 +4,13 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Xml.Linq;
+using Autofac;
 using EdFi.LoadTools;
-using EdFi.LoadTools.ApiClient;
 using EdFi.LoadTools.Engine;
-using EdFi.LoadTools.Engine.Factories;
-using EdFi.LoadTools.Engine.Mapping;
-using EdFi.LoadTools.Engine.MappingFactories;
-using EdFi.LoadTools.Engine.XmlLookupPipeline;
 using EdFi.XmlLookup.Console.Application;
 using log4net;
 using log4net.Config;
-using SimpleInjector;
 
 namespace EdFi.XmlLookup.Console
 {
@@ -55,32 +46,45 @@ namespace EdFi.XmlLookup.Console
             {
                 try
                 {
-                    using (var container = new Container())
-                    {
-                        LogConfiguration(p.Object);
+                    var container = RegisterContainer();
+                    using var scope = container.BeginLifetimeScope();
 
-                        // configure DI container
-                        ConfigureCompositionRoot(container, p.Object);
+                    LogConfiguration(p.Object);
 
-                        // retrieve application
-                        var application = container.GetInstance<XmlLookupApplication>();
+                    // configure DI container
 
-                        // run application 
-                        exitCode = application.Run().Result;
-                    }
+                    // retrieve application
+                    var application = container.Resolve<XmlLookupApplication>();
+
+                    // run application
+                    exitCode = application.Run().Result;
                 }
-                catch (AggregateException ex)
+                catch (Exception ex)
                 {
                     exitCode = 1;
-
-                    foreach (var e in ex.InnerExceptions)
-                    {
-                        Log.Error(e);
-                    }
+                    Log.Error(ex);
                 }
             }
 
             Environment.Exit(exitCode);
+
+            ILifetimeScope RegisterContainer()
+            {
+                var builder = new ContainerBuilder();
+
+                builder.RegisterInstance(p.Object)
+                    .As<IApiConfiguration>()
+                    .As<IDataConfiguration>()
+                    .As<IOAuthTokenConfiguration>()
+                    .As<IApiMetadataConfiguration>()
+                    .As<IXsdConfiguration>()
+                    .AsSelf()
+                    .SingleInstance();
+
+                builder.RegisterModule(new XmlLookupModule());
+
+                return builder.Build();
+            }
         }
 
         private static void LogConfiguration(Configuration configuration)
@@ -92,70 +96,6 @@ namespace EdFi.XmlLookup.Console
             Log.Info($"Data Folder:    {configuration.DataFolder}");
             Log.Info($"Working Folder: {configuration.WorkingFolder}");
             Log.Info($"Xsd Folder:     {configuration.XsdFolder}");
-        }
-
-        private static void ConfigureCompositionRoot(Container container, Configuration configuration)
-        {
-            container.RegisterSingleton<IApiConfiguration>(() => configuration);
-            container.RegisterSingleton<IApiMetadataConfiguration>(() => configuration);
-            container.RegisterSingleton<IDataConfiguration>(() => configuration);
-            container.RegisterSingleton<IOAuthTokenConfiguration>(() => configuration);
-            container.RegisterSingleton<IXsdConfiguration>(() => configuration);
-
-            container.RegisterSingleton<SwaggerMetadataRetriever>();
-            container.RegisterSingleton<SwaggerRetriever>();
-            container.RegisterSingleton<XsdStreamsRetriever>();
-
-            container.RegisterSingleton<NameMatchingMetadataMapper>();
-
-            container.RegisterSingleton<IXmlPairsFactory, XmlIoFactory>();
-            container.RegisterSingleton<IMetadataFactory<JsonModelMetadata>, JsonMetadataFactory>();
-            container.RegisterSingleton<IMetadataFactory<XmlModelMetadata>, XsdMetadataFactory>();
-            container.RegisterSingleton<IHashProvider, HashProvider>();
-            container.RegisterSingleton<ITokenRetriever, TokenRetriever>();
-
-            container.RegisterSingleton(() => container.GetInstance<SchemaSetFactory>().GetSchemaSet());
-
-            container.RegisterSingleton<IDictionary<string, XElement>>(
-                () =>
-                    new ConcurrentDictionary<string, XElement>());
-
-            container.RegisterSingleton<IEnumerable<JsonModelMetadata>>(
-                () => container.GetInstance<IMetadataFactory<JsonModelMetadata>>().GetMetadata().ToArray());
-
-            container.RegisterSingleton<IEnumerable<XmlModelMetadata>>(
-                () => container.GetInstance<IMetadataFactory<XmlModelMetadata>>().GetMetadata().ToArray());
-
-            container.Collection.Register<ILookupPipelineStep>(
-                new[]
-                {
-                    typeof(IdentifyResourceTypeStep), typeof(ComputeHashStep), typeof(DirectLookupToIdentityMappingStep),
-                    typeof(AvoidDuplicateLookupsStep), typeof(EducationOrganizationCacheLookupStep), typeof(MapXmlLookupToGetByExampleJsonStep),
-                    typeof(PerformGetByExampleStep), typeof(MapResourceToIdentityStep), typeof(StoreIdentityForWritingStep)
-                });
-
-            container.Register<IEducationOrganizationIdentityCache, EducationOrganizationIdentityCache>();
-
-            container.RegisterConditional<IMetadataMappingFactory, LookupToGetByExampleMetadataMappingFactory>(
-                Lifestyle.Singleton,
-                c => c.Consumer.ImplementationType == typeof(MapXmlLookupToGetByExampleJsonStep));
-
-            container.RegisterConditional<IMetadataMappingFactory, ResourceToIdentityMetadataMappingFactory>(
-                Lifestyle.Singleton,
-                c => c.Consumer.ImplementationType == typeof(MapResourceToIdentityStep));
-
-            container.RegisterConditional<IMetadataMappingFactory, LookupToIdentityMetadataMappingFactory>(
-                Lifestyle.Singleton,
-                c => c.Consumer.ImplementationType == typeof(DirectLookupToIdentityMappingStep));
-
-            container.Collection.Register<IMetadataMapper>(
-                new[]
-                {
-                    typeof(PremappedLookupMetadataMapper), typeof(LookupDoNotMapPropertyMapper), typeof(DescriptorReferenceMetadataMapper),
-                    typeof(NameMatchingMetadataMapper)
-                });
-
-            container.Verify();
         }
 
         /// <summary>
