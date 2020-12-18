aff8675c1b6b0d944494068b5ab307549fb23604
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2020-12-18 16:39:36 -0600

    Semantic model enhancement: Added missing namespace usages.

--- Application/EdFi.Ods.Common/Models/Resource/ProfileResourceModel.cs
+++ Application/EdFi.Ods.Common/Models/Resource/ProfileResourceModel.cs
@@ -8,11 +8,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Xml.Linq;
+using EdFi.Common.Extensions;
 using EdFi.Common.Utils.Extensions;
 using EdFi.Ods.Common.Conventions;
 using EdFi.Ods.Common.Extensions;
 using EdFi.Ods.Common.Models.Domain;
-using EdFi.Ods.Common.Utils.Extensions;
 
 namespace EdFi.Ods.Common.Models.Resource
 {
--- Application/EdFi.Ods.Common/Models/Resource/ResourceSelector.cs
+++ Application/EdFi.Ods.Common/Models/Resource/ResourceSelector.cs
@@ -5,6 +5,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using EdFi.Common.Extensions;
 using EdFi.Ods.Common.Extensions;
 using EdFi.Ods.Common.Models.Domain;
 
