ce76fcbd14742c859b71af334c3f81491ada115f
Jim McKay <james.mckay@instructure.com>
2021-01-13 08:13:38 -0600

    added xsdMetadata feature

---
+++ Application/EdFi.Ods.Features/XsdMetadata/XsdMetadataModule.cs
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using Autofac;
+using EdFi.Ods.Api.Middleware;
+using EdFi.Ods.Api.Providers;
+using EdFi.Ods.Common.Configuration;
+using EdFi.Ods.Common.Constants;
+using EdFi.Ods.Common.Container;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace EdFi.Ods.Features.XsdMetadata
+{
+    public class XsdMetadataModule : ConditionalModule
+    {
+        public XsdMetadataModule(ApiSettings apiSettings)
+            : base(apiSettings, nameof(XsdMetadataModule)) { }
+
+        public override bool IsSelected() => IsFeatureEnabled(ApiFeature.XsdMetadata);
+
+        public override void ApplyConfigurationSpecificRegistrations(ContainerBuilder builder)
+        {
+            builder.RegisterType<XsdFileInformationProvider>()
+                .As<IXsdFileInformationProvider>()
+                .SingleInstance();
+
+            builder.RegisterType<XsdMetadataRouteConvention>()
+                .As<IApplicationModelConvention>()
+                .SingleInstance();
+
+            builder.RegisterType<XsdMetadataFileMiddleware>()
+                .As<IMiddleware>()
+                .AsSelf()
+                .SingleInstance();
+        }
+    }
+}
---
+++ Application/EdFi.Ods.Features/XsdMetadata/XsdMetadataRouteConvention.cs
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Linq;
+using System.Reflection;
+using EdFi.Common.Configuration;
+using EdFi.Ods.Api.Constants;
+using EdFi.Ods.Common.Configuration;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace EdFi.Ods.Features.XsdMetadata
+{
+    public class XsdMetadataRouteConvention : IApplicationModelConvention
+    {
+        private readonly ApiSettings _apiSettings;
+
+        public XsdMetadataRouteConvention(ApiSettings apiSettings)
+        {
+            _apiSettings = apiSettings;
+        }
+
+        public void Apply(ApplicationModel application)
+        {
+            var controller =
+                application.Controllers.FirstOrDefault(x => x.ControllerType == typeof(XsdMetadataController).GetTypeInfo());
+
+            if (controller != null)
+            {
+                var routePrefix = new AttributeRouteModel {Template = CreateRouteTemplate()};
+
+                foreach (var selector in controller.Selectors)
+                {
+                    if (selector.AttributeRouteModel != null)
+                    {
+                        selector.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(
+                            routePrefix,
+                            selector.AttributeRouteModel);
+                    }
+                }
+            }
+
+            string CreateRouteTemplate()
+            {
+                string template = $"metadata/";
+
+                if (_apiSettings.GetApiMode() == ApiMode.YearSpecific)
+                {
+                    template += RouteConstants.SchoolYearFromRoute;
+                }
+
+                if (_apiSettings.GetApiMode() == ApiMode.InstanceYearSpecific)
+                {
+                    template += RouteConstants.InstanceIdFromRoute;
+                    template += RouteConstants.SchoolYearFromRoute;
+                }
+
+                return template;
+            }
+        }
+    }
+}
