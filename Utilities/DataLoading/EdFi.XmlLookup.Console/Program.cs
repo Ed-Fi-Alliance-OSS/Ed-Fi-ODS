af4fae356f033055a48b764bad037658612ba0f7
Raghu Ramakrishna <rramakrishna@inniveinc.com>
2021-02-02 22:06:46 +0530

    removed TLS settings from LoadTools

--- Utilities/DataLoading/EdFi.XmlLookup.Console/Program.cs
+++ Utilities/DataLoading/EdFi.XmlLookup.Console/Program.cs
@@ -26,7 +26,6 @@ namespace EdFi.XmlLookup.Console
         private static async Task Main(string[] args)
         {
             ConfigureLogging();
-            ConfigureTls();
 
             CommandLineOverrides commandLineOverrides = null;
 
@@ -166,16 +165,6 @@ namespace EdFi.XmlLookup.Console
             Log.Info($"Data Folder:    {xmlLookupConfiguration.DataFolder}");
             Log.Info($"Working Folder: {xmlLookupConfiguration.WorkingFolder}");
             Log.Info($"Xsd Folder:     {xmlLookupConfiguration.XsdFolder}");
-        }
-
-        /// <summary>
-        /// Explicitly configures all outgoing network calls to use the latest version of TLS where possible.
-        /// c.f https://docs.microsoft.com/en-us/dotnet/framework/network-programming/tls
-        /// </summary>
-        private static void ConfigureTls()
-        {
-            // TLS 1.2 is not available by default for version of the frameworks less that .NET 4.6.2
-            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
         }
     }
 }
