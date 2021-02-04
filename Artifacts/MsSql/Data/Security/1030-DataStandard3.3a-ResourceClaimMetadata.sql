ad978d7712dd5f5059b3b938afe6c70403dfded6
semalai <m.semalaiappan@gmail.com>
2021-02-04 15:36:29 -0600

    Integrate Latest 3.3.0-a Data Standard Updated

--- Artifacts/MsSql/Data/Security/1030-DataStandard3.3a-ResourceClaimMetadata.sql
+++ Artifacts/MsSql/Data/Security/1030-DataStandard3.3a-ResourceClaimMetadata.sql
@@ -3,8 +3,6 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-DECLARE @ClaimSetId INT;
-DECLARE @AuthorizationStrategyId INT;
 DECLARE @applicationId INT;
 DECLARE @systemDescriptorsResourceClaimId INT;
 DECLARE @relationshipBasedDataResourceClaimId INT;
