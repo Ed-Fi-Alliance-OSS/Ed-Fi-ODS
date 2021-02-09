e44fdf6fcf0934acd578d42b46317ded190af93f
travis.meador <t.meador@wearedoubleline.com>
2021-02-09 14:12:15 -0600

    move null handling to provider

--- Utilities/CodeGeneration/EdFi.Ods.CodeGen/Providers/Impl/ExtensionLocationPluginsProvider.cs
+++ Utilities/CodeGeneration/EdFi.Ods.CodeGen/Providers/Impl/ExtensionLocationPluginsProvider.cs
@@ -3,18 +3,21 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System;
+using System.Collections.Generic;
+
 namespace EdFi.Ods.CodeGen.Providers.Impl
 {
     public class ExtensionLocationPluginsProvider : IExtensionLocationPluginsProvider
     {
-        private readonly string[] _extensionPaths;
+        private readonly IEnumerable<string> _extensionPaths;
 
-        public ExtensionLocationPluginsProvider(string[] extensionPaths)
+        public ExtensionLocationPluginsProvider(IEnumerable<string> extensionPaths)
         {
-            _extensionPaths = extensionPaths;
+            _extensionPaths = extensionPaths ?? Array.Empty<string>();
         }
 
-        public string[] GetExtensionLocationPlugins()
+        public IEnumerable<string> GetExtensionLocationPlugins()
         {
             return _extensionPaths;
         }
