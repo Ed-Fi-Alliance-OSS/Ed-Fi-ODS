e1c1272edfb6c47399d1b884b63d9ff3f6a6665a
semalai <m.semalaiappan@gmail.com>
2021-02-13 15:09:32 -0600

    formatting

--- Artifacts/PgSql/Data/Security/1030-DataStandard3.3a-ResourceClaimMetadata.sql
+++ Artifacts/PgSql/Data/Security/1030-DataStandard3.3a-ResourceClaimMetadata.sql
@@ -8,46 +8,46 @@ DECLARE
 application_id INT;
 systemDescriptorsResourceClaim_Id INT;
 relationshipBasedDataResourceClaim_Id INT;
-	
+    
 BEGIN
 
     IF  EXISTS (SELECT 1 FROM  dbo.Applications  WHERE  ApplicationName  = 'Ed-Fi ODS API')
     THEN
-	    SELECT applicationid INTO application_id
-	    FROM  dbo.Applications  WHERE  ApplicationName  = 'Ed-Fi ODS API';
+        SELECT applicationid INTO application_id
+        FROM  dbo.Applications  WHERE  ApplicationName  = 'Ed-Fi ODS API';
     END IF;
 
     IF  EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ResourceName = 'systemDescriptors' AND Application_ApplicationId = application_id)
     THEN
-	    SELECT ResourceClaimId INTO systemDescriptorsResourceClaim_Id
-	    FROM dbo.ResourceClaims WHERE ResourceName = 'systemDescriptors' AND Application_ApplicationId = application_id;
+        SELECT ResourceClaimId INTO systemDescriptorsResourceClaim_Id
+        FROM dbo.ResourceClaims WHERE ResourceName = 'systemDescriptors' AND Application_ApplicationId = application_id;
     END IF;
 
     IF  EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ResourceName = 'relationshipBasedData' AND Application_ApplicationId = application_id)
     THEN
-	    SELECT ResourceClaimId INTO relationshipBasedDataResourceClaim_Id
-	    FROM dbo.ResourceClaims WHERE ResourceName = 'systemDescriptors' AND Application_ApplicationId = application_id;
+        SELECT ResourceClaimId INTO relationshipBasedDataResourceClaim_Id
+        FROM dbo.ResourceClaims WHERE ResourceName = 'systemDescriptors' AND Application_ApplicationId = application_id;
     END IF;
 
-        /* new descriptors */
-        IF (NOT EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ResourceName = 'ancestryEthnicOriginDescriptor' AND Application_ApplicationId = application_id))
-        THEN
+    /* new descriptors */
+    IF (NOT EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ResourceName = 'ancestryEthnicOriginDescriptor' AND Application_ApplicationId = application_id))
+    THEN
         INSERT INTO dbo.ResourceClaims (DisplayName, ResourceName, ClaimName, ParentResourceClaimId, Application_ApplicationId)
         VALUES (N'ancestryEthnicOriginDescriptor', N'ancestryEthnicOriginDescriptor', N'http://ed-fi.org/ods/identity/claims/ancestryEthnicOriginDescriptor', systemDescriptorsResourceClaim_Id, application_id);
-        END IF;
-	
-        -- Try insert StudentDisciplineIncidentBehaviorAssociation
-        IF (NOT EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ResourceName = 'studentDisciplineIncidentBehaviorAssociation' AND Application_ApplicationId = application_id))
-        THEN
+    END IF;
+    
+    -- Try insert StudentDisciplineIncidentBehaviorAssociation
+    IF (NOT EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ResourceName = 'studentDisciplineIncidentBehaviorAssociation' AND Application_ApplicationId = application_id))
+    THEN
         INSERT INTO dbo.ResourceClaims (DisplayName, ResourceName, ClaimName, ParentResourceClaimId, Application_ApplicationId)
         VALUES (N'studentDisciplineIncidentBehaviorAssociation', N'studentDisciplineIncidentBehaviorAssociation', N'http://ed-fi.org/ods/identity/claims/studentDisciplineIncidentBehaviorAssociation', relationshipBasedDataResourceClaim_Id, application_id);
-        END IF;
-	
-        -- Try insert StudentDisciplineIncidentNonOffenderAssociation
-        IF (NOT EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ResourceName = 'studentDisciplineIncidentNonOffenderAssociation' AND Application_ApplicationId = application_id))
-        THEN
+    END IF;
+    
+    -- Try insert StudentDisciplineIncidentNonOffenderAssociation
+    IF (NOT EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ResourceName = 'studentDisciplineIncidentNonOffenderAssociation' AND Application_ApplicationId = application_id))
+    THEN
         INSERT INTO dbo.ResourceClaims (DisplayName, ResourceName, ClaimName, ParentResourceClaimId, Application_ApplicationId)
         VALUES (N'studentDisciplineIncidentNonOffenderAssociation', N'studentDisciplineIncidentNonOffenderAssociation', N'http://ed-fi.org/ods/identity/claims/studentDisciplineIncidentNonOffenderAssociation', relationshipBasedDataResourceClaim_Id, application_id);
-        END IF;
+    END IF;
 
 END $$;
