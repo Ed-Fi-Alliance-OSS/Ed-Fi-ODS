67e162865ecc5e85fd58723f91365a7dd7910074
semalai <m.semalaiappan@gmail.com>
2021-02-13 21:08:34 -0600

    review comments fix

--- Artifacts/MsSql/Data/Security/1060-RemoveRedundantBootstrapPermissions.sql
+++ Artifacts/MsSql/Data/Security/1060-RemoveRedundantBootstrapPermissions.sql
@@ -1,45 +1,65 @@
--- SPDX-License-Identifier: Apache-2.0
--- Licensed to the Ed-Fi Alliance under one or more agreements.
--- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
--- See the LICENSE and NOTICES files in the project root for more information.
+    -- SPDX-License-Identifier: Apache-2.0
+    -- Licensed to the Ed-Fi Alliance under one or more agreements.
+    -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+    -- See the LICENSE and NOTICES files in the project root for more information.
 
-DECLARE @claim_Name NVARCHAR(MAX) = 'http://ed-fi.org/ods/identity/claims/domains/educationOrganizations';
-DECLARE @educationOrganizationsResourceClaimId INT;
-DECLARE @claimSet_Id INT;
-DECLARE @claimSet_Name NVARCHAR(MAX) = 'Bootstrap Descriptors and EdOrgs';
-DECLARE @msg NVARCHAR(MAX);
-IF NOT EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ClaimName = @claim_Name)    
-BEGIN
-	SET @msg = 'ClaimName does not exist';
-    THROW 50000, @msg, 1
-END
+    DECLARE @claim_Name NVARCHAR(MAX) = 'http://ed-fi.org/ods/identity/claims/domains/educationOrganizations';
+    DECLARE @claimSet_Name NVARCHAR(MAX) = 'Bootstrap Descriptors and EdOrgs';
+    DECLARE @educationOrganizationsResourceClaimId INT;
+    DECLARE @claimSet_Id INT;
+    DECLARE @msg NVARCHAR(MAX);
+    DECLARE @messageText NVARCHAR(MAX);
+    DECLARE @createActionId INT;
+    DECLARE @authorizationStrategyId INT;
 
-IF NOT EXISTS (SELECT 1 FROM dbo.ClaimSets WHERE ClaimSetName = @claimSet_Name)    
-BEGIN
-   SET @msg = 'ClaimSetName does not exist';
-   THROW 50000, @msg, 1
-END
+    SELECT @authorizationStrategyId=AuthorizationStrategyId FROM dbo.AuthorizationStrategies where DisplayName='No Further Authorization Required'
+    SELECT @createActionId =ActionId FROM dbo.Actions where ActionName='Create'
 
-IF  EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ClaimName = @claim_Name)    
-BEGIN
-	SELECT	@educationOrganizationsResourceClaimId = ResourceClaimId
-	FROM	dbo.ResourceClaims rc
-	WHERE	rc.ClaimName = @claim_Name
-END
+    --New message for claimname does not exist error
+    EXEC sys.sp_addmessage
+    @msgnum = 50001,
+    @severity = 1,
+    @msgtext = N'ClaimName ''%s'' does not exist.',
+    @with_log = true,
+    @replace = 'REPLACE',
+    @lang = 'us_english';
 
-IF  EXISTS (SELECT 1 FROM dbo.ClaimSets WHERE ClaimSetName = @claimSet_Name)    
-BEGIN
-	SELECT	@claimSet_Id = claimSetId
-	FROM	dbo.ClaimSets rc
-	WHERE	rc.ClaimSetName = @claimSet_Name
-END
+    IF NOT  EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ClaimName = @claim_Name)
+    BEGIN
+        SET @messageText =  FORMATMESSAGE(50001,@claim_Name);
+        THROW 50001, @messageText, 1
+    END
 
+    --New message for ClaimSetName does not exist error
+    EXEC sys.sp_addmessage
+    @msgnum = 50002,
+    @severity = 1,
+    @msgtext = N'ClaimSetName ''%s'' does not exist.',
+    @with_log = true,
+    @replace = 'REPLACE',
+    @lang = 'us_english';
 
-DELETE FROM dbo.ClaimSetResourceClaims 
-WHERE  ClaimSetResourceClaimId IN (SELECT ClaimSetResourceClaimId
-FROM   dbo.ClaimSetResourceClaims csrc
-WHERE  csrc.ClaimSet_ClaimSetId = @claimSet_Id
-AND csrc.ResourceClaim_ResourceClaimId IN 
-(SELECT  ResourceClaimId
-FROM	dbo.ResourceClaims rc
-WHERE	rc.ParentResourceClaimId = @educationOrganizationsResourceClaimId))
+    IF NOT  EXISTS (SELECT 1 FROM dbo.ClaimSets WHERE ClaimSetName = @claimSet_Name)
+    BEGIN
+        SET @messageText =  FORMATMESSAGE(50002,@claimSet_Name);
+        THROW 50002, @messageText, 1
+    END
+
+    SELECT	@educationOrganizationsResourceClaimId = ResourceClaimId
+    FROM	dbo.ResourceClaims rc
+    WHERE	rc.ClaimName = @claim_Name
+
+    SELECT	@claimSet_Id = claimSetId
+    FROM	dbo.ClaimSets rc
+    WHERE	rc.ClaimSetName = @claimSet_Name
+
+    DELETE FROM dbo.ClaimSetResourceClaims 
+    WHERE  ClaimSetResourceClaimId IN (SELECT ClaimSetResourceClaimId
+    FROM   dbo.ClaimSetResourceClaims csrc
+    WHERE  csrc.ClaimSet_ClaimSetId = @claimSet_Id
+    AND csrc.Action_ActionId = @createActionId
+    AND csrc.AuthorizationStrategyOverride_AuthorizationStrategyId = @authorizationStrategyId
+    AND csrc.ResourceClaim_ResourceClaimId IN 
+    (SELECT  ResourceClaimId
+    FROM	dbo.ResourceClaims rc
+    WHERE	rc.ParentResourceClaimId = @educationOrganizationsResourceClaimId))
--- Artifacts/PgSql/Data/Security/1060-RemoveRedundantBootstrapPermissions.sql
+++ Artifacts/PgSql/Data/Security/1060-RemoveRedundantBootstrapPermissions.sql
@@ -1,49 +1,50 @@
--- SPDX-License-Identifier: Apache-2.0
--- Licensed to the Ed-Fi Alliance under one or more agreements.
--- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
--- See the LICENSE and NOTICES files in the project root for more information.
+    -- SPDX-License-Identifier: Apache-2.0
+    -- Licensed to the Ed-Fi Alliance under one or more agreements.
+    -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+    -- See the LICENSE and NOTICES files in the project root for more information.
 
-DO language plpgsql $$
-DECLARE
+    DO language plpgsql $$
+    DECLARE
+    claim_Name VARCHAR(250) := 'http://ed-fi.org/ods/identity/claims/domains/educationOrganizations';
+    claimSet_Name VARCHAR(250) := 'Bootstrap Descriptors and EdOrgs';
     educationOrganizationsResourceClaimId INT;
-	claimSet_Id INT;
-	claim_Name VARCHAR(250) := 'http://ed-fi.org/ods/identity/claims/domains/educationOrganizations';
-	claimSet_Name VARCHAR(250) := 'Bootstrap Descriptors and EdOrgs';	
-BEGIN
-   
-   IF NOT EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ClaimName = claim_Name)
-   THEN
+    claimSet_Id INT;
+    createAction_Id INT;
+    authorizationStrategy_Id INT;
+
+    BEGIN
+
+    SELECT AuthorizationStrategyId INTO authorizationStrategy_Id FROM dbo.AuthorizationStrategies where DisplayName='No Further Authorization Required';
+    SELECT ActionId INTO createAction_Id FROM dbo.Actions where ActionName='Create';
+
+    IF NOT EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ClaimName = claim_Name)
+    THEN
         RAISE NOTICE '% does not exist', claim_Name;
-   END IF;
-   
-   	IF NOT EXISTS (SELECT 1 FROM dbo.ClaimSets WHERE ClaimSetName = claimSet_Name)
-   	THEN
-       	RAISE NOTICE '% does not exist', claimSet_Name;
-   	END IF;
-	
-   IF EXISTS (SELECT 1 FROM dbo.ResourceClaims WHERE ClaimName = claim_Name)
-   	THEN
-        SELECT ResourceClaimId INTO educationOrganizationsResourceClaimId
-        FROM dbo.ResourceClaims
-        WHERE ClaimName = claim_Name;
-		
-   		IF EXISTS (SELECT 1 FROM dbo.ClaimSets WHERE ClaimSetName = claimSet_Name)
-    	 THEN
-        	SELECT claimSetId INTO claimSet_Id
-        	FROM dbo.ClaimSets
-        	WHERE ClaimSetName = claimSet_Name;
-   		 END IF;
-		
-		DELETE FROM dbo.ClaimSetResourceClaims 
-		WHERE ClaimSetResourceClaimId IN  
-		(SELECT ClaimSetResourceClaimId
-		FROM dbo.ClaimSetResourceClaims 
-		WHERE ClaimSet_ClaimSetId = claimSet_Id
-		AND ResourceClaim_ResourceClaimId IN 
-		(SELECT  ResourceClaimId
-		FROM dbo.ResourceClaims 
-		WHERE ParentResourceClaimId = educationOrganizationsResourceClaimId));  
-		 
-   END IF;
-   
-END $$;
+    END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM dbo.ClaimSets WHERE ClaimSetName = claimSet_Name)
+    THEN
+        RAISE NOTICE '% does not exist', claimSet_Name;
+    END IF;
+
+    SELECT ResourceClaimId INTO educationOrganizationsResourceClaimId
+    FROM dbo.ResourceClaims
+    WHERE ClaimName = claim_Name;
+
+    SELECT claimSetId INTO claimSet_Id
+    FROM dbo.ClaimSets
+    WHERE ClaimSetName = claimSet_Name;
+
+    DELETE FROM dbo.ClaimSetResourceClaims 
+    WHERE ClaimSetResourceClaimId IN  
+    (SELECT ClaimSetResourceClaimId
+    FROM dbo.ClaimSetResourceClaims 
+    WHERE ClaimSet_ClaimSetId = claimSet_Id
+    AND Action_ActionId = createAction_Id
+    AND AuthorizationStrategyOverride_AuthorizationStrategyId = authorizationStrategy_Id
+    AND ResourceClaim_ResourceClaimId IN 
+    (SELECT  ResourceClaimId
+    FROM dbo.ResourceClaims 
+    WHERE ParentResourceClaimId = educationOrganizationsResourceClaimId));
+
+    END $$;
