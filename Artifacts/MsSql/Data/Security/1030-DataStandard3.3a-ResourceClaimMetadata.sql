fe147be8e7eb0ac3a4b5d10e8c214149e783d818
semalai <m.semalaiappan@gmail.com>
2021-02-23 14:25:22 -0600

    Added No Further Authorization Required for organizationDepartment

--- Artifacts/MsSql/Data/Security/1030-DataStandard3.3a-ResourceClaimMetadata.sql
+++ Artifacts/MsSql/Data/Security/1030-DataStandard3.3a-ResourceClaimMetadata.sql
@@ -6,10 +6,16 @@
     DECLARE @applicationId INT;
     DECLARE @systemDescriptorsResourceClaimId INT;
     DECLARE @relationshipBasedDataResourceClaimId INT;
+    DECLARE @authorizationStrategyId INT;
+    DECLARE @assessmentMetadataResourceClaimId INT;
+    DECLARE @claimSetId INT;
 
     SELECT @applicationId = (SELECT applicationid FROM  dbo.Applications  WHERE  ApplicationName  = 'Ed-Fi ODS API');
     SELECT @systemDescriptorsResourceClaimId = (SELECT ResourceClaimId FROM dbo.ResourceClaims WHERE ResourceName = 'systemDescriptors' AND Application_ApplicationId = @applicationId);
     SELECT @relationshipBasedDataResourceClaimId = (SELECT ResourceClaimId FROM dbo.ResourceClaims WHERE ResourceName = 'relationshipBasedData' AND Application_ApplicationId = @applicationId);
+    SELECT @assessmentMetadataResourceClaimId = (SELECT ResourceClaimId FROM dbo.ResourceClaims WHERE ResourceName = 'assessmentMetadata' AND Application_ApplicationId = @applicationId);
+    SELECT @authorizationStrategyId= (SELECT AuthorizationStrategyId FROM dbo.AuthorizationStrategies WHERE DisplayName='No Further Authorization Required');
+    SELECT @claimSetId= (SELECT ClaimSetId  FROM dbo.ClaimSets where ClaimSetName='Bootstrap Descriptors and EdOrgs');
 
     /* new descriptors */
     IF (NOT EXISTS (SELECT ResourceClaimId FROM dbo.ResourceClaims WHERE ResourceName = 'ancestryEthnicOriginDescriptor' AND Application_ApplicationId = @applicationId))
@@ -36,7 +42,7 @@
     IF (NOT EXISTS (SELECT ResourceClaimId FROM dbo.ResourceClaims WHERE ResourceName = 'assessmentScoreRangeLearningStandard' AND Application_ApplicationId = @applicationId))
     BEGIN
         INSERT [dbo].[ResourceClaims] ([DisplayName], [ResourceName], [ClaimName], [ParentResourceClaimId], [Application_ApplicationId])
-        VALUES (N'assessmentScoreRangeLearningStandard', N'assessmentScoreRangeLearningStandard', N'http://ed-fi.org/ods/identity/claims/assessmentScoreRangeLearningStandard', @relationshipBasedDataResourceClaimId, @applicationId);
+        VALUES (N'assessmentScoreRangeLearningStandard', N'assessmentScoreRangeLearningStandard', N'http://ed-fi.org/ods/identity/claims/assessmentScoreRangeLearningStandard', @assessmentMetadataResourceClaimId, @applicationId);
     END
 
     -- Try insert OrganizationDepartment
@@ -46,3 +52,11 @@
         VALUES (N'organizationDepartment', N'organizationDepartment', N'http://ed-fi.org/ods/identity/claims/organizationDepartment', @relationshipBasedDataResourceClaimId, @applicationId);
     END
 
+    --Apply  NofurhterauthorizationRequired on this OrganizationDepartment resource
+    INSERT INTO  dbo.ClaimSetResourceClaims( Action_ActionId , ClaimSet_ClaimSetId , ResourceClaim_ResourceClaimId , AuthorizationStrategyOverride_AuthorizationStrategyId , ValidationRuleSetNameOverride )
+    SELECT ac.ActionId, @claimSetId, ResourceClaimId, @authorizationStrategyId, null
+    FROM [dbo].[ResourceClaims]
+    CROSS APPLY
+    (SELECT ActionId  FROM [dbo].[Actions]
+    WHERE ActionName IN ('Create','Read','Update','Delete')) AS ac
+    WHERE ResourceName = 'organizationDepartment';
