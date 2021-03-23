60af79506d32aaa09cf11d7d0b720ae0c81f80d9
travis.meador <t.meador@wearedoubleline.com>
2021-03-23 15:17:44 -0500

    enable appsettings support

--- Utilities/DataLoading/EdFi.SmokeTest.Console/Application/SmokeTestsConfiguration.cs
+++ Utilities/DataLoading/EdFi.SmokeTest.Console/Application/SmokeTestsConfiguration.cs
@@ -135,7 +135,7 @@ namespace EdFi.SmokeTest.Console.Application
                 OAuthKey = configuration.GetValue<string>("OdsApi:Key"),
                 OAuthSecret = configuration.GetValue<string>("OdsApi:Secret"),
                 SchoolYear = configuration.GetValue<int?>("OdsApi:SchoolYear"),
-                NamespacePrefix = string.IsNullOrEmpty(configuration.GetValue<string>("NamespacePrefix")) ? "uri://ed-fi.org" : configuration.GetValue<string>("NamespacePrefix"),
+                NamespacePrefix = configuration.GetValue<string>("NamespacePrefix"),
                 SdkLibraryPath = configuration.GetValue<string>("SdkLibraryPath"),
                 TestSet = testSet,
                 ApiMode = apiMode
