9e3a34e40217d044b58741d32b214206fa512e0e
Jim McKay <james.mckay@instructure.com>
2021-03-23 19:11:06 -0500

    fixed year for xml lookup

--- Utilities/DataLoading/EdFi.XmlLookup.Console/Application/XmlLookupConfiguration.cs
+++ Utilities/DataLoading/EdFi.XmlLookup.Console/Application/XmlLookupConfiguration.cs
@@ -163,7 +163,7 @@ namespace EdFi.XmlLookup.Console.Application
             {
                 return apiMode == ApiMode.YearSpecific
                     ? Regex.Replace(
-                        url, @"(?<year>\d{4})\b", configuration.GetValue<string>("OdsApi:SchoolYear"), RegexOptions.None)
+                        url, @"\/(?<year>\b\d{4}\b)\/", configuration.GetValue<string>("OdsApi:SchoolYear"), RegexOptions.None)
                     : url;
             }
         }
