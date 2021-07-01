3f484dd206a8dd3a85d2f45a021a7ed587547a30
travis.meador <t.meador@wearedoubleline.com>
2021-07-01 16:16:56 -0500

    add FKs

---
+++ Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/1302-CreateEdOrgToEdOrgFK.sql
@@ -0,0 +1,12 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+ALTER TABLE auth.EducationOrganizationIdToEducationOrganizationId ADD CONSTRAINT FK_Source_EducationOrganization FOREIGN KEY (SourceEducationOrganizationId)
+REFERENCES edfi.EducationOrganization (EducationOrganizationId)
+ON DELETE CASCADE;
+
+ALTER TABLE auth.EducationOrganizationIdToEducationOrganizationId ADD CONSTRAINT FK_Target_EducationOrganization FOREIGN KEY (TargetEducationOrganizationId)
+REFERENCES edfi.EducationOrganization (EducationOrganizationId)
+ON DELETE CASCADE;
