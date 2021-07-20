8de6e84b956645124cec59bce199a259fbfae9d1
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2021-07-20 01:07:38 -0500

    Various refinements coming out of work for Change Queries code generation.

--- Utilities/Generator/Nde.Adviser.Lds.SqlGeneration/AdviserLdsPlugin.cs
+++ Utilities/Generator/Nde.Adviser.Lds.SqlGeneration/AdviserLdsPlugin.cs
@@ -3,8 +3,11 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System.Reflection;
 using Autofac;
 using EdFi.Ods.Common.Models;
+using EdFi.Ods.Generator.Database.TemplateModelProviders;
+using EdFi.Ods.Generator.Database.Transformers;
 using EdFi.Ods.Generator.Rendering;
 using Nde.Adviser.Lds.SqlGeneration.Transformers;
 
@@ -16,16 +19,25 @@ namespace Nde.Adviser.Lds.SqlGeneration
         {
             // Register domain model transformers
             containerBuilder
-                .RegisterType<MetaEdModelBoilerplateStripper>()
+                .RegisterType<IdAlternateIdentifierStripper>()
                 .As<IDomainModelDefinitionsTransformer>();
             
             containerBuilder
-                .RegisterType<SchoolYearContextTransformer>()
+                .RegisterType<SchoolYearContextInjectionTransformer>()
                 .As<IDomainModelDefinitionsTransformer>();
 
             containerBuilder
                 .RegisterType<CapabilityStatementTransformer>()
                 .As<IDomainModelDefinitionsTransformer>();
+
+            // Register enhancers for Database artifacts generation
+            containerBuilder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .AssignableTo<ITableEnhancer>()
+                .AsImplementedInterfaces();
+            
+            containerBuilder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .AssignableTo<IColumnEnhancer>()
+                .AsImplementedInterfaces();
         }
     }
 }
