4dcd950c964743a6cc3ae70a0dff8709537309db
Benjamin Meyers <blmeyers@users.noreply.github.com>
2021-07-20 17:04:33 -0400

    Adding copyright headers

--- tests/EdFi.Ods.Api.IntegrationTests/EducationOrganizationTests.cs
+++ tests/EdFi.Ods.Api.IntegrationTests/EducationOrganizationTests.cs
@@ -1,5 +1,9 @@
-﻿using NUnit.Framework;
-using Shouldly;
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using NUnit.Framework;
 
 namespace EdFi.Ods.Api.IntegrationTests
 {
