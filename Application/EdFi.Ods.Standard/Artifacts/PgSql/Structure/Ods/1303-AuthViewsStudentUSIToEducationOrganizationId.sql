2d5d9d61c6f341bb90c34042482c3e6e617fb27b
semalai <m.semalaiappan@gmail.com>
2021-07-22 11:35:09 -0500

    code fix for postgresql

---
+++ Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/1303-AuthViewsStudentUSIToEducationOrganizationId.sql
@@ -0,0 +1,14 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+CREATE OR REPLACE VIEW auth.StudentUSIToEducationOrganizationId 
+AS
+SELECT  tuple.SourceEducationOrganizationId AS  SourceEducationOrganizationId ,
+		ssa.studentUSI  AS TargetEducationOrganizationId
+FROM edfi.StudentSchoolAssociation ssa
+INNER JOIN auth.EducationOrganizationIdToEducationOrganizationId tuple
+ON ssa.SchoolId = tuple.TargetEducationOrganizationId
+
+GO
