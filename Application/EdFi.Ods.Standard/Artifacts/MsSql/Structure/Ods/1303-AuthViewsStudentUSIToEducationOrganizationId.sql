fdec17deeb0a088384c843ca849884f612a7b6ba
semalaiappan <34613894+semalaiappan@users.noreply.github.com>
2021-08-02 10:21:58 -0500

    Update Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/1303-AuthViewsStudentUSIToEducationOrganizationId.sql
    Co-authored-by: Travis Meador <t.meador@wearedoubleline.com>

--- Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/1303-AuthViewsStudentUSIToEducationOrganizationId.sql
+++ Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/1303-AuthViewsStudentUSIToEducationOrganizationId.sql
@@ -8,7 +8,7 @@ IF OBJECT_ID('auth.StudentUSIToEducationOrganizationId', 'V') IS NOT NULL
 GO
 
 CREATE VIEW auth.StudentUSIToEducationOrganizationId AS
-SELECT  tuple.SourceEducationOrganizationId AS  SourceEducationOrganizationId ,ssa.studentUSI  AS studentUSI
+SELECT tuple.SourceEducationOrganizationId, ssa.StudentUSI
 FROM edfi.StudentSchoolAssociation ssa
 INNER JOIN auth.EducationOrganizationIdToEducationOrganizationId tuple
 ON ssa.SchoolId = tuple.TargetEducationOrganizationId
