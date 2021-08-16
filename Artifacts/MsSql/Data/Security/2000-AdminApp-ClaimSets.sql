b09e14ddeb33dd2791ceeb3d1d79ab5715e371ec
Thomas Nunnery <t.nunnery@wearedoubleline.com>
2021-08-16 08:12:01 -0500

    [ODS-5026] Create "Ed-Fi ODS Admin App" and "AB Connect" claimsets during the EdFi_Security database setup/ creation (#344)
    Co-authored-by: semalaiappan <34613894+semalaiappan@users.noreply.github.com>

---
+++ Artifacts/MsSql/Data/Security/2000-AdminApp-ClaimSets.sql
@@ -0,0 +1,182 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+-- Create Ed-Fi ODS Admin App ClaimSet
+
+DECLARE @applicationId int
+DECLARE @applicationName  nvarchar(max)
+DECLARE @claimSetName  nvarchar(255)
+SET @claimSetName = 'Ed-Fi ODS Admin App'
+SET @applicationName = 'Ed-Fi ODS API'
+
+SELECT @applicationId = ApplicationId FROM dbo.Applications WHERE ApplicationName = @applicationName
+
+PRINT 'Ensuring Ed-Fi ODS Admin App Claimset exists.'
+INSERT INTO dbo.ClaimSets (ClaimSetName, Application_ApplicationId)
+SELECT DISTINCT @claimSetName, @applicationId FROM dbo.ClaimSets
+WHERE NOT EXISTS (SELECT 1
+                  FROM dbo.ClaimSets
+				  WHERE ClaimSetName = @claimSetName AND Application_ApplicationId = @applicationId )
+GO
+
+-- Configure Ed-Fi ODS Admin App ClaimSet
+
+DECLARE @actionName nvarchar(255)
+DECLARE @claimSetName nvarchar(255)
+DECLARE @resourceNames TABLE (ResourceName nvarchar(255))
+DECLARE @resourceClaimIds TABLE (ResourceClaimId int)
+DECLARE @authorizationStrategyId INT
+DECLARE @ResourceClaimId INT
+
+SET @claimSetName = 'Ed-Fi ODS Admin App'
+
+DECLARE @edFiOdsAdminAppClaimSetId as INT
+SELECT @edFiOdsAdminAppClaimSetId = ClaimsetId
+FROM dbo.ClaimSets c
+WHERE c.ClaimSetName = @claimSetName
+
+DELETE FROM [dbo].[ClaimSetResourceClaims]
+WHERE [ClaimSet_ClaimSetId] = @edFiOdsAdminAppClaimSetId
+
+PRINT 'Creating Temporary Records.'
+INSERT INTO @resourceNames VALUES ('educationOrganizations'),('systemDescriptors'),('managedDescriptors')
+INSERT INTO @resourceClaimIds SELECT ResourceClaimId FROM dbo.ResourceClaims WHERE ResourceName IN (SELECT ResourceName FROM @resourceNames)
+
+SELECT @authorizationStrategyId = AuthorizationStrategyId
+FROM   dbo.AuthorizationStrategies
+WHERE  AuthorizationStrategyName = 'NoFurtherAuthorizationRequired'
+
+DECLARE @actionId int
+DECLARE @claimSetId int
+
+SELECT @actionId = ActionId FROM dbo.Actions WHERE ActionName = @actionName
+SELECT @claimSetId = ClaimSetId FROM dbo.ClaimSets WHERE ClaimSetName = @claimSetName
+
+PRINT 'Configuring Claims for Ed-Fi ODS Admin App Claimset...'
+INSERT INTO dbo.ClaimSetResourceClaims
+    (Action_ActionId, ClaimSet_ClaimSetId, ResourceClaim_ResourceClaimId, AuthorizationStrategyOverride_AuthorizationStrategyId)
+SELECT ActionId, @claimSetId, ResourceClaimId , @authorizationStrategyId FROM dbo.Actions a, @resourceClaimIds rc
+WHERE NOT EXISTS (SELECT 1
+                  FROM dbo.ClaimSetResourceClaims
+				  WHERE Action_ActionId = a.ActionId AND ClaimSet_ClaimSetId = @claimSetId AND ResourceClaimId = rc.ResourceClaimId)
+
+SELECT @actionId = ActionId FROM dbo.Actions WHERE ActionName = 'Read'
+SELECT @ResourceClaimId = ResourceClaimId FROM dbo.ResourceClaims WHERE ResourceName = 'types'
+
+INSERT INTO dbo.ClaimSetResourceClaims
+    (Action_ActionId, ClaimSet_ClaimSetId, ResourceClaim_ResourceClaimId, AuthorizationStrategyOverride_AuthorizationStrategyId)
+	VALUES (@actionId, @claimSetId, @ResourceClaimId , @authorizationStrategyId)
+
+GO
+
+-- Create AB Connect ClaimSet
+
+DECLARE @applicationId int
+DECLARE @applicationName  nvarchar(max)
+DECLARE @claimSetName  nvarchar(255)
+SET @claimSetName = 'AB Connect'
+SET @applicationName = 'Ed-Fi ODS API'
+
+SELECT @applicationId = ApplicationId FROM  dbo.Applications WHERE ApplicationName = @applicationName
+
+PRINT 'Ensuring AB Connect Claimset exists.'
+INSERT INTO dbo.ClaimSets (ClaimSetName, Application_ApplicationId)
+SELECT DISTINCT @claimSetName, @applicationId FROM dbo.ClaimSets
+WHERE NOT EXISTS (SELECT 1
+                  FROM dbo.ClaimSets
+				  WHERE ClaimSetName = @claimSetName AND Application_ApplicationId = @applicationId )
+GO
+
+-- Configure AB Connect ClaimSet
+
+DECLARE @actionName nvarchar(255)
+DECLARE @claimSetName nvarchar(255)
+DECLARE @resourceNames TABLE (ResourceName nvarchar(255))
+DECLARE @resourceClaimIds TABLE (ResourceClaimId int)
+
+SET @claimSetName = 'AB Connect'
+
+DECLARE @abConnectClaimSetId as INT
+SELECT @abConnectClaimSetId = ClaimsetId
+FROM dbo.ClaimSets c
+WHERE c.ClaimSetName = @claimSetName
+
+DELETE FROM [dbo].[ClaimSetResourceClaims]
+WHERE [ClaimSet_ClaimSetId] = @abConnectClaimSetId
+
+PRINT 'Creating Temporary Records.'
+INSERT INTO @resourceNames VALUES ('gradeLevelDescriptor'),('academicSubjectDescriptor'),('publicationStatusDescriptor'),('educationStandards')
+INSERT INTO @resourceClaimIds SELECT ResourceClaimId FROM dbo.ResourceClaims WHERE ResourceName IN (SELECT ResourceName FROM @resourceNames)
+
+DECLARE @actionId int
+DECLARE @claimSetId int
+
+SELECT @actionId = ActionId FROM dbo.Actions WHERE ActionName = @actionName
+SELECT @claimSetId = ClaimSetId FROM dbo.ClaimSets WHERE ClaimSetName = @claimSetName
+
+PRINT 'Configuring Claims for AB Connect Claimset...'
+INSERT INTO dbo.ClaimSetResourceClaims
+    (Action_ActionId, ClaimSet_ClaimSetId, ResourceClaim_ResourceClaimId)
+SELECT ActionId, @claimSetId, ResourceClaimId FROM dbo.Actions a, @resourceClaimIds rc
+WHERE NOT EXISTS (SELECT 1
+                  FROM dbo.ClaimSetResourceClaims
+				  WHERE Action_ActionId = a.ActionId AND ClaimSet_ClaimSetId = @claimSetId AND ResourceClaimId = rc.ResourceClaimId)
+GO
+
+-- Set educationStandards to not require further authorization for READ 
+
+DECLARE @authorizationStrategyId INT
+DECLARE @resourceClaimId INT
+DECLARE @actionId INT
+
+SELECT @authorizationStrategyId = AuthorizationStrategyId
+FROM   dbo.AuthorizationStrategies
+WHERE  AuthorizationStrategyName = 'NoFurtherAuthorizationRequired'
+
+SELECT @resourceClaimId = ResourceClaimId
+FROM   dbo.ResourceClaims
+WHERE  ResourceName = 'educationStandards'
+
+SELECT @ActionId = ActionId FROM dbo.Actions WHERE ActionName = 'Read'
+
+PRINT 'Updating educationStandards authorization strategy for READ.'
+
+UPDATE dbo.ResourceClaimAuthorizationMetadatas SET AuthorizationStrategy_AuthorizationStrategyId = @authorizationStrategyId
+WHERE  Action_ActionId = @actionId AND ResourceClaim_ResourceClaimId = @resourceClaimId
+
+GO
+
+-- Add performanceLevelDescriptor to the assessment vendor claimset (if not already present)
+
+DECLARE @performanceLevelDescriptorClaimId as INT
+
+SELECT @performanceLevelDescriptorClaimId = ResourceClaimId
+FROM dbo.ResourceClaims rc
+WHERE rc.ResourceName = 'performanceLevelDescriptor'
+
+DECLARE @claimSetId as INT
+
+SELECT @claimSetId = ClaimsetId
+FROM dbo.ClaimSets c
+WHERE c.ClaimSetName = 'Assessment Vendor'
+
+PRINT 'Ensuring create and read actions for performanceLevelDescriptor are assigned to Assessment Vendor claimset'
+
+IF EXISTS (SELECT 1 FROM dbo.ClaimSetResourceClaims WHERE ResourceClaim_ResourceClaimId = @performanceLevelDescriptorClaimId AND ClaimSet_ClaimSetId = @claimSetId)
+BEGIN
+
+	DELETE FROM dbo.ClaimSetResourceClaims WHERE ResourceClaim_ResourceClaimId = @performanceLevelDescriptorClaimId AND ClaimSet_ClaimSetId = @claimSetId
+
+END
+
+
+INSERT INTO dbo.ClaimSetResourceClaims
+    (Action_ActionId, ClaimSet_ClaimSetId, ResourceClaim_ResourceClaimId)
+SELECT ActionId, @claimSetId, @performanceLevelDescriptorClaimId FROM dbo.Actions a
+WHERE a.ActionName in ('Read','Create') AND NOT EXISTS (SELECT 1
+                  FROM dbo.ClaimSetResourceClaims
+				  WHERE Action_ActionId = a.ActionId AND ClaimSet_ClaimSetId = @claimSetId AND ClaimsetResourceClaims.ResourceClaim_ResourceClaimId = @performanceLevelDescriptorClaimId)
+GO
+----------------------------------------------------------------------------------------------------
---
+++ Artifacts/PgSql/Data/Security/2000- AdminApp-ClaimSets.sql
@@ -0,0 +1,189 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+-- Create and configure Ed-Fi ODS Admin App claim set
+DO $$
+    DECLARE application_name varchar(50) := 'Ed-Fi ODS API';
+    DECLARE application_id int;
+    DECLARE claimset_name varchar(50) := 'Ed-Fi ODS Admin App';
+    DECLARE claimset_id int;
+	DECLARE authorizationStrategy_id int;
+BEGIN
+	SELECT applicationid INTO application_id
+    FROM dbo.applications
+    WHERE applicationname = application_name;
+	
+	-- Creating Ed-Fi ODS Admin App claim set
+	IF EXISTS (SELECT 1 FROM dbo.claimsets WHERE claimsetname = claimset_name)
+    THEN
+        RAISE NOTICE '% claimset exists', claimset_name;
+    ELSE
+        RAISE NOTICE 'adding % claimset', claimset_name;
+        INSERT INTO dbo.claimsets (claimsetname, application_applicationid) VALUES (claimset_name, application_id);
+    END IF;	
+
+    -- Configure Ed-Fi ODS Admin App claim set
+	
+	SELECT claimsetid INTO claimset_id
+    FROM dbo.claimsets
+    WHERE claimsetname = claimset_name;
+	
+	DELETE FROM dbo.ClaimSetResourceClaims WHERE ClaimSet_ClaimSetId = claimset_id;
+	
+	SELECT authorizationstrategyid INTO authorizationStrategy_id
+    FROM dbo.authorizationstrategies
+    WHERE authorizationstrategyname = 'NoFurtherAuthorizationRequired';
+	
+    IF EXISTS (SELECT 1 FROM dbo.claimsetresourceclaims WHERE claimset_claimsetid = claimset_id)
+    THEN
+        RAISE NOTICE 'claims already exist for claim %', claimset_name;
+    ELSE
+        RAISE NOTICE 'Configuring Claims for % Claimset...', claimset_name;
+        INSERT INTO dbo.claimsetresourceclaims
+            (Action_ActionId
+            ,ClaimSet_ClaimSetId
+            ,ResourceClaim_ResourceClaimId
+            ,AuthorizationStrategyOverride_AuthorizationStrategyId
+            ,ValidationRuleSetNameOverride)
+        SELECT ac.actionid, claimset_id, resourceclaimid, authorizationStrategy_id, CAST(null AS int)
+        FROM dbo.resourceclaims
+        INNER JOIN LATERAL
+            (SELECT actionid
+            FROM dbo.actions
+            WHERE actionname in ('Create','Read','Update','Delete')) AS ac ON true
+        WHERE resourcename IN ('educationOrganizations','systemDescriptors','managedDescriptors');
+		
+		INSERT INTO dbo.claimsetresourceclaims
+            (Action_ActionId
+            ,ClaimSet_ClaimSetId
+            ,ResourceClaim_ResourceClaimId
+            ,AuthorizationStrategyOverride_AuthorizationStrategyId
+            ,ValidationRuleSetNameOverride)
+        SELECT ac.actionid, claimset_id, resourceclaimid, authorizationStrategy_id, CAST(null AS int)
+        FROM dbo.resourceclaims
+        INNER JOIN LATERAL
+            (SELECT actionid
+            FROM dbo.actions
+            WHERE actionname in ('Read')) AS ac ON true
+        WHERE resourcename IN ('types');
+    END IF;	
+	
+END $$;
+
+-- Create and configure AB Connect claim set
+DO $$
+    DECLARE application_name varchar(50) := 'Ed-Fi ODS API';
+    DECLARE application_id int;
+    DECLARE claimset_name varchar(50) := 'AB Connect';
+    DECLARE claimset_id int;
+BEGIN
+
+    IF NOT EXISTS (SELECT 1 FROM dbo.applications WHERE applicationname = application_name)
+    THEN
+        RAISE NOTICE '% does not exist', application_name;
+    END IF;
+
+    SELECT applicationid INTO application_id
+    FROM dbo.applications
+    WHERE applicationname = application_name;
+
+    -- Create AB Connect ClaimSet
+    IF EXISTS (SELECT 1 FROM dbo.claimsets WHERE claimsetname = claimset_name)
+    THEN
+        RAISE NOTICE '% claimset exists', claimset_name;
+    ELSE
+        RAISE NOTICE 'adding % claimset', claimset_name;
+        INSERT INTO dbo.claimsets (claimsetname, application_applicationid) VALUES (claimset_name, application_id);
+    END IF;
+	
+    SELECT claimsetid INTO claimset_id
+    FROM dbo.claimsets
+    WHERE claimsetname = claimset_name;
+	
+	DELETE FROM dbo.ClaimSetResourceClaims WHERE ClaimSet_ClaimSetId = claimset_id;
+
+    -- Configure AB Connect ClaimSet
+    IF EXISTS (SELECT 1 FROM dbo.claimsetresourceclaims WHERE claimset_claimsetid = claimset_id)
+    THEN
+        RAISE NOTICE 'claims already exist for claim %', claimset_name;
+    ELSE
+        RAISE NOTICE 'Configuring Claims for % Claimset...', claimset_name;
+        INSERT INTO dbo.claimsetresourceclaims
+            (Action_ActionId
+            ,ClaimSet_ClaimSetId
+            ,ResourceClaim_ResourceClaimId
+            ,AuthorizationStrategyOverride_AuthorizationStrategyId
+            ,ValidationRuleSetNameOverride)
+        SELECT ac.actionid, claimset_id, resourceclaimid, CAST(null AS int), CAST(null AS int)
+        FROM dbo.resourceclaims
+        INNER JOIN LATERAL
+            (SELECT actionid
+            FROM dbo.actions
+            WHERE actionname in ('Create','Read','Update','Delete')) AS ac ON true
+        WHERE resourcename IN ('gradeLevelDescriptor','academicSubjectDescriptor','publicationStatusDescriptor','educationStandards');
+    END IF;
+END $$;
+
+-- Set educationStandards to not require further authorization for READ 
+DO $$
+    DECLARE authorization_strategy_id int;
+    DECLARE resource_claim_id int;
+    DECLARE action_id int;
+BEGIN
+    SELECT authorizationstrategyid INTO authorization_strategy_id
+    FROM dbo.authorizationstrategies
+    WHERE authorizationstrategyname = 'NoFurtherAuthorizationRequired';
+
+    SELECT resourceclaimid INTO resource_claim_id
+    FROM dbo.resourceclaims
+    WHERE resourcename = 'educationStandards';
+
+    SELECT actionid INTO action_id
+    FROM dbo.actions
+    WHERE actionname = 'Read';
+
+    RAISE NOTICE 'Updating educationStandards authorization strategy for READ.';
+
+    UPDATE dbo.resourceclaimauthorizationmetadatas
+    SET authorizationstrategy_authorizationstrategyid = authorization_strategy_id
+    WHERE action_actionid = action_id AND resourceclaim_resourceclaimid = resource_claim_id;
+END $$;
+
+-- Add performanceLevel descriptor to the assessment vendor claimset (if not already present)
+DO $$
+    DECLARE resourceclaim_id int;
+    DECLARE claimset_id int;
+BEGIN
+    SELECT resourceclaimid into resourceclaim_id
+    FROM dbo.resourceclaims
+    WHERE resourcename = 'performanceLevelDescriptor';
+
+    SELECT claimsetid into claimset_id
+    FROM dbo.claimsets
+    WHERE claimsetname = 'Assessment Vendor';
+
+    IF EXISTS (SELECT 1
+               FROM dbo.claimsetresourceclaims
+               WHERE claimset_claimsetid = claimset_id AND resourceclaim_resourceclaimid = resourceclaim_id)
+    THEN
+        DELETE FROM dbo.ClaimSetResourceClaims WHERE ResourceClaim_ResourceClaimId = resourceclaim_id AND ClaimSet_ClaimSetId = claimset_id;
+    END IF;
+	
+	RAISE NOTICE 'Ensuring create, read actions for performanceLevelDescriptor are assigned to Assessment Vendor claimset';
+    INSERT INTO dbo.claimsetresourceclaims
+        (Action_ActionId
+        ,ClaimSet_ClaimSetId
+        ,ResourceClaim_ResourceClaimId
+        ,AuthorizationStrategyOverride_AuthorizationStrategyId
+        ,ValidationRuleSetNameOverride)
+    SELECT ac.actionid, claimset_id, resourceclaimid, CAST(null AS int), CAST(null AS int)
+    FROM dbo.resourceclaims
+    INNER JOIN LATERAL
+    (SELECT actionid
+        FROM dbo.actions
+        WHERE actionname IN ('Create','Read')) AS ac ON true
+    WHERE resourceclaimid = resourceclaim_id;
+		
+END $$;
