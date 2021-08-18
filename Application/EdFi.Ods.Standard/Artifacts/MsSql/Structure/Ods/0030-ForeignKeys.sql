82ef6075fe27682191b16a9c5c1d0b9cc9bb000b
semalai <m.semalaiappan@gmail.com>
2021-08-17 20:38:36 -0500

    added hearder for file

--- Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/0030-ForeignKeys.sql
+++ Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/0030-ForeignKeys.sql
@@ -1,3 +1,8 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
 ALTER TABLE [edfi].[AbsenceEventCategoryDescriptor] WITH CHECK ADD CONSTRAINT [FK_AbsenceEventCategoryDescriptor_Descriptor] FOREIGN KEY ([AbsenceEventCategoryDescriptorId])
 REFERENCES [edfi].[Descriptor] ([DescriptorId])
 ON DELETE CASCADE
