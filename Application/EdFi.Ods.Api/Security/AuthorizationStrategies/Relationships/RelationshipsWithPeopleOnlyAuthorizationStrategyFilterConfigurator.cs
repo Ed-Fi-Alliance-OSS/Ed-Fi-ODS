ed2a2e31b592bc3a588e91593571f2e52bf289f3
semalai <m.semalaiappan@gmail.com>
2021-08-19 12:03:59 -0500

    code fix for filters

--- Application/EdFi.Ods.Api/Security/AuthorizationStrategies/Relationships/RelationshipsWithPeopleOnlyAuthorizationStrategyFilterConfigurator.cs
+++ Application/EdFi.Ods.Api/Security/AuthorizationStrategies/Relationships/RelationshipsWithPeopleOnlyAuthorizationStrategyFilterConfigurator.cs
@@ -21,7 +21,6 @@ namespace EdFi.Ods.Api.Security.AuthorizationStrategies.Relationships
             var filters = new List<FilterApplicationDetails>
             {
                 // Local Education Agency/School to Person relationships
-                RelationshipsAuthorizationFilters.StudentUSIToEducationOrganizationId,
                 RelationshipsAuthorizationFilters.LocalEducationAgencyIdToStudentUSI,
                 RelationshipsAuthorizationFilters.SchoolIdToStudentUSI,
                 RelationshipsAuthorizationFilters.LocalEducationAgencyIdToStaffUSI,
