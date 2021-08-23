0b3a85742d043ce384d65bd4d9835244a604ba20
semalai <m.semalaiappan@gmail.com>
2021-08-23 08:19:31 -0500

    EducationOrganizationIdToEducationOrganizationId tuple table added

--- Application/EdFi.Ods.Common/Constants/OrmMappingFileConventions.cs
+++ Application/EdFi.Ods.Common/Constants/OrmMappingFileConventions.cs
@@ -3,12 +3,15 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-namespace EdFi.Ods.Common.Constants {
-    public static class OrmMappingFileConventions{
+namespace EdFi.Ods.Common.Constants
+{
+    public static class OrmMappingFileConventions
+    {
         public const string EntityOrmMappings = "EntityOrmMappings";
         public const string EntityOrmMappingsGeneratedHbm = "EntityOrmMappings.generated.hbm.xml";
         public const string EntityOrmMappingsForQueriesGeneratedHbm = "EntityOrmMappingsForQueries.generated.hbm.xml";
         public const string EntityOrmMappingsForViewsGeneratedHbm = "EntityOrmMappingsForViews.generated.hbm.xml";
+        public const string EntityOrmMappingsForTupleTableGeneratedHbm = "EntityOrmMappingsForTupleTable.generated.hbm.xml";
         public const string OrmMappingAssembly = "EdFi.Ods.Standard";
     }
 }
