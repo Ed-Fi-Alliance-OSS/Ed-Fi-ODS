2c151d7fd57786e0ee65408f053b082468303d87
semalai <m.semalaiappan@gmail.com>
2021-10-07 08:15:46 -0500

    Added-RemoveAllUnusedAuthorizationViews

--- Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/1321-RemoveAllUnusedAuthorizationViews.sql
+++ Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/1321-RemoveAllUnusedAuthorizationViews.sql
@@ -3,27 +3,23 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-DROP VIEW IF EXISTS auth.SchoolIdToStudentUSIThroughEdOrgAssociation;
-DROP VIEW IF EXISTS auth.SchoolIdToStudentUSI;
-DROP VIEW IF EXISTS auth.LocalEducationAgencyIdToStudentUSIThroughEdOrgAssociation;
-DROP VIEW IF EXISTS auth.OrganizationDepartmentIdToSchoolId;
-DROP VIEW IF EXISTS auth.ParentUSIToSchoolId;
-DROP VIEW IF EXISTS auth.ParentUSIToStudentUSI;
-DROP VIEW IF EXISTS auth.PostSecondaryInstitutionIdToStaffUSI;
-DROP VIEW IF EXISTS auth.School;
-DROP VIEW IF EXISTS auth.SchoolIdToStaffUSI;
-DROP VIEW IF EXISTS auth.LocalEducationAgency;
-DROP VIEW IF EXISTS auth.EducationOrganizationToStaffUSI_Employment;
-DROP VIEW IF EXISTS auth.EducationOrganizationToStaffUSI_Assignment;
-DROP VIEW IF EXISTS auth.EducationOrganizationIdToStudentUSI;
-DROP VIEW IF EXISTS auth.EducationOrganizationIdToStateAgencyId;
-DROP VIEW IF EXISTS auth.EducationOrganizationIdToStaffUSI;
-DROP VIEW IF EXISTS auth.EducationOrganizationIdToSchoolId;
-DROP VIEW IF EXISTS auth.EducationOrganizationIdToPostSecondaryInstitutionId;
-DROP VIEW IF EXISTS auth.EducationOrganizationIdToLocalEducationAgencyId;
-DROP VIEW IF EXISTS auth.EducationOrganizationIdToEducationServiceCenterId;
-DROP VIEW IF EXISTS auth.EducationOrganizationIdentifiers;
-DROP VIEW IF EXISTS auth.CommunityProviderIdToStaffUSI;
-DROP VIEW IF EXISTS auth.CommunityProviderIdToEducationOrganizationId;
-DROP VIEW IF EXISTS auth.CommunityOrganizationIdToEducationOrganizationId;
-DROP VIEW IF EXISTS auth.CommunityOrganizationIdToCommunityProviderId;
+    DROP VIEW IF EXISTS auth.LocalEducationAgencyIdToStudentUSI;
+    DROP VIEW IF EXISTS auth.LocalEducationAgencyIdToStudentUSIThroughEdOrgAssociation;
+    DROP VIEW IF EXISTS auth.SchoolIdToStudentUSI;
+    DROP VIEW IF EXISTS auth.SchoolIdToStudentUSIThroughEdOrgAssociation;
+    DROP VIEW IF EXISTS auth.LocalEducationAgencyIdToStaffUSI;
+    DROP VIEW IF EXISTS auth.SchoolIdToStaffUSI;
+    DROP VIEW IF EXISTS auth.LocalEducationAgencyIdToParentUSI;
+    DROP VIEW IF EXISTS auth.ParentUSIToSchoolId;
+    DROP VIEW IF EXISTS auth.EducationOrganizationIdToLocalEducationAgencyId;
+    DROP VIEW IF EXISTS auth.EducationOrganizationIdToSchoolId;
+    DROP VIEW IF EXISTS auth.LocalEducationAgencyIdToSchoolId;
+    DROP VIEW IF EXISTS auth.LocalEducationAgencyIdToOrganizationDepartmentId;
+    DROP VIEW IF EXISTS auth.OrganizationDepartmentIdToSchoolId;
+    DROP VIEW IF EXISTS auth.CommunityOrganizationIdToEducationOrganizationId;
+    DROP VIEW IF EXISTS auth.CommunityProviderIdToEducationOrganizationId;
+    DROP VIEW IF EXISTS auth.CommunityOrganizationIdToCommunityProviderId;
+    DROP VIEW IF EXISTS auth.EducationOrganizationIdToPostSecondaryInstitutionId;
+    DROP VIEW IF EXISTS auth.EducationOrganizationIdToUniversityId;
+    DROP VIEW IF EXISTS auth.EducationOrganizationIdToTeacherPreparationProviderId;
+
