046a916b0e338d321d01cfcd856c4e405a50440c
semalai <m.semalaiappan@gmail.com>
2021-10-06 21:58:09 -0500

    Added-RemoveAllUnusedAuthorizationViews

---
+++ Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/1321-RemoveAllUnusedAuthorizationViews.sql
@@ -0,0 +1,29 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+DROP VIEW IF EXISTS auth.SchoolIdToStudentUSIThroughEdOrgAssociation;
+DROP VIEW IF EXISTS auth.SchoolIdToStudentUSI;
+DROP VIEW IF EXISTS auth.LocalEducationAgencyIdToStudentUSIThroughEdOrgAssociation;
+DROP VIEW IF EXISTS auth.OrganizationDepartmentIdToSchoolId;
+DROP VIEW IF EXISTS auth.ParentUSIToSchoolId;
+DROP VIEW IF EXISTS auth.ParentUSIToStudentUSI;
+DROP VIEW IF EXISTS auth.PostSecondaryInstitutionIdToStaffUSI;
+DROP VIEW IF EXISTS auth.School;
+DROP VIEW IF EXISTS auth.SchoolIdToStaffUSI;
+DROP VIEW IF EXISTS auth.LocalEducationAgency;
+DROP VIEW IF EXISTS auth.EducationOrganizationToStaffUSI_Employment;
+DROP VIEW IF EXISTS auth.EducationOrganizationToStaffUSI_Assignment;
+DROP VIEW IF EXISTS auth.EducationOrganizationIdToStudentUSI;
+DROP VIEW IF EXISTS auth.EducationOrganizationIdToStateAgencyId;
+DROP VIEW IF EXISTS auth.EducationOrganizationIdToStaffUSI;
+DROP VIEW IF EXISTS auth.EducationOrganizationIdToSchoolId;
+DROP VIEW IF EXISTS auth.EducationOrganizationIdToPostSecondaryInstitutionId;
+DROP VIEW IF EXISTS auth.EducationOrganizationIdToLocalEducationAgencyId;
+DROP VIEW IF EXISTS auth.EducationOrganizationIdToEducationServiceCenterId;
+DROP VIEW IF EXISTS auth.EducationOrganizationIdentifiers;
+DROP VIEW IF EXISTS auth.CommunityProviderIdToStaffUSI;
+DROP VIEW IF EXISTS auth.CommunityProviderIdToEducationOrganizationId;
+DROP VIEW IF EXISTS auth.CommunityOrganizationIdToEducationOrganizationId;
+DROP VIEW IF EXISTS auth.CommunityOrganizationIdToCommunityProviderId;
