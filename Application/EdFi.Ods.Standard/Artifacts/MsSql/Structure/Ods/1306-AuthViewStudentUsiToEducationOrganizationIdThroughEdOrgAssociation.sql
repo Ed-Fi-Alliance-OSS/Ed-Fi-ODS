9aee218f55efaf7988a2c2f0d460efcc7d0a745b
semalai <m.semalaiappan@gmail.com>
2021-10-08 15:03:21 -0500

    StudentEducationOrganizationResponsibilityAssociation to StudentEducationOrganizationAssociation

--- Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/1306-AuthViewStudentUsiToEducationOrganizationIdThroughEdOrgAssociation.sql
+++ Application/EdFi.Ods.Standard/Artifacts/MsSql/Structure/Ods/1306-AuthViewStudentUsiToEducationOrganizationIdThroughEdOrgAssociation.sql
@@ -6,7 +6,7 @@
 CREATE OR ALTER VIEW auth.StudentUSIToEducationOrganizationIdThroughEdOrgAssociation AS
     SELECT  edOrgs.SourceEducationOrganizationId, seora.StudentUSI
     FROM    auth.EducationOrganizationIdToEducationOrganizationId edOrgs
-            INNER JOIN edfi.StudentEducationOrganizationResponsibilityAssociation seora
+            INNER JOIN edfi.StudentEducationOrganizationAssociation seora
                 ON edOrgs.TargetEducationOrganizationId = seora.EducationOrganizationId
     GROUP BY edOrgs.SourceEducationOrganizationId, seora.StudentUSI
 GO
