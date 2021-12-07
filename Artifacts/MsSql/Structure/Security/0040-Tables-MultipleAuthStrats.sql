83c2e99c05916c6acb49fc56c5282d83093f6c03
semalai <m.semalaiappan@gmail.com>
2021-12-07 12:27:41 -0600

    updated Unique constriant column  ordering

--- Artifacts/MsSql/Structure/Security/0040-Tables-MultipleAuthStrats.sql
+++ Artifacts/MsSql/Structure/Security/0040-Tables-MultipleAuthStrats.sql
@@ -115,8 +115,8 @@ CREATE TABLE [dbo].[ResourceClaimActionAuthorizationStrategies](
 GO
 
 -- UNIQUE NONCLUSTERED INDEX for [AuthorizationStrategyId],[ResourceClaimActionId] columns 
-CREATE UNIQUE NONCLUSTERED INDEX [IX_AuthorizationStrategyId_ResourceClaimActionId]
-    ON [dbo].[ResourceClaimActionAuthorizationStrategies]([AuthorizationStrategyId],[ResourceClaimActionId] ASC)
+CREATE UNIQUE NONCLUSTERED INDEX [IX_ResourceClaimActionId_AuthorizationStrategyId]
+    ON [dbo].[ResourceClaimActionAuthorizationStrategies]([ResourceClaimActionId],[AuthorizationStrategyId] ASC)
 WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 
