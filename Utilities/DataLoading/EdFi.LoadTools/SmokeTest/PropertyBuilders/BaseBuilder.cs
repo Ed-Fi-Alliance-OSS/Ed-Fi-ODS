aa6cfb7fcc63c2b7e51699187c71328ab2e937c1
Ben Tillman <ben@headspring.com>
2022-02-03 16:21:40 -0600

    Update projects to NET6 and replace obsolete class usage
    Replaced the usages per documentation here:
    https://docs.microsoft.com/en-us/dotnet/fundamentals/syslib-diagnostics/syslib0023
    
    and here:
    https://docs.microsoft.com/en-us/dotnet/fundamentals/syslib-diagnostics/syslib0021

--- Utilities/DataLoading/EdFi.LoadTools/SmokeTest/PropertyBuilders/BaseBuilder.cs
+++ Utilities/DataLoading/EdFi.LoadTools/SmokeTest/PropertyBuilders/BaseBuilder.cs
@@ -48,12 +48,12 @@ namespace EdFi.LoadTools.SmokeTest.PropertyBuilders
             // if necessary in the future.
             const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
             var res = new StringBuilder(length);
-            using (var rng = new RNGCryptoServiceProvider())
+            using (var rng = RandomNumberGenerator.Create())
             {
                 int count = (int)Math.Ceiling(Math.Log(alphabet.Length, 2) / 8.0);
                 Debug.Assert(count <= sizeof(uint));
                 int offset = BitConverter.IsLittleEndian ? 0 : sizeof(uint) - count;
-                int max = (int)(Math.Pow(2, count*8) / alphabet.Length) * alphabet.Length;
+                int max = (int)(Math.Pow(2, count * 8) / alphabet.Length) * alphabet.Length;
                 byte[] uintBuffer = new byte[sizeof(uint)];
 
                 while (res.Length < length)
@@ -62,11 +62,10 @@ namespace EdFi.LoadTools.SmokeTest.PropertyBuilders
                     uint num = BitConverter.ToUInt32(uintBuffer, 0);
                     if (num < max)
                     {
-                        res.Append(alphabet[(int) (num % alphabet.Length)]);
+                        res.Append(alphabet[(int)(num % alphabet.Length)]);
                     }
                 }
             }
-
             return res.ToString();
         }
 
