d87e096c33ed5df2de433c17a2d52113e1a46708
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-02-03 23:36:30 -0600

    Implemented authorization for deletes child resource (.NET components only).
    NOTE: Dropped EdFiAuthorizationProvider and RelationshipsAuthorizationFilters due to removal from v6.0 lineage

---
+++ Application/EdFi.Ods.Api/Security/AuthorizationStrategies/Relationships/RelationshipsWithEdOrgsAndPeopleIncludingDeletesAuthorizationStrategy.cs
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Linq;
+using EdFi.Common.Utils.Extensions;
+using EdFi.Ods.Common.Specifications;
+using EdFi.Ods.Security.Authorization;
+
+namespace EdFi.Ods.Security.AuthorizationStrategies.Relationships
+{
+    public class RelationshipsWithEdOrgsAndPeopleIncludingDeletesAuthorizationStrategy<TContextData>
+        : RelationshipsAuthorizationStrategyBase<TContextData>
+        where TContextData : RelationshipsAuthorizationContextData, new()
+    {
+        public RelationshipsWithEdOrgsAndPeopleIncludingDeletesAuthorizationStrategy(IConcreteEducationOrganizationIdAuthorizationContextDataTransformer<TContextData> concreteEducationOrganizationIdAuthorizationContextDataTransformer)
+            : base(concreteEducationOrganizationIdAuthorizationContextDataTransformer) { }
+
+        protected override void BuildAuthorizationSegments(
+            AuthorizationBuilder<TContextData> authorizationBuilder,
+            string[] authorizationContextPropertyNames)
+        {
+            // For person USIs, use the view that incorporates the deleted associations
+            authorizationContextPropertyNames
+                .Where(PersonEntitySpecification.IsPersonIdentifier)
+                .ForEach(pn => authorizationBuilder.ClaimsMustBeAssociatedWith(pn, "IncludingDeletes"));
+            
+            // For EdOrgs, use the standard views
+            authorizationContextPropertyNames
+                .Where(pn => !PersonEntitySpecification.IsPersonIdentifier(pn))
+                .ForEach(pn => authorizationBuilder.ClaimsMustBeAssociatedWith(pn));
+        }
+    }
+}
---
+++ Application/EdFi.Ods.Api/Security/AuthorizationStrategies/Relationships/RelationshipsWithEdOrgsAndPeopleIncludingDeletesAuthorizationStrategyFilterConfigurator.cs
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Collections.Generic;
+using EdFi.Ods.Common.Infrastructure.Filtering;
+using EdFi.Ods.Security.AuthorizationStrategies.Relationships.Filters;
+
+namespace EdFi.Ods.Security.AuthorizationStrategies.Relationships
+{
+    public class RelationshipsWithEdOrgsAndPeopleIncludingDeletesAuthorizationStrategyFilterConfigurator
+        : INHibernateFilterConfigurator
+    {
+        /// <summary>
+        /// Gets the NHibernate filter definitions and a functional delegate for determining when to apply them.
+        /// </summary>
+        /// <returns>A read-only list of filter application details to be applied to the NHibernate configuration and entity mappings.</returns>
+        public IReadOnlyList<FilterApplicationDetails> GetFilters()
+        {
+            var filters = new List<FilterApplicationDetails>
+            {
+                // Education Service Center to Person relationships
+                RelationshipsAuthorizationFilters.EducationServiceCenterIdToStudentUSIIncludingDeletes,
+                RelationshipsAuthorizationFilters.EducationServiceCenterIdToStaffUSIIncludingDeletes,
+                RelationshipsAuthorizationFilters.EducationServiceCenterIdToParentUSIIncludingDeletes,
+
+                // Local Education Agency to Person relationships
+                RelationshipsAuthorizationFilters.LocalEducationAgencyIdToStudentUSIIncludingDeletes,
+                RelationshipsAuthorizationFilters.LocalEducationAgencyIdToStaffUSIIncludingDeletes,
+                RelationshipsAuthorizationFilters.LocalEducationAgencyIdToParentUSIIncludingDeletes,
+
+                // School to Person relationships
+                RelationshipsAuthorizationFilters.SchoolIdToStudentUSIIncludingDeletes,
+                RelationshipsAuthorizationFilters.SchoolIdToStaffUSIIncludingDeletes,
+                RelationshipsAuthorizationFilters.SchoolIdToParentUSIIncludingDeletes,
+            };
+
+            return filters;
+        }
+    }
+}
