bbf1ac6435474b726730cddd53ad043e29d65e3f
semalai <m.semalaiappan@gmail.com>
2022-02-23 10:34:51 -0600

    IsReadOnly and IsInternalUseOnly columns added

---
+++ Artifacts/PgSql/Data/Security/2050-UpdateClaimSetReadOnlyAndInternalUseOnlyColum.sql
@@ -0,0 +1,14 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+UPDATE dbo.ClaimSets
+SET IsInternalUseOnly = 'TRUE'
+WHERE ClaimSetName in ('Bootstrap Descriptors and EdOrgs','Ed-Fi ODS Admin App');
+
+UPDATE dbo.ClaimSets
+SET IsReadOnly = 'TRUE'
+WHERE ClaimSetName in ('SIS Vendor','Ed-Fi Sandbox','Roster Vendor','Assessment Vendor','Assessment Read',
+'Bootstrap Descriptors and EdOrgs','Ownership Based Test','District Hosted SIS Vendor','Ed-Fi ODS Admin App',
+'AB Connect','Education Preparation Program');
---
+++ Artifacts/PgSql/Structure/Security/0060-AlterClaimsetTable.sql
@@ -0,0 +1,7 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+ALTER TABLE dbo.ClaimSets ADD  IsReadOnly BOOLEAN  NOT NULL DEFAULT FALSE;
+ALTER TABLE dbo.ClaimSets ADD IsInternalUseOnly BOOLEAN   NOT NULL DEFAULT FALSE;
