4048949478c978d46072293b306124f8f3bea8fe
semalaiappan <34613894+semalaiappan@users.noreply.github.com>
2022-02-25 14:01:14 -0600

    ODS-4021 Add metadata to claim set records so that Admin App can determine which to present and which to treat as default/read-only (#444)
    * IsReadOnly and IsInternalUseOnly columns added
    
    * IsPreset column updated
    
    * updated to IsEdfiPreset
    
    * Ownership Based Test updated
    
    * Claimset updated for Ownership Based Test

---
+++ Artifacts/MsSql/Data/Security/2050-UpdateClaimSetReadOnlyAndInternalUseOnlyColum.sql
@@ -0,0 +1,15 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+UPDATE [dbo].[ClaimSets]
+SET [ForApplicationUseOnly] = 1 
+WHERE [ClaimSetName] in ('Bootstrap Descriptors and EdOrgs','Ed-Fi ODS Admin App','Ownership Based Test')
+GO
+
+UPDATE [dbo].[ClaimSets]
+SET [IsEdfiPreset] = 1 
+WHERE [ClaimSetName] in ('SIS Vendor','Ed-Fi Sandbox','Roster Vendor','Assessment Vendor','Assessment Read',
+'Bootstrap Descriptors and EdOrgs','Ownership Based Test','District Hosted SIS Vendor','Ed-Fi ODS Admin App','AB Connect')
+GO
---
+++ Artifacts/PgSql/Data/Security/2050-UpdateClaimSetReadOnlyAndInternalUseOnlyColum.sql
@@ -0,0 +1,13 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+UPDATE dbo.ClaimSets
+SET ForApplicationUseOnly = 'TRUE'
+WHERE ClaimSetName in ('Bootstrap Descriptors and EdOrgs','Ed-Fi ODS Admin App', 'Ownership Based Test');
+
+UPDATE dbo.ClaimSets
+SET IsEdfiPreset = 'TRUE'
+WHERE ClaimSetName in ('SIS Vendor','Ed-Fi Sandbox','Roster Vendor','Assessment Vendor','Assessment Read',
+'Bootstrap Descriptors and EdOrgs','Ownership Based Test','District Hosted SIS Vendor','Ed-Fi ODS Admin App','AB Connect');
