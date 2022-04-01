7000e1473a08edf655e8b0da83e1fd98f776d9ae
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-03-31 21:55:25 -0500

    Miscellaneous namespace cleanup.

--- Application/EdFi.Ods.Api/Security/AuthorizationStrategies/Relationships/RelationshipsWithEdOrgsAndPeopleAuthorizationStrategy.cs
+++ Application/EdFi.Ods.Api/Security/AuthorizationStrategies/Relationships/RelationshipsWithEdOrgsAndPeopleAuthorizationStrategy.cs
@@ -3,10 +3,8 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Intrinsics.X86;
 
 namespace EdFi.Ods.Api.Security.AuthorizationStrategies.Relationships
 {
