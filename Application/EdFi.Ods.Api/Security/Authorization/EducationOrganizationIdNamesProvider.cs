f5d0e2633bc0edde389fa738a22623ee2604ddca
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-03-31 21:53:39 -0500

    Removed unused functionality around analysis of the edorg hierarchy.

--- Application/EdFi.Ods.Api/Security/Authorization/EducationOrganizationIdNamesProvider.cs
+++ Application/EdFi.Ods.Api/Security/Authorization/EducationOrganizationIdNamesProvider.cs
@@ -13,86 +13,25 @@ using QuickGraph;
 
 namespace EdFi.Ods.Api.Security.Authorization
 {
+    /// <summary>
+    /// Provides methods for obtaining and testing matches with the names of the all types of education organization ids in the current model.
+    /// </summary>
     public class EducationOrganizationIdNamesProvider : IEducationOrganizationIdNamesProvider
     {
-        private readonly Lazy<BidirectionalGraph<string, Edge<string>>> _edOrgIdGraph;
         private readonly Lazy<string[]> _concreteEducationOrganizationIdNames;
-        private readonly Lazy<HashSet<(string, string)>> _accessibleSegments;
         private readonly Lazy<List<string>> _sortedEducationOrganizationIdNames; 
 
         public EducationOrganizationIdNamesProvider(IDomainModelProvider domainModelProvider)
         {
-            _edOrgIdGraph = new Lazy<BidirectionalGraph<string, Edge<string>>>(
-                () =>
-                {
-                    // Get the EducationOrganization base entity
-                    var edOrgEntity = domainModelProvider.GetDomainModel().EducationOrganizationBaseEntity();
+            _concreteEducationOrganizationIdNames = new Lazy<string[]>(() =>
+            {
+                // Get the EducationOrganization base entity
+                var edOrgEntity = domainModelProvider.GetDomainModel().EducationOrganizationBaseEntity();
+                
+                var derivedEntities = edOrgEntity.DerivedEntities;
 
-                    // Process all derived entities for their concrete primary key names
-                    var directedGraph = new BidirectionalGraph<string, Edge<string>>();
-
-                    var derivedEntities = edOrgEntity.DerivedEntities;
-
-                    directedGraph.AddVertexRange(derivedEntities.Select(e => e.Identifier.Properties.Single().PropertyName));
-
-                    directedGraph.AddEdgeRange(
-                        derivedEntities.SelectMany(
-                            entity =>
-                            {
-                                string currentEntityPropertyName = entity.Identifier.Properties.Single().PropertyName;
-
-                                // Identify parent key(s) for the EdOrg hierarchy 
-                                var parentEdOrgAssociations = entity.Properties.SelectMany(
-                                    p => p.IncomingAssociations.Where(
-                                        a => a.AssociationType == AssociationViewType.ManyToOne
-                                            && (a.OtherEntity.IsEducationOrganizationBaseEntity()
-                                                || a.OtherEntity.IsEducationOrganizationDerivedEntity())
-                                            && !a.IsSelfReferencing));
-
-                                return parentEdOrgAssociations.SelectMany(
-                                    a => a.OtherEntity.IsEducationOrganizationBaseEntity()
-                                        // For abstract EducationOrganizationId references, add edges for all permutations of other *concrete* EdOrgIds to the current entity property
-                                        ? directedGraph.Vertices.Where(v => v != currentEntityPropertyName).Select(v => new Edge<string>(v, currentEntityPropertyName))
-                                        // For concrete EdOrg associations, just add the single edge
-                                        : new[] { new Edge<string>(a.OtherProperties.Single().PropertyName, currentEntityPropertyName)});
-                            }));
-
-                    return directedGraph;
-                });
-
-            _accessibleSegments = new Lazy<HashSet<(string, string)>>(
-                () =>
-                {
-                    var accessibleSegments = new HashSet<(string, string)>();
-
-                    var allSegments = _edOrgIdGraph.Value.Vertices
-                        .SelectMany(v => GetAccessibleVertices(v).Select(av => (v, av)));
-
-                    foreach (var segment in allSegments)
-                    {
-                        accessibleSegments.Add(segment);
-                    }
-
-                    return accessibleSegments;
-                    
-                    IEnumerable<string> GetAccessibleVertices(string vertex)
-                    {
-                        foreach (var outEdge in _edOrgIdGraph.Value.OutEdges(vertex))
-                        {
-                            // Return the direct edges
-                            yield return outEdge.Target;
-
-                            // Recursively find and return all reachable vertices
-                            foreach (string accessibleVertex in GetAccessibleVertices(outEdge.Target))
-                            {
-                                yield return accessibleVertex;
-                            }
-                        }
-                    }
-
-                });
-
-            _concreteEducationOrganizationIdNames = new Lazy<string[]>(() => _edOrgIdGraph.Value.Vertices.ToArray());
+                return derivedEntities.Select(e => e.Identifier.Properties.Single().PropertyName).ToArray();
+            });
             
             _sortedEducationOrganizationIdNames =
                 new Lazy<List<string>>(
@@ -117,19 +56,7 @@ namespace EdFi.Ods.Api.Security.Authorization
             return _concreteEducationOrganizationIdNames.Value;
         }
 
-        /// <inheritdoc cref="IEducationOrganizationIdNamesProvider.IsEducationOrganizationIdAccessible" />
-        public bool IsEducationOrganizationIdAccessible(
-            string sourceEducationOrganizationIdPropertyName,
-            string targetEducationOrganizationId)
-        {
-            if (sourceEducationOrganizationIdPropertyName == targetEducationOrganizationId)
-            {
-                return true;
-            }
-            
-            return _accessibleSegments.Value.Contains((sourceEducationOrganizationIdPropertyName, targetEducationOrganizationId));
-        }
-
+        /// <inheritdoc cref="IEducationOrganizationIdNamesProvider.IsEducationOrganizationIdName" />
         public bool IsEducationOrganizationIdName(string candidateName)
         {
             return _sortedEducationOrganizationIdNames.Value.BinarySearch(candidateName) >= 0;
