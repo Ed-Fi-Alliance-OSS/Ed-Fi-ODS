795f43ba75bf8f7bc5156fde0ebb544dd34dc15c
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-04-02 17:53:37 -0500

    Eliminate unused Generator-utility classes and move API dependencies into the Ed-Fi-Ods solution projects.

---
+++ Application/EdFi.Ods.Common/Database/NamingConventions/PhysicalNameParts.cs
@@ -0,0 +1,32 @@
+namespace EdFi.Ods.Generator.Database.NamingConventions
+{
+    public class PhysicalNameParts
+    {
+        public PhysicalNameParts(string name, string prefix = null, string suffix = null)
+        {
+            Prefix = prefix;
+            Name = name;
+            Suffix = suffix;
+        }
+        
+        /// <summary>
+        /// Gets or sets the physical naming convention's applied prefix.
+        /// </summary>
+        public string Prefix { get; set; }
+        
+        /// <summary>
+        /// The core physical name of the object.
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the physical naming convention's applied suffix.
+        /// </summary>
+        public string Suffix { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Prefix}{Name}{Suffix}";
+        }
+    }
+}
