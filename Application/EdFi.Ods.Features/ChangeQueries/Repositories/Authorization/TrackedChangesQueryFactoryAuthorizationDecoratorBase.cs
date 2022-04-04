165e0897854412235928e62eb5eae0012c61d306
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-04-03 21:31:32 -0500

    Improved exception messages around security metadata - 403s instead of 500s.

--- Application/EdFi.Ods.Features/ChangeQueries/Repositories/Authorization/TrackedChangesQueryFactoryAuthorizationDecoratorBase.cs
+++ Application/EdFi.Ods.Features/ChangeQueries/Repositories/Authorization/TrackedChangesQueryFactoryAuthorizationDecoratorBase.cs
@@ -6,13 +6,13 @@
 using System;
 using System.Security;
 using System.Security.Claims;
-using EdFi.Common.Utils.Extensions;
-using EdFi.Ods.Api.Security.AuthorizationStrategies.Relationships.Filters;
+using EdFi.Ods.Common.Exceptions;
+using EdFi.Ods.Common.Infrastructure.Filtering;
 using EdFi.Ods.Common.Models;
 using EdFi.Ods.Common.Models.Resource;
+using EdFi.Ods.Common.Security;
 using EdFi.Ods.Common.Security.Claims;
 using EdFi.Ods.Features.ChangeQueries.DomainModelEnhancers;
-using EdFi.Ods.Generator.Database.NamingConventions;
 using SqlKata;
 
 namespace EdFi.Ods.Features.ChangeQueries.Repositories.Authorization
@@ -20,20 +20,20 @@ namespace EdFi.Ods.Features.ChangeQueries.Repositories.Authorization
     public class TrackedChangesQueryFactoryAuthorizationDecoratorBase
     {
         private readonly IAuthorizationContextProvider _authorizationContextProvider;
+        private readonly IAuthorizationFilterApplicationDetailsProvider _authorizationFilterApplicationDetailsProvider;
         private readonly IEdFiAuthorizationProvider _edFiAuthorizationProvider;
-        private readonly IDatabaseNamingConvention _namingConvention;
 
         protected TrackedChangesQueryFactoryAuthorizationDecoratorBase(
             IAuthorizationContextProvider authorizationContextProvider,
             IEdFiAuthorizationProvider edFiAuthorizationProvider,
-            IDatabaseNamingConvention namingConvention,
             IDomainModelProvider domainModelProvider,
-            IDomainModelEnhancer domainModelEnhancer)
+            IDomainModelEnhancer domainModelEnhancer,
+            IAuthorizationFilterApplicationDetailsProvider authorizationFilterApplicationDetailsProvider)
         {
             _authorizationContextProvider = authorizationContextProvider;
             _edFiAuthorizationProvider = edFiAuthorizationProvider;
-            _namingConvention = namingConvention;
-            
+            _authorizationFilterApplicationDetailsProvider = authorizationFilterApplicationDetailsProvider;
+
             domainModelEnhancer.Enhance(domainModelProvider.GetDomainModel());
         }
 
@@ -53,93 +53,28 @@ namespace EdFi.Ods.Features.ChangeQueries.Repositories.Authorization
                 _authorizationContextProvider.GetAction(),
                 entityType);
 
-            var filters = _edFiAuthorizationProvider.GetAuthorizationFilters(authorizationContext);
+            var filterContexts = _edFiAuthorizationProvider.GetAuthorizationFilters(authorizationContext);
 
             var filterIndex = 0;
 
             // Apply authorization filters
-            foreach (var filter in filters)
+            foreach (var filterContext in filterContexts)
             {
-                // TODO: Decompose this conditional logic into an array of injected IDeletedItemsAuthorizationFilterHandler instances
-                if (filter.FilterName == "Namespace")
+                if (!_authorizationFilterApplicationDetailsProvider.TryGetAuthorizationFilterDefinition(
+                        filterContext.FilterName,
+                        out var filterApplicationDetails))
                 {
-                    string namespaceColumnName =
-                        _namingConvention.ColumnName($"{ChangeQueriesDatabaseConstants.OldKeyValueColumnPrefix}Namespace");
-                    
-                    if (filter.ClaimValues.Length == 1)
-                    {
-                        query.Where(
-                            q => q
-                                .WhereNotNull(namespaceColumnName)
-                                .WhereLike(namespaceColumnName, filter.ClaimValues[0]));
-                    }
-                    else if (filter.ClaimValues.Length > 1)
-                    {
-                        query.Where(
-                            q => q
-                                .WhereNotNull(namespaceColumnName) 
-                                .Where(
-                                q2 =>
-                                {
-                                    filter.ClaimValues.ForEach(cv => q2.OrWhereLike(namespaceColumnName, cv));
-
-                                    return q2;
-                                }));
-                    }
-                    else
-                    {
-                        // This should never happen
-                        throw new SecurityException("No namespaces found in claims.");
-                    }
+                    throw new EdFiSecurityException($"Filter '{filterContext.FilterName}' was not found. Are you using the correct authorization strategy for the '{resource.FullName}' resource and the API client's claim set?");
                 }
-                // Determine if this matches the relationship-based authorization strategy naming pattern
-                else if (filter.FilterName.Contains("To"))
-                {
-                    if (filter.ClaimEndpointName == filter.SubjectEndpointName)
-                    {
-                        // Apply claim value criteria directly to the column value
-                        query.WhereIn($"c.{_namingConvention.ColumnName($"{ChangeQueriesDatabaseConstants.OldKeyValueColumnPrefix}{filter.ClaimEndpointName}")}", filter.ClaimValues);
-                    }
-                    else
-                    {
-                        // Endpoint names do not match -- use an authorization view join
-                        // TODO: For v5.3, view and filter names don't match due to the new EdOrgIdToEdOrgId auth view generalization
-                        var filterDetails = RelationshipsAuthorizationFilters.GetViewFilterApplicationDetails(filter.FilterName);
 
-                        string viewName = filterDetails.ViewName;
-                        
-                        string trackedChangesPropertyName = resource.Entity.IsDerived 
-                            ? GetBasePropertyNameForSubjectEndpointName() 
-                            : filter.SubjectEndpointName;
-
-                        // Generalize relationships-based naming convention
-                        query.Join(
-                            $"auth.{_namingConvention.IdentifierName(viewName)} AS rba{filterIndex}",
-                            $"c.{_namingConvention.ColumnName($"{ChangeQueriesDatabaseConstants.OldKeyValueColumnPrefix}{trackedChangesPropertyName}")}",
-                            $"rba{filterIndex}.{_namingConvention.ColumnName(filterDetails.ViewTargetEndpointName)}");
-
-                        // Apply claim value criteria
-                        query.WhereIn($"rba{filterIndex}.{_namingConvention.ColumnName(filterDetails.ViewSourceEndpointName)}", filter.ClaimValues);
-                    }
-                }
-                else
-                {
-                    throw new SecurityException(
-                        $"Support for filtering tracked changes with filter '{filter.FilterName}' has not been implemented.");
-                }
+                filterApplicationDetails.TrackedChangesCriteriaApplicator(
+                    filterApplicationDetails,
+                    filterContext,
+                    resource,
+                    filterIndex,
+                    query);
 
                 filterIndex++;
-
-                string GetBasePropertyNameForSubjectEndpointName()
-                {
-                    if (!resource.Entity.PropertyByName.TryGetValue(filter.SubjectEndpointName, out var entityProperty))
-                    {
-                        throw new Exception(
-                            $"Unable to find property '{filter.SubjectEndpointName}' on entity '{resource.Entity.FullName}'.");
-                    }
-
-                    return entityProperty.BaseProperty.PropertyName;
-                }
             }
         }
     }
