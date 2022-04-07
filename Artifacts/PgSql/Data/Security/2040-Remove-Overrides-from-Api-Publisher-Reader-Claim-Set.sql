1dccda50364e7738e8be20580694911744d11f31
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-04-07 13:13:55 -0500

    Cleanup of the generated scripts.

--- Artifacts/PgSql/Data/Security/2040-Remove-Overrides-from-Api-Publisher-Reader-Claim-Set.sql
+++ Artifacts/PgSql/Data/Security/2040-Remove-Overrides-from-Api-Publisher-Reader-Claim-Set.sql
@@ -845,8 +845,4 @@ BEGIN
     claim_id_stack := (select claim_id_stack[1:array_upper(claim_id_stack, 1) - 1]);
 
     COMMIT;
-
-    -- TODO: Remove - For interactive development only
-    -- SELECT dbo.GetAuthorizationMetadataDocument();
-    -- ROLLBACK;
 END $$;
