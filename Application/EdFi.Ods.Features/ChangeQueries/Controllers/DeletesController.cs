70736bf0d6dba7662d54e617be6b572520d83128
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-04-19 17:05:24 -0500

    Added query parameter validation to KeyChanges and Deletes controllers using a shared extension method.

--- Application/EdFi.Ods.Features/ChangeQueries/Controllers/DeletesController.cs
+++ Application/EdFi.Ods.Features/ChangeQueries/Controllers/DeletesController.cs
@@ -3,14 +3,17 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
+using EdFi.Ods.Api.ExceptionHandling;
 using EdFi.Ods.Api.Helpers;
 using EdFi.Ods.Api.Security.Authorization;
 using EdFi.Ods.Common.Configuration;
 using EdFi.Ods.Common.Constants;
 using EdFi.Ods.Common.Models;
 using EdFi.Ods.Common.Models.Queries;
+using EdFi.Ods.Common.Models.Validation;
 using EdFi.Ods.Common.Security.Claims;
 using EdFi.Ods.Common.Serialization;
 using EdFi.Ods.Features.ChangeQueries.Repositories.DeletedItems;
@@ -34,6 +37,8 @@ namespace EdFi.Ods.Features.ChangeQueries.Controllers
         private readonly IAuthorizationContextProvider _authorizationContextProvider;
         private readonly IResourceClaimUriProvider _resourceClaimUriProvider;
         private readonly ISecurityRepository _securityRepository;
+        private readonly int _defaultPageLimitSize;
+
         private readonly ILog _logger = LogManager.GetLogger(typeof(DeletesController));
         private readonly bool _isEnabled;
 
@@ -43,6 +48,7 @@ namespace EdFi.Ods.Features.ChangeQueries.Controllers
             IAuthorizationContextProvider authorizationContextProvider,
             IResourceClaimUriProvider resourceClaimUriProvider,
             ISecurityRepository securityRepository,
+            IDefaultPageSizeLimitProvider defaultPageSizeLimitProvider,
             ApiSettings apiSettings)
         {
             _domainModelProvider = domainModelProvider;
@@ -50,6 +56,8 @@ namespace EdFi.Ods.Features.ChangeQueries.Controllers
             _authorizationContextProvider = authorizationContextProvider;
             _resourceClaimUriProvider = resourceClaimUriProvider;
             _securityRepository = securityRepository;
+
+            _defaultPageLimitSize = defaultPageSizeLimitProvider.GetDefaultPageSizeLimit();
 
             _isEnabled = apiSettings.IsFeatureEnabled(ApiFeature.ChangeQueries.GetConfigKeyName());
         }
@@ -63,19 +71,25 @@ namespace EdFi.Ods.Features.ChangeQueries.Controllers
 
                 return ControllerHelpers.NotFound();
             }
-            
+
             var resourceClass = _domainModelProvider.GetDomainModel().ResourceModel.GetResourceByApiCollectionName(schema, resource);
 
             if (resourceClass == null)
             {
                 return ControllerHelpers.NotFound();
             }
-            
+
+            var parameterMessages = urlQueryParametersRequest.Validate(_defaultPageLimitSize).ToArray();
+
+            if (parameterMessages.Any())
+            {
+                return BadRequest(ErrorTranslator.GetErrorMessage(string.Join(" ", parameterMessages)));
+            }
+
             // Set authorization context (should this be moved?)
             _authorizationContextProvider.SetResourceUris(_resourceClaimUriProvider.GetResourceClaimUris(resourceClass));
             _authorizationContextProvider.SetAction(_securityRepository.GetActionByName("ReadChanges").ActionUri);
 
-            // TODO: Validate the parameter here rather than deeper in the call stack
             var queryParameters = new QueryParameters(urlQueryParametersRequest);
 
             var deletedItemsResponse = await _deletedItemsResourceDataProvider.GetResourceDataAsync(resourceClass, queryParameters);
