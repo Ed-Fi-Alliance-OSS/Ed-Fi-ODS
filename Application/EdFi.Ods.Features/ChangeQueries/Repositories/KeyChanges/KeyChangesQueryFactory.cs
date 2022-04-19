a073550e4c0773487254f0ff5ff951ce18b4cd62
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-04-19 17:24:06 -0500

    Removed commented out code from KeyChangesQueryFactory.

--- Application/EdFi.Ods.Features/ChangeQueries/Repositories/KeyChanges/KeyChangesQueryFactory.cs
+++ Application/EdFi.Ods.Features/ChangeQueries/Repositories/KeyChanges/KeyChangesQueryFactory.cs
@@ -34,31 +34,6 @@ namespace EdFi.Ods.Features.ChangeQueries.Repositories.KeyChanges
 
         private Query CreateTemplateQuery(Resource resource)
         {
-            /*
-            var changeWindow = new Query("tracked_changes_edfi.studentschoolassociation AS c")
-                .Select("c.id")
-                .SelectRaw("MIN(c.ChangeVersion) as initialchangeversion")
-                .SelectRaw("MAX(c.ChangeVersion) as finalchangeversion")
-                .GroupBy("c.id");
-
-            var query = new Query("changewindow as cw")
-                .With("changewindow", changeWindow)
-                .Join("tracked_changes_edfi.studentschoolassociation as c_old", 
-                    j => j.On("cw.id", "c_old.id")
-                            .On("cw.initialchangeversion", "c_old.changeversion"))
-                .Join("tracked_changes_edfi.studentschoolassociation as c_new", 
-                    j => j.On("cw.id", "c_new.id")
-                            .On("cw.finalchangeversion", "c_new.changeversion"))
-                .Select("cw.id", "cw.finalchangeversion as changeversion",
-                    "c_old.oldentrydate AS oldentrydate", "c_new.newentrydate AS newentrydate",
-                    "c_old.oldschoolid AS oldschoolid", "c_new.newschoolid AS newschoolid",        
-                    "c_old.oldstudentuniqueid AS oldstudentuniqueid", "c_new.newstudentuniqueid AS newstudentuniqueid"
-                    )
-                .OrderBy("cw.finalchangeversion")
-                .Skip(0)
-                .Limit(25)
-             */
-
             // Derive column names
             string idColumnFqn = $"{ChangeQueriesDatabaseConstants.TrackedChangesAlias}.{_namingConvention.ColumnName("Id")}";
             var changeVersionColumnFqn = $"{ChangeQueriesDatabaseConstants.TrackedChangesAlias}.{_namingConvention.ColumnName(ChangeQueriesDatabaseConstants.ChangeVersionColumnName)}";
