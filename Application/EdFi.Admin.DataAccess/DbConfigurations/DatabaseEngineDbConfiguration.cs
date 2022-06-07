4ce8d8de4d232f3155322aae224b0b58ac8eeb29
Eric Stockdale <ericstockdale@gmail.com>
2022-06-07 13:39:25 -0700

    Added License and Copyright Notification

--- Application/EdFi.Admin.DataAccess/DbConfigurations/DatabaseEngineDbConfiguration.cs
+++ Application/EdFi.Admin.DataAccess/DbConfigurations/DatabaseEngineDbConfiguration.cs
@@ -1,7 +1,11 @@
-﻿using EdFi.Common.Configuration;
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.Common.Configuration;
 using Npgsql;
 using System.Data.Entity;
-using System.Data.Entity.Core.Common;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.SqlServer;
 using System.Data.SqlClient;
