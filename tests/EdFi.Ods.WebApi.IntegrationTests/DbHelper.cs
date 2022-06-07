8aae6ef355eae759f5a1d96b0b12df02302dee16
Eric Stockdale <ericstockdale@gmail.com>
2022-06-07 13:45:34 -0700

    Added License and Copyright Notification

--- tests/EdFi.Ods.WebApi.IntegrationTests/DbHelper.cs
+++ tests/EdFi.Ods.WebApi.IntegrationTests/DbHelper.cs
@@ -1,13 +1,14 @@
-﻿using EdFi.Common.Configuration;
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.Common.Configuration;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
 using NUnit.Framework;
 using System.Data;
 using System.Data.SqlClient;
-// SPDX-License-Identifier: Apache-2.0
-// Licensed to the Ed-Fi Alliance under one or more agreements.
-// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
-// See the LICENSE and NOTICES files in the project root for more information.
 
 namespace EdFi.Ods.WebApi.IntegrationTests
 {
