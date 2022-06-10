f54341f8332d588ace3d29fe54ad5cb028ab3e45
Eric Stockdale <ericstockdale@gmail.com>
2022-06-10 11:13:36 -0700

    Removed unused using statements

--- tests/EdFi.Ods.WebApi.CompositeSpecFlowTests/EnvironmentSetupTests.cs
+++ tests/EdFi.Ods.WebApi.CompositeSpecFlowTests/EnvironmentSetupTests.cs
@@ -3,18 +3,15 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System.Data;
-using System.Data.SqlClient;
+using Dapper;
+using EdFi.Common.Configuration;
+using EdFi.Ods.Common.Database;
+using NUnit.Framework;
+using Shouldly;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Dapper;
-using EdFi.Common.Configuration;
-using EdFi.Ods.Common.Database;
-using Npgsql;
-using NUnit.Framework;
-using Shouldly;
 
 namespace EdFi.Ods.WebApi.CompositeSpecFlowTests
 {
