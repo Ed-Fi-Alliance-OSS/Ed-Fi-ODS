8fa3b8a72cd2e5543386d5880f5777877e78382f
Axel Marquez <axel_marqz@hotmail.com>
2022-07-13 23:23:32 -0500

    Execute the tests in batches and follow the dependencies order

---
+++ Utilities/DataLoading/EdFi.SmokeTest.Console/Application/DependenciesSorter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using EdFi.LoadTools.ApiClient;
+using EdFi.LoadTools.SmokeTest;
+
+namespace EdFi.SmokeTest.Console.Application
+{
+    public class DependenciesSorter : Dictionary<Type, Func<IEnumerable<Dependency>, IEnumerable<Dependency>>>, IDependenciesSorter
+    { }
+}
