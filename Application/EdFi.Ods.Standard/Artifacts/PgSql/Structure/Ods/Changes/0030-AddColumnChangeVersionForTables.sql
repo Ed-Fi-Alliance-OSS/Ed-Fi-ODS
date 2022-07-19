0357943b97eedc3e0b5e9d6b2e738cab71c35566
Eric Stockdale <ericstockdale@gmail.com>
2022-07-19 12:04:02 -0700

    Make table_name values lowercase

--- Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/Changes/0030-AddColumnChangeVersionForTables.sql
+++ Application/EdFi.Ods.Standard/Artifacts/PgSql/Structure/Ods/Changes/0030-AddColumnChangeVersionForTables.sql
@@ -1,6 +1,6 @@
--- SPDX-license-Identifier: Apache-2.0
--- licensed to the Ed-Fi Alliance under one or more agreements.
--- The Ed-Fi Alliance licenses this file to you under the Apache license, Version 2.0.
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
 DO $$
