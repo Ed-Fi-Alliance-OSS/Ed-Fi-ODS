5226b65e62ad7ca8646f1f4f5025be92b3379142
Ben Tillman <ben.tillman@accenture.com>
2022-08-17 19:10:39 -0500

    Add always failing test to see if GH Action catches it

---
+++ Application/EdFi.Ods.Tests/EdFi.Ods.Api/Validation/NoDangerousTextAttributeTests - Copy.cs
@@ -0,0 +1,20 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.TestFixture;
+using NUnit.Framework;
+using Test.Common;
+
+namespace EdFi.Ods.Tests.EdFi.Ods.Api.Validation
+{
+ public class When_validating_always_failing_test : TestFixtureBase
+    {
+        [Assert]
+        public void Should_always_fail()
+        {
+            Assert.Equals(1, 2);
+        }
+    }
+}
