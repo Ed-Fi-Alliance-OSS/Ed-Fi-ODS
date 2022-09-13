7204f867109519f2400eb93ba25ed64664de6c7d
semalai <m.semalaiappan@gmail.com>
2022-09-13 15:06:46 -0500

    InformationalVersion updated to 6.1

--- Application/EdFi.Ods.Api/Constants/ApiVersionConstants.cs
+++ Application/EdFi.Ods.Api/Constants/ApiVersionConstants.cs
@@ -27,12 +27,12 @@ namespace EdFi.Ods.Api.Constants
         /// <summary>
         /// Informational version of the ods api.
         /// </summary>
-        public const string InformationalVersion = "6.0";
+        public const string InformationalVersion = "6.1";
 
         /// <summary>
         /// Semantic version of the ods api.
         /// </summary>
-        public const string Version = "6.0";
+        public const string Version = "6.1";
 
         /// <summary>
         /// Suite version of the ods api.
