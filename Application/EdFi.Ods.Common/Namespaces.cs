fce373095794ed783f8649403330fff10595ec7e
Axel Marquez <axel_marqz@hotmail.com>
2022-09-23 13:29:47 -0500

    Add dummy code change 2 to trigger GitHub workflows

--- Application/EdFi.Ods.Common/Namespaces.cs
+++ Application/EdFi.Ods.Common/Namespaces.cs
@@ -3,7 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-// Dummy code change to trigger GitHub workflows (Do not merge)
+// Dummy code change 2 to trigger GitHub workflows (Do not merge)
 
 namespace EdFi.Ods.Common
 {
