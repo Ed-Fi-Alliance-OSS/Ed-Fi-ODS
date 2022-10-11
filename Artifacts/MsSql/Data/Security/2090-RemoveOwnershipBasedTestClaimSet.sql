42881513f224fed9a69bb9d3a55f2bdd2396b7f2
semalaiappan <m.semalaiappan@gmail.com>
2022-10-11 09:07:58 -0500

    Remove OwnershipBased Test Claimset

---
+++ Artifacts/MsSql/Data/Security/2090-RemoveOwnershipBasedTestClaimSet.sql
@@ -0,0 +1,25 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+DECLARE @claimSetId INT;
+DECLARE @applicationId INT;
+
+SELECT @applicationId = (SELECT applicationid FROM [dbo].[Applications] WHERE [ApplicationName] = 'Ed-Fi ODS API');
+
+IF  EXISTS(SELECT 1 FROM [dbo].[ClaimSets] WHERE [ClaimSetName] ='Ownership Based Test' AND Application_ApplicationId = @applicationId)
+BEGIN
+
+SELECT @claimSetId = claimsetid FROM [dbo].[ClaimSets] 
+WHERE [ClaimSetName] ='Ownership Based Test' AND Application_ApplicationId = @applicationId
+
+DELETE FROM dbo.ClaimSetResourceClaimActionAuthorizationStrategyOverrides WHERE ClaimSetResourceClaimActionId 
+IN (SELECT ClaimSetResourceClaimActionId FROM dbo.claimsetresourceclaimactions WHERE claimsetid =@claimSetId)
+
+DELETE FROM dbo.ClaimSetResourceClaimActions where claimsetid =@claimSetId
+
+DELETE FROM [dbo].[ClaimSets]  WHERE [ClaimSetName] ='Ownership Based Test' AND Application_ApplicationId = @applicationId
+
+END
+
---
+++ Artifacts/PgSql/Data/Security/2090-RemoveOwnershipBasedTestClaimSet.sql
@@ -0,0 +1,26 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+DO $$
+DECLARE
+    application_id INTEGER;
+    claim_set_id INTEGER;
+BEGIN
+    SELECT applicationid INTO application_id  FROM dbo.applications WHERE ApplicationName = 'Ed-Fi ODS API';
+
+    IF  EXISTS(SELECT 1 FROM dbo.ClaimSets WHERE ClaimSetName ='Ownership Based Test' AND Application_ApplicationId = application_Id) THEN
+
+        SELECT claimsetid INTO claim_set_id FROM dbo.ClaimSets 
+        WHERE ClaimSetName ='Ownership Based Test' AND Application_ApplicationId = application_Id;
+
+        DELETE FROM dbo.ClaimSetResourceClaimActionAuthorizationStrategyOverrides WHERE ClaimSetResourceClaimActionId 
+        IN (SELECT ClaimSetResourceClaimActionId FROM dbo.claimsetresourceclaimactions WHERE claimsetid =claim_set_id);
+
+        DELETE FROM dbo.ClaimSetResourceClaimActions where claimsetid =claim_set_id;
+
+        DELETE FROM dbo.ClaimSets  WHERE ClaimSetName ='Ownership Based Test' AND Application_ApplicationId = application_Id;
+    END IF;
+
+END $$;
