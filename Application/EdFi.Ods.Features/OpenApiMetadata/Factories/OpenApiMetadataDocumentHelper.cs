311c860d194ea8090cca0bf7d613aef30666f958
Ben Tillman <ben.tillman@accenture.com>
2022-10-27 23:10:50 -0500

    ODS-5608 - Add x-nullable property to metadata for nullable properties (#598)
    * Add nullable property to schema that is set when resource property is nullable
    
    This way when openapi generator creates the sdk from the metadata
    it will mark the property as nullable and have EmitDefaultValue = true
    
    * Make property nullable so it only shows up when true
    
    * Use existing method instead of creating a new one

--- Application/EdFi.Ods.Features/OpenApiMetadata/Factories/OpenApiMetadataDocumentHelper.cs
+++ Application/EdFi.Ods.Features/OpenApiMetadata/Factories/OpenApiMetadataDocumentHelper.cs
@@ -67,7 +67,7 @@ namespace EdFi.Ods.Features.OpenApiMetadata.Factories
 
         public static Schema CreatePropertySchema(ResourceProperty resourceProperty)
         {
-            return new Schema
+            var schema =  new Schema
             {
                 type = PropertyType(resourceProperty),
                 format = resourceProperty.PropertyType.ToOpenApiFormat(),
@@ -77,6 +77,13 @@ namespace EdFi.Ods.Features.OpenApiMetadata.Factories
                 isDeprecated = GetIsDeprecated(resourceProperty),
                 deprecatedReasons = GetDeprecatedReasons(resourceProperty)
             };
+
+            if (resourceProperty.PropertyType.IsNullable)
+            {
+                schema.nullable = true;
+            }
+
+            return schema;
         }
 
         public static bool? GetIsIdentity(ResourceProperty resourceProperty)
@@ -145,15 +152,7 @@ namespace EdFi.Ods.Features.OpenApiMetadata.Factories
                                            {
                                                IsRequired = x.IsIdentifying || reference.IsRequired,
                                                PropertyName = x.JsonPropertyName,
-                                               Schema =
-                                                        new Schema
-                                                        {
-                                                            type = PropertyType(x),
-                                                            format = PropertyFormat(x),
-                                                            description = PropertyDescription(x),
-                                                            maxLength = GetMaxLength(x),
-                                                            isIdentity = GetIsIdentity(x)
-                                                        }
+                                               Schema = CreatePropertySchema(x)
                                            })
                                       .OrderBy(x => x.PropertyName)
                                       .ToList();
