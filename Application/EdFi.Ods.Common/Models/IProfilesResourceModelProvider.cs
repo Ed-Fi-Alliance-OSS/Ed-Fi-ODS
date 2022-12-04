4d80947e4f9e6e6b85f70f46d46a6b957b22866c
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2022-12-04 15:22:21 -0600

    Split out ProfileResourceModelProvider implementation from interface.

--- Application/EdFi.Ods.Common/Models/IProfilesResourceModelProvider.cs
+++ Application/EdFi.Ods.Common/Models/IProfilesResourceModelProvider.cs
@@ -3,10 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System;
-using System.Collections.Concurrent;
 using EdFi.Ods.Common.Exceptions;
-using EdFi.Ods.Common.Metadata;
 using EdFi.Ods.Common.Models.Resource;
 
 namespace EdFi.Ods.Common.Models
@@ -22,34 +19,6 @@ namespace EdFi.Ods.Common.Models
         /// <param name="profileName">The name of the profile for which a model should be created.</param>
         /// <returns>The <see cref="ProfileResourceModel"/> for the specified profile.</returns>
         ProfileResourceModel GetProfileResourceModel(string profileName);
-    }
-
-    public class ProfileResourceModelProvider : IProfileResourceModelProvider
-    {
-        private readonly ConcurrentDictionary<string, ProfileResourceModel> _modelByProfileName
-            = new ConcurrentDictionary<string, ProfileResourceModel>(StringComparer.InvariantCultureIgnoreCase);
-        private readonly IProfileMetadataProvider _profileMetadataProvider;
-
-        private readonly Lazy<ResourceModel> _resourceModel;
-
-        public ProfileResourceModelProvider(
-            IResourceModelProvider resourceModelProvider,
-            IProfileMetadataProvider profileMetadataProvider)
-        {
-            _profileMetadataProvider = profileMetadataProvider;
-
-            _resourceModel = new Lazy<ResourceModel>(resourceModelProvider.GetResourceModel);
-        }
-
-        public ProfileResourceModel GetProfileResourceModel(string profileName)
-        {
-            return _modelByProfileName.GetOrAdd(
-                profileName,
-                pn =>
-                    new ProfileResourceModel(
-                        _resourceModel.Value,
-                        _profileMetadataProvider.GetProfileDefinition(profileName)));
-        }
     }
 
     public class ProfilePassthroughResourceModelProvider : IProfileResourceModelProvider
