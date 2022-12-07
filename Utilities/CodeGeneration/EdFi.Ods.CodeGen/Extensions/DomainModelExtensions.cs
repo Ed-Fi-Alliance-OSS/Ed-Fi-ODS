3bc3e782b016596ff36bd3ec1c040a41f3f6a712
Ben Tillman <ben.tillman@accenture.com>
2022-12-06 19:22:07 -0600

    Check for min and max value and use those for range, otherwise fall back to previous logic

--- Utilities/CodeGeneration/EdFi.Ods.CodeGen/Extensions/DomainModelExtensions.cs
+++ Utilities/CodeGeneration/EdFi.Ods.CodeGen/Extensions/DomainModelExtensions.cs
@@ -115,11 +115,23 @@ namespace EdFi.Ods.CodeGen.Extensions
                 case DbType.Decimal:
 
                     return string.Format(
-                        "[Range(typeof(decimal), \"-{0}.{1}\", \"{0}.{1}\")]",
-                        new string(
-                            '9',
-                            property.EntityProperty.PropertyType.Precision - property.EntityProperty.PropertyType.Scale),
-                        new string('9', property.EntityProperty.PropertyType.Scale));
+                        "[Range(typeof(decimal), \"-{0}\", \"{1}\")]",
+                        property.EntityProperty.PropertyType.MinValue.HasValue
+                            ? property.EntityProperty.PropertyType.MinValue.Value
+                            : string.Format(
+                                "{0}.{1}",
+                                new string(
+                                    '9',
+                                    property.EntityProperty.PropertyType.Precision - property.EntityProperty.PropertyType.Scale),
+                                new string('9', property.EntityProperty.PropertyType.Scale)),
+                        property.EntityProperty.PropertyType.MaxValue.HasValue
+                            ? property.EntityProperty.PropertyType.MaxValue.Value
+                            : string.Format(
+                                "{0}.{1}",
+                                new string(
+                                    '9',
+                                    property.EntityProperty.PropertyType.Precision - property.EntityProperty.PropertyType.Scale),
+                                new string('9', property.EntityProperty.PropertyType.Scale)));
 
                 case DbType.Currency:
                     return "[Range(typeof(decimal), \"-922337203685477.5808\", \"922337203685477.5807\")]";
