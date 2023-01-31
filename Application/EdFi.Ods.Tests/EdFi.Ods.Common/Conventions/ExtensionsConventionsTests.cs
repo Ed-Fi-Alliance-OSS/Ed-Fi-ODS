9c230a4b61fcc36a25c63ca1d2a94a27d7763a6b
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-01-31 15:59:14 -0600

    Updated unit test for modified exception message.

--- Application/EdFi.Ods.Tests/EdFi.Ods.Common/Conventions/ExtensionsConventionsTests.cs
+++ Application/EdFi.Ods.Tests/EdFi.Ods.Common/Conventions/ExtensionsConventionsTests.cs
@@ -246,7 +246,7 @@ namespace EdFi.Ods.Tests.EdFi.Ods.Common.Conventions
                         {
                             new DomainModelDefinitionsProvider(), new EdFiDomainModelDefinitionsProvider()
                         },
-                        new IDomainModelDefinitionsTransformer[0]).GetDomainModel();
+                        Array.Empty<IDomainModelDefinitionsTransformer>()).GetDomainModel();
 
                 ExtensionsConventions.GetAggregateExtensionMemberName(
                     domainModel.Entities.FirstOrDefault(e => e.Name == "StaffLeaveExtension"));
@@ -257,7 +257,7 @@ namespace EdFi.Ods.Tests.EdFi.Ods.Common.Conventions
             {
                 AssertHelper.All(
                     () => Assert.That(ActualException.GetType(), Is.EqualTo(typeof(Exception))),
-                    () => Assert.That(ActualException.Message, Is.EqualTo("The supplied 'aggregateExtensionEntity' was not an aggregate extension."))
+                    () => Assert.That(ActualException.Message, Is.EqualTo("The supplied 'aggregateExtensionEntity' is not a top-level aggregate extension entity."))
                 );
             }
         }
