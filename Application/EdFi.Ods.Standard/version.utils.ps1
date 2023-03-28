d6b4594b8c8d60dd7d8b080d29a6b869c620e056
Jesus Flores <43448209+simpat-jesus@users.noreply.github.com>
2023-03-28 16:02:51 -0600

    [ODS-5770] Update versionmap.json when new standard is released (#687)
    * Added version map file
    
    * Add helper script to get standard versions, and repo mappings

---
+++ Application/EdFi.Ods.Standard/version.utils.ps1
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+[CmdLetBinding()]
+param(
+    [string]
+    [ValidateSet("Get-Versions", "Get-StandardTag", "Get-TpdmTag")]
+    $Command = "Get-Versions",
+
+    [string]
+    $StandardVersion
+)
+
+function Get-Versions {
+    Get-ChildItem -Path ./Standard -Directory -Force -ErrorAction SilentlyContinue | Select -ExpandProperty Name
+}
+
+function Get-StandardTag {
+    param (
+        [string]
+        $StandardVersion
+    )
+
+    (Get-Content versionmap.json | ConvertFrom-Json).'Ed-Fi-Standard'.$StandardVersion
+}
+
+function Get-TpdmTag {
+    param (
+        [string]
+        $StandardVersion
+    )
+
+    (Get-Content versionmap.json | ConvertFrom-Json).'Ed-Fi-TPDM-Artifacts'.$StandardVersion
+}
+
+try {
+    switch ($Command) {
+        Get-Versions { Get-Versions }
+        Get-StandardTag { Get-StandardTag($standardversion) }
+        Get-TpdmTag { Get-TpdmTag($standardversion) }
+    }
+    exit 0
+} catch [Exception] {
+    Write-Host
+    Write-Error $_.Exception.Message
+    exit 1
+}
