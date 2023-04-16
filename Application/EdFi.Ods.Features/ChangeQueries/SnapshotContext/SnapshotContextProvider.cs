8f8a5c8670dab796b39774a44449fc45883ce370
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-04-16 01:43:55 -0500

    IHttpContextStorageTransferKeys cleanup.

--- Application/EdFi.Ods.Features/ChangeQueries/SnapshotContext/SnapshotContextProvider.cs
+++ Application/EdFi.Ods.Features/ChangeQueries/SnapshotContext/SnapshotContextProvider.cs
@@ -3,12 +3,11 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using System.Collections.Generic;
 using EdFi.Ods.Common.Context;
 
 namespace EdFi.Ods.Features.ChangeQueries.SnapshotContext
 {
-    public class SnapshotContextProvider : ISnapshotContextProvider, IHttpContextStorageTransferKeys
+    public class SnapshotContextProvider : ISnapshotContextProvider
     {
         private const string SnapshotContextKeyName = "SnapshotContextProvider.SnapshotContext";
         private readonly IContextStorage _contextStorage;
@@ -34,19 +33,6 @@ namespace EdFi.Ods.Features.ChangeQueries.SnapshotContext
         public void SetSnapshotContext(SnapshotContext snapshotContext)
         {
             _contextStorage.SetValue(SnapshotContextKeyName, snapshotContext);
-        }
-
-        /// <summary>
-        /// Declare the <see cref="IContextStorage"/> keys that are read and written by the component
-        /// and should be transferred from <see cref="HttpContext"/> to <see cref="CallContext"/> for background Tasks.
-        /// </summary>
-        /// <returns>A list of keys to be transferred from <see cref="HttpContext"/> to <see cref="CallContext"/>.</returns>
-        IReadOnlyList<string> IHttpContextStorageTransferKeys.GetKeys()
-        {
-            return new[]
-            {
-                SnapshotContextKeyName
-            };
         }
     }
 }
