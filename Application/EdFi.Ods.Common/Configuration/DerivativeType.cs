e19f8d4d66838d8fd0fcba4d017c8f4dcd4b49a3
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-04-16 01:30:40 -0500

    Corrected the derivative type enumeration to include snapshot instead of read/write.

--- Application/EdFi.Ods.Common/Configuration/DerivativeType.cs
+++ Application/EdFi.Ods.Common/Configuration/DerivativeType.cs
@@ -2,12 +2,21 @@ using EdFi.Common;
 
 namespace EdFi.Ods.Common.Configuration;
 
+/// <summary>
+/// Contains entries for the supported types of "derivative" ODS database instances.
+/// </summary>
 public class DerivativeType : Enumeration<DerivativeType>
 {
     private DerivativeType(int value, string displayName)
         : base(value, displayName) { }
 
-    public static readonly DerivativeType ReadWrite = new DerivativeType(1, "ReadWrite");
-
+    /// <summary>
+    /// A read-replica of the primary ODS database that is continuously kept up-to-date.
+    /// </summary>
     public static readonly DerivativeType ReadReplica = new DerivativeType(2, "ReadReplica");
+    
+    /// <summary>
+    /// A read-only, point-in-time snapshot or clone of the primary ODS instance.
+    /// </summary>
+    public static readonly DerivativeType Snapshot = new DerivativeType(1, "Snapshot");
 }
