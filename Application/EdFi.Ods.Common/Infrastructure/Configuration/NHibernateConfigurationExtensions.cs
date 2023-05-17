3f16f0734a0df9ed24a5f7e88f8c085f50b35e07
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-05-17 15:30:30 -0500

    Fixed issue where a key change on a resource was not being validated using on the new key value.

--- Application/EdFi.Ods.Common/Infrastructure/Configuration/NHibernateConfigurationExtensions.cs
+++ Application/EdFi.Ods.Common/Infrastructure/Configuration/NHibernateConfigurationExtensions.cs
@@ -3,6 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System.Collections.Generic;
 using EdFi.Ods.Common.Infrastructure.Interceptors;
 using EdFi.Ods.Common.Infrastructure.Listeners;
 using NHibernate.Event;
@@ -11,12 +12,14 @@ namespace EdFi.Ods.Common.Infrastructure.Configuration
 {
     public static class NHibernateConfigurationExtensions
     {
-        public static void AddCreateDateHooks(this NHibernate.Cfg.Configuration configuration)
+        public static void AddCreateDateHooks(
+            this NHibernate.Cfg.Configuration configuration,
+            IEnumerable<IEntityValidator> entityValidators)
         {
             configuration.Interceptor = new CreateDateBasedTransientInterceptor();
             configuration.SetListener(ListenerType.PreInsert, new EdFiOdsPreInsertListener());
             configuration.SetListener(ListenerType.PostInsert, new EdFiOdsPostInsertListener());
-            configuration.SetListener(ListenerType.PostUpdate, new EdFiOdsPostUpdateEventListener());
+            configuration.SetListener(ListenerType.PostUpdate, new EdFiOdsPostUpdateEventListener(entityValidators));
         }
     }
 }
