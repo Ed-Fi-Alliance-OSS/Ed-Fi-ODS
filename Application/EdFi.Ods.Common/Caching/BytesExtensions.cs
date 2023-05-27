3b2ce7f6f1d98fd10d01c73fe8911bb26842cc69
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-05-27 16:56:16 -0500

    BytesExtensions improvements for supporting string lists in signatures.

--- Application/EdFi.Ods.Common/Caching/BytesExtensions.cs
+++ Application/EdFi.Ods.Common/Caching/BytesExtensions.cs
@@ -4,12 +4,23 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Standart.Hash.xxHash;
 
 namespace EdFi.Ods.Common.Caching;
 
 public static class BytesExtensions
 {
+    private static readonly byte[] _enumerableItemDelimiter = { 0 };
+
+    public static byte[] GetBytes(this byte[] bytesValue)
+    {
+        return bytesValue;
+    }
+    
     public static byte[] GetBytes<T>(this T value)
     {
         if (value is int intValue)
@@ -17,21 +28,59 @@ public static class BytesExtensions
             return BitConverter.GetBytes(intValue);
         }
 
-        if (value is string stringValue)
-        {
-            return Encoding.UTF8.GetBytes(stringValue);
-        }
-
         if (value is ulong ulongValue)
         {
             return BitConverter.GetBytes(ulongValue);
+        }
+
+        if (value is string stringValue)
+        {
+            return Encoding.UTF8.GetBytes(stringValue);
         }
 
         if (value is byte[] bytesValue)
         {
             return bytesValue;
         }
+
+        if (value is IList<string> stringListValue)
+        {
+            return GetBytes(stringListValue);
+        }
         
         throw new NotImplementedException($"Support for extracting bytes from type '{typeof(T)}' has not been implemented.");
+    }
+
+    private static byte[] GetBytes(this IList<string> value)
+    {
+        var byteArrays = value.Select(i => i.GetBytes()).ToList();
+        
+        return GetArrayBytes(byteArrays);
+    }
+    
+    private static byte[] GetArrayBytes(List<byte[]> byteArrays)
+    {
+        var totalBytes = byteArrays.Sum(x => x.Length + 1);
+
+        var buffer = ArrayPool<byte>.Shared.Rent(totalBytes);
+
+        try
+        {
+            int startIndex = 0;
+
+            foreach (byte[] valueAsBytes in byteArrays)
+            {
+                valueAsBytes.AsSpan().CopyTo(buffer.AsSpan(startIndex));
+                _enumerableItemDelimiter.AsSpan().CopyTo(buffer.AsSpan(valueAsBytes.Length + 1));
+                startIndex += valueAsBytes.Length + 1;
+            }
+
+            // Reduce list to a single hash value, then return the ulong result as bytes
+            return xxHash3.ComputeHash(buffer, totalBytes).GetBytes();
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
     }
 }
