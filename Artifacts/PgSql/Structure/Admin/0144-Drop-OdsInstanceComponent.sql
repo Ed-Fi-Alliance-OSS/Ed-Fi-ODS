50f0d367aebe8e593a815012054df29a2a64cd3d
Adam Hopkins <adam.hopkins@simpat.tech>
2023-06-21 09:24:27 -0500

    Remove unused OdsInstance Database components, add missing copyright notices

--- Artifacts/PgSql/Structure/Admin/0144-Drop-OdsInstanceComponent.sql
+++ Artifacts/PgSql/Structure/Admin/0144-Drop-OdsInstanceComponent.sql
@@ -3,12 +3,4 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-CREATE OR ALTER FUNCTION dbo.GetOdsInstanceConfigurationById (
-    @OdsInstanceId int
-)
-RETURNS TABLE
-RETURN
-    SELECT  ods.OdsInstanceId, ods.ConnectionString
-    FROM    dbo.OdsInstances ods
-    WHERE   ods.OdsInstanceId = @OdsInstanceId
-GO
+DROP TABLE IF EXISTS dbo.OdsInstanceComponents
