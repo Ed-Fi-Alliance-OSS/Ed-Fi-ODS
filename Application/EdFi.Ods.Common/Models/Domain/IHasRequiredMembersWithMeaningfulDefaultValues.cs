927742b09c7acaeff02e98ddac9319d478d645c8
Adam Hopkins <adam.hopkins@simpat.tech>
2023-06-30 09:37:21 -0500

    add interface

---
+++ Application/EdFi.Ods.Common/Models/Domain/IHasRequiredMembersWithMeaningfulDefaultValues.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdFi.Ods.Common.Models.Domain
+{
+    internal interface IHasRequiredMembersWithMeaningfulDefaultValues
+    {
+    }
+}
