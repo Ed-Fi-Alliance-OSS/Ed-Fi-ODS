f9578dcac82605fa9105eb72fd5d71ac913012cc
m.semalaiappan <m.semalaiappan@gmail.com>
2023-07-07 11:26:25 -0500

    Codefix for DataStanadard_400_ApprovalTests

--- Utilities/CodeGeneration/EdFi.Ods.CodeGen.Tests/Approval/DataStanadard_400_ApprovalTests.Verify.Extensions.TPDM.1.1.0_Std_4.0.0_Models_Mappers_EntityMapper.generated.approved.cs
+++ Utilities/CodeGeneration/EdFi.Ods.CodeGen.Tests/Approval/DataStanadard_400_ApprovalTests.Verify.Extensions.TPDM.1.1.0_Std_4.0.0_Models_Mappers_EntityMapper.generated.approved.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using EdFi.Ods.Api.Extensions;
 using EdFi.Ods.Api.Providers;
--- Utilities/CodeGeneration/EdFi.Ods.CodeGen.Tests/Approval/DataStanadard_400_ApprovalTests.Verify.Standard_Std_4.0.0_Models_Entities_EntitiesForQueriesViews.generated.approved.cs
+++ Utilities/CodeGeneration/EdFi.Ods.CodeGen.Tests/Approval/DataStanadard_400_ApprovalTests.Verify.Standard_Std_4.0.0_Models_Entities_EntitiesForQueriesViews.generated.approved.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using EdFi.Ods.Common.Models.Domain;
 
