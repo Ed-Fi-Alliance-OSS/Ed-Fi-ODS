d211e2449a9bc5ed8879549a2bc3ed4deb3f28c7
m.semalaiappan <m.semalaiappan@gmail.com>
2023-07-09 23:51:02 -0500

    dbo.GetClientForToken function updated with BigInt

--- Application/EdFi.Ods.Standard/Standard/5.0.0/Artifacts/PgSql/Structure/Admin/0150-Alter-EducationOrganizationId-to-bigint.sql
+++ Application/EdFi.Ods.Standard/Standard/5.0.0/Artifacts/PgSql/Structure/Admin/0150-Alter-EducationOrganizationId-to-bigint.sql
@@ -82,4 +82,65 @@
     END
     $BODY$ LANGUAGE plpgsql;
 
+    DROP FUNCTION IF EXISTS dbo.GetClientForToken;
+
+    CREATE FUNCTION dbo.GetClientForToken (AccessToken UUID)
+    RETURNS TABLE (
+        Key VARCHAR(50)
+        ,UseSandbox BOOLEAN
+        ,StudentIdentificationSystemDescriptor VARCHAR(306)
+        ,EducationOrganizationId BIGINT
+        ,ClaimSetName VARCHAR(255)
+        ,NamespacePrefix VARCHAR(255)
+        ,ProfileName VARCHAR
+        ,CreatorOwnershipTokenId SMALLINT
+        ,OwnershipTokenId SMALLINT
+        ,OdsInstanceId INT
+        ,ApiClientId INT
+        ,Expiration TIMESTAMP
+    )
+    AS
+    $BODY$
+    BEGIN
+        RETURN QUERY
+        SELECT
+            ac.Key
+            ,ac.UseSandbox
+            ,ac.StudentIdentificationSystemDescriptor
+            ,aeo.EducationOrganizationId
+            ,app.ClaimSetName
+            ,vnp.NamespacePrefix
+            ,p.ProfileName
+            ,ac.CreatorOwnershipTokenId_OwnershipTokenId as CreatorOwnershipTokenId
+            ,acot.OwnershipToken_OwnershipTokenId as OwnershipTokenId
+            ,acoi.OdsInstance_OdsInstanceId as OdsInstanceId
+            ,cat.ApiClient_ApiClientId as ApiClientId
+            ,cat.Expiration
+        FROM dbo.ClientAccessTokens cat
+             INNER JOIN dbo.ApiClients ac ON
+            cat.ApiClient_ApiClientId = ac.ApiClientId
+            AND cat.Id = AccessToken
+             INNER JOIN dbo.Applications app ON
+            app.ApplicationId = ac.Application_ApplicationId
+             LEFT OUTER JOIN dbo.Vendors v ON
+            v.VendorId = app.Vendor_VendorId
+             LEFT OUTER JOIN dbo.VendorNamespacePrefixes vnp ON
+            v.VendorId = vnp.Vendor_VendorId
+             -- Outer join so client key is always returned even if no EdOrgs have been enabled
+             LEFT OUTER JOIN dbo.ApiClientApplicationEducationOrganizations acaeo ON
+            acaeo.ApiClient_ApiClientId = cat.ApiClient_ApiClientId
+             LEFT OUTER JOIN dbo.ApplicationEducationOrganizations aeo ON
+            aeo.ApplicationEducationOrganizationId = acaeo.ApplicationEdOrg_ApplicationEdOrgId
+                AND (cat.Scope IS NULL OR aeo.EducationOrganizationId = CAST(cat.Scope AS INTEGER))
+             LEFT OUTER JOIN dbo.ProfileApplications ap ON
+            ap.Application_ApplicationId = app.ApplicationId
+             LEFT OUTER JOIN dbo.Profiles p ON
+            p.ProfileId = ap.Profile_ProfileId
+            LEFT OUTER JOIN dbo.ApiClientOwnershipTokens acot ON
+            ac.ApiClientId = acot.ApiClient_ApiClientId
+            LEFT OUTER JOIN dbo.ApiClientOdsInstances acoi ON
+            acoi.ApiClient_ApiClientId = ac.ApiClientId
+        WHERE cat.Expiration > CURRENT_TIMESTAMP;
+    END
+    $BODY$ LANGUAGE plpgsql;
 
