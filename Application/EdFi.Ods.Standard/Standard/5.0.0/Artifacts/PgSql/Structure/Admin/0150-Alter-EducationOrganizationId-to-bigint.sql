c69815e0e618000a826a5113db00ae41ecb40244
m.semalaiappan <m.semalaiappan@gmail.com>
2023-07-09 22:42:08 -0500

    dbo.GetClientForKey Function updated with EducationOrganizationId BIGINT

--- Application/EdFi.Ods.Standard/Standard/5.0.0/Artifacts/PgSql/Structure/Admin/0150-Alter-EducationOrganizationId-to-bigint.sql
+++ Application/EdFi.Ods.Standard/Standard/5.0.0/Artifacts/PgSql/Structure/Admin/0150-Alter-EducationOrganizationId-to-bigint.sql
@@ -3,47 +3,94 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-do
-$do$
-begin
-    if exists (select * from information_schema.columns c where c.table_name = 'applicationeducationorganizations' and c.column_name = 'educationorganizationid' AND DATA_TYPE = 'integer') then
+DO
+$DO$
+BEGIN
+    IF EXISTS (SELECT * FROM information_schema.columns c WHERE c.table_name = 'applicationeducationorganizations' AND c.column_name = 'educationorganizationid' AND DATA_TYPE = 'integer') THEN
         -- Drop the view using the EducationOrganizationId
-        drop view dbo.apiclientidentityrawdetails;
+        DROP VIEW dbo.apiclientidentityrawdetails;
 
         -- Increase the size of the EducationOrganizationId column
-        alter table dbo.applicationeducationorganizations alter column educationorganizationid TYPE bigint;
+        ALTER TABLE dbo.applicationeducationorganizations ALTER COLUMN educationorganizationid TYPE BIGINT;
 
         -- Recreate the view
-        create view dbo.apiclientidentityrawdetails
-            (key, usesandbox, studentidentificationsystemdescriptor, educationorganizationid, claimsetname,
-             namespaceprefix, profilename, creatorownershiptokenid, ownershiptokenid, apiclientid, secret,
-             secretishashed)
-        as
-        SELECT ac.key,
-               ac.usesandbox,
-               ac.studentidentificationsystemdescriptor,
-               aeo.educationorganizationid,
-               app.claimsetname,
-               vnp.namespaceprefix,
-               p.profilename,
-               ac.creatorownershiptokenid_ownershiptokenid AS creatorownershiptokenid,
-               acot.ownershiptoken_ownershiptokenid        AS ownershiptokenid,
-               ac.apiclientid,
-               ac.secret,
-               ac.secretishashed
-        FROM dbo.apiclients ac
-                 JOIN dbo.applications app ON app.applicationid = ac.application_applicationid
-                 LEFT JOIN dbo.vendors v ON v.vendorid = app.vendor_vendorid
-                 LEFT JOIN dbo.vendornamespaceprefixes vnp ON v.vendorid = vnp.vendor_vendorid
-                 LEFT JOIN dbo.apiclientapplicationeducationorganizations acaeo ON acaeo.apiclient_apiclientid = ac.apiclientid
-                 LEFT JOIN dbo.applicationeducationorganizations aeo
-                           ON aeo.applicationeducationorganizationid = acaeo.applicationedorg_applicationedorgid
-                 LEFT JOIN dbo.profileapplications ap ON ap.application_applicationid = app.applicationid
-                 LEFT JOIN dbo.profiles p ON p.profileid = ap.profile_profileid
-                 LEFT JOIN dbo.apiclientownershiptokens acot ON ac.apiclientid = acot.apiclient_apiclientid;
+    CREATE VIEW dbo.ApiClientIdentityRawDetails
+    AS
+        SELECT
+            ac.Key
+            ,ac.UseSandbox
+            ,ac.StudentIdentificationSystemDescriptor
+            ,aeo.EducationOrganizationId
+            ,app.ClaimSetName
+            ,vnp.NamespacePrefix
+            ,p.ProfileName
+            ,ac.CreatorOwnershipTokenId_OwnershipTokenId AS CreatorOwnershipTokenId
+            ,acot.OwnershipToken_OwnershipTokenId AS OwnershipTokenId
+            ,ac.ApiClientId
+            ,ac.Secret
+            ,ac.SecretIsHashed
+        FROM dbo.ApiClients ac
+        INNER JOIN dbo.Applications app 
+            ON app.ApplicationID = ac.Application_ApplicationID
+        LEFT OUTER JOIN dbo.Vendors v 
+            ON v.VendorId = app.Vendor_VendorId
+        LEFT OUTER JOIN dbo.VendorNamespacePrefixes vnp
+            ON v.VendorId = vnp.Vendor_VendorId
+        -- Outer join so client key is always returned even if no EdOrgs have been enabled
+        LEFT OUTER JOIN dbo.ApiClientApplicationEducationOrganizations acaeo
+            ON acaeo.ApiClient_ApiClientId = ac.ApiClientId
+        LEFT OUTER JOIN dbo.ApplicationEducationOrganizations aeo
+            ON aeo.ApplicationEducationOrganizationId = acaeo.applicationedorg_applicationedorgid
+        LEFT OUTER JOIN dbo.ProfileApplications ap
+            ON ap.Application_ApplicationId = app.ApplicationId
+        LEFT OUTER JOIN dbo.Profiles p
+            ON p.ProfileId = ap.Profile_ProfileId
+        LEFT OUTER JOIN dbo.ApiClientOwnershipTokens acot
+            ON ac.ApiClientId = acot.ApiClient_ApiClientId;
 
-        alter table dbo.apiclientidentityrawdetails
-            owner to postgres;
-    end if;
-end
-$do$
+
+        ALTER TABLE dbo.apiclientidentityrawdetails
+            OWNER to postgres;
+    END IF;
+
+        DROP FUNCTION IF EXISTS dbo.GetClientForKey;
+
+        CREATE FUNCTION dbo.GetClientForKey (ApiKey VARCHAR(50))
+        RETURNS TABLE (
+            Key VARCHAR(50)
+            , UseSandbox BOOLEAN
+            , StudentIdentificationSystemDescriptor VARCHAR(306)
+            , EducationOrganizationId BIGINT
+            , ClaimSetName VARCHAR(255)
+            , NamespacePrefix VARCHAR(255)
+            , ProfileName VARCHAR
+            , CreatorOwnershipTokenId SMALLINT
+            , OwnershipTokenId SMALLINT
+            , ApiClientId INT
+            , Secret VARCHAR(100)
+            , SecretIsHashed BOOLEAN
+        )
+        AS
+        $$
+        BEGIN
+            RETURN QUERY
+            SELECT
+                d.Key
+                , d.UseSandbox
+                , d.StudentIdentificationSystemDescriptor
+                , d.EducationOrganizationId
+                , d.ClaimSetName
+                , d.NamespacePrefix
+                , d.ProfileName
+                , d.CreatorOwnershipTokenId
+                , d.OwnershipTokenId
+                , d.ApiClientId
+                , d.Secret
+                , d.SecretIsHashed
+            FROM    dbo.ApiClientIdentityRawDetails d
+            WHERE   d.Key = ApiKey;
+        END
+        $$
+        LANGUAGE plpgsql;
+END
+$DO$
