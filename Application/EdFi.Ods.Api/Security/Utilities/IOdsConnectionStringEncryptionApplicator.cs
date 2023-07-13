5f95dcdd7cdd60918fa52c3980f192c314066ac2
Matthew Jackson <mjackson@unicon.net>
2023-07-13 16:18:28 -0500

    Add string encryption applicator

---
+++ Application/EdFi.Ods.Api/Security/Utilities/IOdsConnectionStringEncryptionApplicator.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.Ods.Api.Configuration;
+
+namespace EdFi.Ods.Api.Security.Utilities;
+
+public interface IOdsConnectionStringEncryptionApplicator
+{
+    public string DecryptOrApplyEncryption(RawOdsInstanceConfigurationDataRow configurationDataRow, out bool rowHasChanged);
+}
---
+++ Application/EdFi.Ods.Api/Security/Utilities/OdsConnectionStringEncryptionApplicator.cs
@@ -0,0 +1,40 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System;
+using EdFi.Ods.Api.Configuration;
+using EdFi.Ods.Api.Providers;
+
+namespace EdFi.Ods.Api.Security.Utilities;
+
+public class OdsConnectionStringEncryptionApplicator : IOdsConnectionStringEncryptionApplicator
+{
+    private readonly byte[] _privateKey;
+    private readonly ISymmetricStringEncryptionProvider _encryptionProvider;
+    
+    public OdsConnectionStringEncryptionApplicator(byte[] privateKey, ISymmetricStringEncryptionProvider encryptionProvider)
+    {
+        _privateKey = privateKey;
+        _encryptionProvider = encryptionProvider;
+    }
+    
+    public string DecryptOrApplyEncryption(RawOdsInstanceConfigurationDataRow configurationDataRow, out bool rowHasChanged)
+    {
+        rowHasChanged = false;
+
+        if(_encryptionProvider.TryDecrypt(configurationDataRow.ConnectionString, out string connectionStringPlainText, _privateKey))
+        {
+            return connectionStringPlainText;
+        }
+        
+        connectionStringPlainText = configurationDataRow.ConnectionString;
+        string encryptedConnectionString = _encryptionProvider.Encrypt(connectionStringPlainText, _privateKey);
+        
+        configurationDataRow.ConnectionString = encryptedConnectionString;
+        rowHasChanged = true;
+
+        return connectionStringPlainText;
+    }
+}
