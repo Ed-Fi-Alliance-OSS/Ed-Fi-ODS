a328a1a0e4fb8cfc25cd84e06b3bfb1bbe44f15a
Matthew Jackson <mjackson@unicon.net>
2023-07-20 19:37:55 -0500

    Split string encryption provider into encryption and decryption providers

--- Application/EdFi.Ods.Api/Providers/AES256SymmetricStringEncryptionProvider.cs
+++ Application/EdFi.Ods.Api/Providers/AES256SymmetricStringEncryptionProvider.cs
@@ -60,11 +60,12 @@ public class Aes256SymmetricStringEncryptionProvider : ISymmetricStringEncryptio
             }
         }
 
-        byte[] hmacSignatureValue = GenerateHmacSignature(key, encryptedBytes);
+        byte[] hmacSignatureValue = GenerateHmacSignature();
 
-        return $"{Convert.ToBase64String(aesInstance.IV)}.{Convert.ToBase64String(encryptedBytes)}.{Convert.ToBase64String(hmacSignatureValue)}";
-        
-        byte[] GenerateHmacSignature(byte[] key, byte[] encryptedBytes)
+        return
+            $"{Convert.ToBase64String(aesInstance.IV)}.{Convert.ToBase64String(encryptedBytes)}.{Convert.ToBase64String(hmacSignatureValue)}";
+
+        byte[] GenerateHmacSignature()
         {
             byte[] hmacSignature;
 
@@ -74,91 +75,6 @@ public class Aes256SymmetricStringEncryptionProvider : ISymmetricStringEncryptio
             }
 
             return hmacSignature;
-        }
-    }
-
-    /// <summary>
-    /// Decrypt a string value using the specified key.
-    /// </summary>
-    /// <para name="value">The data to be decrypted and related information as three base64
-    /// encoded segments concatenated in the format "IV.EncryptedMessage.HMACSignature"</para>
-    /// <para name="output">If decryption is successful, then the plaintext output, otherwise null</para>
-    /// <para name="key">The 256 bit private key to be used for decryption.</para>
-    /// <returns>A boolean value indicating if decryption was successful.</returns>
-    public bool TryDecrypt(string value, out string output, byte[] key)
-    {
-        if (String.IsNullOrWhiteSpace(value))
-            throw new ArgumentException("Input value cannot be null or whitespace.", nameof(value));
-
-        if (key == null || key.Length != 32)
-            throw new ArgumentException("Key must be 256 bits (32 bytes) in length.", nameof(key));
-
-        string[] inputParts = value.Split('.');
-
-        if (inputParts.Length != 3)
-        {
-            output = null;
-            return false;
-        }
-
-        string base64IV = inputParts[0];
-        string base64EncryptedBytes = inputParts[1];
-        string base64HashValue = inputParts[2];
-        byte[] iv;
-        byte[] encryptedBytes;
-        byte[] hashValue;
-
-        try
-        {
-            iv = Convert.FromBase64String(base64IV);
-            encryptedBytes = Convert.FromBase64String(base64EncryptedBytes);
-            hashValue = Convert.FromBase64String(base64HashValue);
-        }
-        catch
-        {
-            output = null;
-            return false;
-        }
-
-        bool signatureIsValid = IsHmacSignatureValid(key, encryptedBytes, hashValue);
-
-        if (!signatureIsValid)
-        {
-            output = null;
-            return false;
-        }
-
-        using Aes aesInstance = Aes.Create();
-
-        aesInstance.Key = key;
-        aesInstance.IV = iv;
-
-        ICryptoTransform decryptor = aesInstance.CreateDecryptor(aesInstance.Key, aesInstance.IV);
-
-        using (MemoryStream msDecrypt = new MemoryStream(encryptedBytes))
-        {
-            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-            {
-                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                {
-                    output = srDecrypt.ReadToEnd();
-                }
-            }
-        }
-
-        return true;
-        
-        bool IsHmacSignatureValid(byte[] key, byte[] message, byte[] hmacValue)
-        {
-            bool signatureIsValid;
-
-            using (HMACSHA256 hmac = new HMACSHA256(key))
-            {
-                byte[] computedHashValue = hmac.ComputeHash(message);
-                signatureIsValid = hmacValue.SequenceEqual(computedHashValue);
-            }
-
-            return signatureIsValid;
         }
     }
 }
