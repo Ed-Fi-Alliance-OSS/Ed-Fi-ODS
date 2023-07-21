40e917ece1a0c84be7115fdf257d926ddbc51234
Matthew Jackson <mjackson@unicon.net>
2023-07-21 00:54:31 -0500

    Remove unneeded tests and update string encryption/decryption tests

--- Application/EdFi.Ods.Tests/EdFi.Ods.Api/Providers/Aes256SymmetricStringEncryptionProviderTests.cs
+++ Application/EdFi.Ods.Tests/EdFi.Ods.Api/Providers/Aes256SymmetricStringEncryptionProviderTests.cs
@@ -11,14 +11,16 @@ namespace EdFi.Ods.Tests.EdFi.Ods.Api.Providers;
 using System;
 using NUnit.Framework;
 
-public class Aes256SymmetricStringEncryptionProviderTests
+public class Aes256SymmetricStringEncryptionDecryptionProviderTests
 {
     private Aes256SymmetricStringEncryptionProvider _encryptionProvider;
+    private Aes256SymmetricStringDecryptionProvider _decryptionProvider;
     private byte[] _staticKey;
     [SetUp]
     public void SetUp()
     {
         _encryptionProvider = new Aes256SymmetricStringEncryptionProvider();
+        _decryptionProvider = new Aes256SymmetricStringDecryptionProvider();
         _staticKey = Convert.FromBase64String("t3OD+5YCKzCdGSaWBAvkQ9+d9R4ZbZnC9YG8L/ar8vA=");
     }
 
@@ -30,21 +32,23 @@ public class Aes256SymmetricStringEncryptionProviderTests
 
         // Act
         string encryptedString = _encryptionProvider.Encrypt(plainText, _staticKey);
-        bool decryptedSuccessfully = _encryptionProvider.TryDecrypt(encryptedString, out string decryptionResult, _staticKey);
+        bool decryptedSuccessfully = _decryptionProvider.TryDecrypt(encryptedString, out string decryptionResult, _staticKey);
 
         // Assert
         decryptedSuccessfully.ShouldBeTrue();
         decryptionResult.ShouldBe(plainText);
+        encryptedString.ShouldNotBe(plainText);
     }
     
     [Test]
     public void When_decrypting_a_string_using_the_correct_key()
     {
         // Arrange
+        // This is a valid ciphertext with a valid HMAC signature which was encrypted using the static key and decrypts to "Hello World"
         var cipherText = "Gx5FXSHSelzRF2quaCOaNw==.RlK3dR28TP/dbslPezxa4w==.D7VcBfHXG0iPkPR36QDvv9W7SGiGWUUe7WBu5neV/fo=";
 
         // Act
-        bool decryptionSuccessful = _encryptionProvider.TryDecrypt(cipherText, out string output, _staticKey);
+        bool decryptionSuccessful = _decryptionProvider.TryDecrypt(cipherText, out string output, _staticKey);
 
         // Assert
         decryptionSuccessful.ShouldBeTrue();
@@ -55,14 +59,19 @@ public class Aes256SymmetricStringEncryptionProviderTests
     public void When_hmac_signature_of_ciphertext_is_invalid()
     {
         // Arrange
-        var cipherText = "Gx5FXSHSelzRF2quaCOaNw==.RlK3dR28TP/dbslPezxa4w==.D7VcBfHXG0iPkPR36QDvv8W7SGiGWUUe7WBu5neV/fo=";
+        // This is a valid ciphertext with a valid HMAC signature which was encrypted using the static key and decrypts to "Hello World"
+        var ciphertextWithValidHmac = "Gx5FXSHSelzRF2quaCOaNw==.RlK3dR28TP/dbslPezxa4w==.D7VcBfHXG0iPkPR36QDvv9W7SGiGWUUe7WBu5neV/fo=";
+        // Change the last character of the HMAC signature to make it invalid
+        var cipherTextWithInvalidHmac = "Gx5FXSHSelzRF2quaCOaNw==.RlK3dR28TP/dbslPezxa4w==.D7VcBfHXG0iPkPR36QDvv9W7SGiGWUUe7WBu5neV/f0=";
 
         // Act
-        bool decryptionSuccessful = _encryptionProvider.TryDecrypt(cipherText, out string output, _staticKey);
-
+        bool decryptionOfValidCiphertextWithValidHmacSuccessful = _decryptionProvider.TryDecrypt(ciphertextWithValidHmac, out string vaildHmacOutput, _staticKey);
+        bool decryptionOfValidCiphertextWithInvalidHmacSuccessful = _decryptionProvider.TryDecrypt(cipherTextWithInvalidHmac, out string invalidHmacOutput, _staticKey);
         // Assert
-        decryptionSuccessful.ShouldBeFalse();
-        output.ShouldBeNull();
+        decryptionOfValidCiphertextWithValidHmacSuccessful.ShouldBeTrue();
+        vaildHmacOutput.ShouldBe("Hello World");
+        decryptionOfValidCiphertextWithInvalidHmacSuccessful.ShouldBeFalse();
+        invalidHmacOutput.ShouldBeNull();
     }
     
     [Test]
@@ -72,7 +81,7 @@ public class Aes256SymmetricStringEncryptionProviderTests
         var cipherText = "Not a valid format for decryption";
 
         // Act
-        bool decryptionSuccessful = _encryptionProvider.TryDecrypt(cipherText, out string output, _staticKey);
+        bool decryptionSuccessful = _decryptionProvider.TryDecrypt(cipherText, out string output, _staticKey);
 
         // Assert
         decryptionSuccessful.ShouldBeFalse();
