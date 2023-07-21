9d78f6ff587fcd809ff96380938c0925a48314ca
Matthew Jackson <mjackson@unicon.net>
2023-07-20 23:11:26 -0500

    Add connection string writer

---
+++ Application/EdFi.Ods.Api/Security/Authentication/EdFiAdminRawOdsConnectionStringDatabaseWriter.cs
@@ -0,0 +1,66 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System;
+using System.Data.Common;
+using Dapper;
+using EdFi.Admin.DataAccess.Providers;
+
+namespace EdFi.Ods.Api.Security.Authentication;
+
+/// <summary>
+/// Defines an interface for writing the connection string for an ODS instance to the admin database.
+/// </summary>
+public class EdFiAdminRawOdsConnectionStringDatabaseWriter : IEdFiAdminRawOdsConnectionStringWriter
+{
+    private readonly IAdminDatabaseConnectionStringProvider _adminDatabaseConnectionStringProvider;
+    private readonly DbProviderFactory _dbProviderFactory;
+    
+    private const string UpdateOdsConnectionStringByIdSql = "UPDATE dbo.OdsInstances SET ConnectionString = @ConnectionString WHERE OdsInstanceId = @OdsInstanceId";
+    private const string UpdateOdsDerivativeConnectionStringByIdSql = "UPDATE dbo.OdsInstanceDerivative SET ConnectionString = @ConnectionString WHERE OdsInstanceId = @OdsInstanceId AND DerivativeType = @DerivativeType";
+
+    /// <inheritdoc cref="IEdFiAdminRawOdsConnectionStringWriter.WriteConnectionString"/>
+    public EdFiAdminRawOdsConnectionStringDatabaseWriter(IAdminDatabaseConnectionStringProvider adminDatabaseConnectionStringProvider,
+        DbProviderFactory dbProviderFactory)
+    {
+        _adminDatabaseConnectionStringProvider = adminDatabaseConnectionStringProvider;
+        _dbProviderFactory = dbProviderFactory;
+    }
+    
+    public void WriteConnectionString(int odsInstanceId, string connectionString, string derivativeType = null)
+    {
+        var connection = CreateConnection();
+        
+        if(!string.IsNullOrEmpty(derivativeType))
+        {
+            connection.Query(
+                UpdateOdsDerivativeConnectionStringByIdSql,
+                new
+                {
+                    ConnectionString = connectionString,
+                    OdsInstanceId = odsInstanceId
+                });
+        }
+        else
+        {
+            connection.Query(
+                UpdateOdsConnectionStringByIdSql,
+                new
+                {
+                    ConnectionString = connectionString,
+                    OdsInstanceId = odsInstanceId,
+                    DerivativeType = derivativeType
+                });
+        }
+        
+        DbConnection CreateConnection()
+        {
+            var newConnection = _dbProviderFactory.CreateConnection();
+            newConnection.ConnectionString = _adminDatabaseConnectionStringProvider.GetConnectionString();
+
+            return newConnection;
+        }
+    }
+}
---
+++ Application/EdFi.Ods.Api/Security/Authentication/IEdFiAdminRawOdsConnectionStringWriter.cs
@@ -0,0 +1,26 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+namespace EdFi.Ods.Api.Security.Authentication;
+
+/// <summary>
+/// Defines an interface for writing the connection string for an ODS instance to persistent storage.
+/// </summary>
+public interface IEdFiAdminRawOdsConnectionStringWriter
+{
+    /// <summary>
+    /// Writes the connection string for an ODS instance to persistent storage.
+    /// </summary>
+    /// <param name="odsInstanceId">
+    /// The identifier of the ODS instance for which the connection string is to be written.
+    /// </param>
+    /// <param name="connectionString">
+    /// The connection string to be written.
+    /// </param>
+    /// <param name="derivativeType">
+    /// Optional. The type of ODS instance derivative to which the connection string should be written.
+    /// </param>
+    public void WriteConnectionString(int odsInstanceId, string connectionString, string derivativeType = null);
+}
