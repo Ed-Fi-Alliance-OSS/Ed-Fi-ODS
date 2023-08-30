e265f36ee90a91b8cc5efeeeb9d102161ce3f965
Adam Hopkins <adam.hopkins@simpat.tech>
2023-08-30 10:58:33 -0500

    SQL scripts rename OdsInstanceContext

---
+++ Artifacts/MsSql/Structure/Admin/0160-Rename-OdsInstanceContext.sql
@@ -0,0 +1,30 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+IF EXISTS (SELECT 1
+    FROM   information_schema.columns
+    WHERE  table_name = 'OdsInstanceContext'
+        AND table_schema ='dbo')
+  BEGIN
+	ALTER TABLE [dbo].[OdsInstanceContext]
+    DROP CONSTRAINT [FK_OdsInstanceContext_OdsInstanceId_OdsInstanceId];
+
+	ALTER TABLE [dbo].[OdsInstanceContext]
+    DROP CONSTRAINT [UC_OdsInstanceDerivative_OdsInstanceId_ContextKey];
+    
+    EXEC SP_RENAME 'dbo.OdsInstanceContext.OdsInstanceId', 'OdsInstance_OdsInstanceId', 'COLUMN';
+    
+    EXEC SP_RENAME 'dbo.OdsInstanceContext', 'OdsInstanceContexts';    
+ END
+GO
+
+ALTER TABLE [dbo].[OdsInstanceContexts]  WITH CHECK
+ADD
+    CONSTRAINT [FK_OdsInstanceContexts_OdsInstance_OdsInstanceId] FOREIGN KEY([OdsInstance_OdsInstanceId]) REFERENCES [dbo].[OdsInstances] ([OdsInstanceId]),
+    CONSTRAINT [UC_OdsInstanceContexts_OdsInstance_OdsInstanceId_ContextKey] UNIQUE(OdsInstance_OdsInstanceId, ContextKey);
+GO
+
+ALTER TABLE [dbo].[OdsInstanceContexts] CHECK CONSTRAINT [FK_OdsInstanceContexts_OdsInstance_OdsInstanceId];
+GO
---
+++ Artifacts/PgSql/Structure/Admin/0160-Rename-OdsInstanceContext.sql
@@ -0,0 +1,20 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+ALTER TABLE dbo.OdsInstanceContext
+	DROP CONSTRAINT IF EXISTS FK_OdsInstanceContext_OdsInstanceId_OdsInstanceId;
+
+ALTER TABLE dbo.OdsInstanceContext
+	DROP CONSTRAINT IF EXISTS UC_OdsInstanceDerivative_OdsInstanceId_ContextKey;
+
+ALTER TABLE dbo.OdsInstanceContext
+    RENAME COLUMN OdsInstanceId TO OdsInstance_OdsInstanceId;
+
+ALTER TABLE dbo.OdsInstanceContext
+    RENAME TO OdsInstanceContexts;
+
+ALTER TABLE dbo.OdsInstanceContexts
+    ADD CONSTRAINT FK_OdsInstanceContexts_OdsInstance_OdsInstanceId FOREIGN KEY(OdsInstance_OdsInstanceId) REFERENCES dbo.OdsInstances (OdsInstanceId),
+    ADD CONSTRAINT UC_OdsInstanceContexts_OdsInstance_OdsInstanceId_ContextKey UNIQUE(OdsInstance_OdsInstanceId, ContextKey);
