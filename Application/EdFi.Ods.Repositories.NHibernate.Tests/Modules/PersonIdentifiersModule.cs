190f5d6a317b697274828a10e2a212aaad2eb1bc
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-09-14 20:05:02 -0500

    Removing NHibernate StudentProgramAssociation-derived lifecycle tests (to be replaced with Postman integrated tests).

--- Application/EdFi.Ods.Repositories.NHibernate.Tests/Modules/PersonIdentifiersModule.cs
+++ Application/EdFi.Ods.Repositories.NHibernate.Tests/Modules/PersonIdentifiersModule.cs
@@ -3,8 +3,14 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System;
+using System.Collections.Generic;
 using Autofac;
+using Autofac.Core;
+using EdFi.Common.Extensions;
+using EdFi.Ods.Api.Caching;
 using EdFi.Ods.Api.IdentityValueMappers;
+using EdFi.Ods.Common.Configuration;
 using EdFi.Ods.Common.Specifications;
 
 namespace EdFi.Ods.Repositories.NHibernate.Tests.Modules
@@ -23,6 +29,42 @@ namespace EdFi.Ods.Repositories.NHibernate.Tests.Modules
 
             builder.RegisterType<FakePersonTypesProvider>()
                 .As<IPersonTypesProvider>()
+                .SingleInstance();
+
+            RegisterPersonIdentifierCaching(builder);
+        }
+        
+        private static void RegisterPersonIdentifierCaching(ContainerBuilder builder)
+        {
+            var cacheSuppression = new Dictionary<string, bool>()
+            {
+                { "Student", false },
+                { "Staff", false },
+                { "Parent", false },
+                { "Contact", false },
+            };
+            
+            builder
+                .RegisterType<InMemoryMapCache<(ulong odsInstanceHashId, string personType, PersonMapType personMapType), string, int>>()
+                .WithParameter(new NamedParameter("slidingExpiration", TimeSpan.FromSeconds(14400)))
+                .WithParameter(new NamedParameter("absoluteExpirationPeriod", TimeSpan.FromSeconds(86400)))                .As<IMapCache<(ulong odsInstanceHashId, string personType, PersonMapType mapType), string, int>>()
+                .SingleInstance();
+
+            builder.RegisterType<InMemoryMapCache<(ulong odsInstanceHashId, string personType, PersonMapType mapType), int, string>>()
+                .WithParameter(new NamedParameter("slidingExpiration", TimeSpan.FromSeconds(14400)))
+                .WithParameter(new NamedParameter("absoluteExpirationPeriod", TimeSpan.FromSeconds(86400)))                .As<IMapCache<(ulong odsInstanceHashId, string personType, PersonMapType mapType), int, string>>()
+                .SingleInstance();
+
+            builder.RegisterType<PersonMapCacheInitializer>().As<IPersonMapCacheInitializer>().SingleInstance();
+
+            builder.RegisterType<PersonUniqueIdResolver>()
+                .WithParameter(new NamedParameter("cacheSuppressionByPersonType", cacheSuppression))
+                .As<IPersonUniqueIdResolver>()
+                .SingleInstance();
+
+            builder.RegisterType<PersonUsiResolver>()
+                .WithParameter(new NamedParameter("cacheSuppressionByPersonType", cacheSuppression))
+                .As<IPersonUsiResolver>()
                 .SingleInstance();
         }
     }
