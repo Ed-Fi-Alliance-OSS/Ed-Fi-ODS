1eee422f30fb9c3d5e1509f155fe40243101abb0
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-10-05 15:06:18 -0500

    Fixed object equality implementation for resource classes, added support for correct case-sensitivity comparison for strings and descriptor values, and removed NoDuplicateMembers validation attribute from the entity classes, as well as the Distinct invocation during collection item mapping (as it is no longer necessary).

--- Application/EdFi.Ods.Common/Attributes/NoDuplicateMembersAttribute.cs
+++ Application/EdFi.Ods.Common/Attributes/NoDuplicateMembersAttribute.cs
@@ -6,43 +6,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using EdFi.Common.Inflection;
 
-namespace EdFi.Ods.Common.Attributes
+namespace EdFi.Ods.Common.Attributes;
+
+public sealed class NoDuplicateMembersAttribute : ValidationAttribute
 {
-    public sealed class NoDuplicateMembersAttribute : ValidationAttribute
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        var enumerable = value as IEnumerable;
+
+        if (enumerable == null)
         {
-            var enumerable = value as IEnumerable;
-
-            if (enumerable == null)
-            {
-                return ValidationResult.Success;
-            }
-
-            var i = 0;
-            var enumerableHashSet = new HashSet<object>();
-
-            foreach (var item in enumerable)
-            {
-                if (item != null)
-                {
-                    if (!enumerableHashSet.Add(item))
-                    {
-                        return
-                            new ValidationResult(
-                                string.Format(
-                                    "{0} enumerable contains duplicate at index: {1}",
-                                    validationContext.DisplayName,
-                                    i));
-                    }
-                }
-
-                i++;
-            }
-
-            // If we're still here, validation was successful
             return ValidationResult.Success;
         }
+
+        var itemNumber = 1;
+        var enumerableHashSet = new HashSet<object>();
+
+        foreach (var item in enumerable)
+        {
+            if (item != null)
+            {
+                if (!enumerableHashSet.Add(item))
+                {
+                    return new ValidationResult(
+                        $"The {Inflector.AddOrdinalSuffix(itemNumber.ToString())} item of the {validationContext.DisplayName} collection is a duplicate.");
+                }
+            }
+
+            itemNumber++;
+        }
+
+        // If we're still here, validation was successful
+        return ValidationResult.Success;
     }
 }
