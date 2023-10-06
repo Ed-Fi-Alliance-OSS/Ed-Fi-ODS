6ff7e36cca9ec8d8c90408e392d76458b8d3bd2d
Jesus Flores <jesus@simpat.tech>
2023-10-05 18:41:37 -0600

    Added validation middleware

---
+++ Application/EdFi.Ods.Tests/EdFi.Ods.Api/Middleware/OAuthContentTypeValidationMiddleware.cs
@@ -0,0 +1,78 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.IO;
+using System.Threading.Tasks;
+using EdFi.Ods.Api.Middleware;
+using FakeItEasy;
+using Microsoft.AspNetCore.Http;
+using NUnit.Framework;
+using Shouldly;
+
+namespace EdFi.Ods.Tests.EdFi.Ods.Api.Middleware
+{
+    [TestFixture]
+    public class OAuthContentTypeValidationMiddlewareTests
+    {
+        private OAuthContentTypeValidationMiddleware _middleware;
+
+        [SetUp]
+        public void Setup()
+        {
+            _middleware = new OAuthContentTypeValidationMiddleware();
+        }
+
+        [Test]
+        public async Task Middleware_Returns_BadRequest_When_Content_Type_Is_Missing_For_OAuth_Request()
+        {
+            // Arrange
+            var context = new DefaultHttpContext();
+            context.Request.Path = "/oauth/token";
+            context.Response.Body = new MemoryStream();
+            var next = A.Fake<RequestDelegate>();
+
+            // Act
+            await _middleware.InvokeAsync(context, next);
+
+            // Assert
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+            var reader = new StreamReader(context.Response.Body);
+            var streamText = await reader.ReadToEndAsync();
+            streamText.ShouldBe("Content-Type header is missing.");
+            context.Response.StatusCode.ShouldBe(415);
+        }
+
+        [Test]
+        public async Task Middleware_Passes_Through_When_Content_Type_Is_Present_For_OAuth_Request()
+        {
+            // Arrange
+            var context = new DefaultHttpContext();
+            context.Request.Path = "/oauth/token";
+            context.Request.ContentType = "application/json";
+            var next = A.Fake<RequestDelegate>();
+
+            // Act
+            await _middleware.InvokeAsync(context, next);
+
+            // Assert
+            A.CallTo(() => next(context)).MustHaveHappened();
+        }
+
+        [Test]
+        public async Task Middleware_Passes_Through_For_Non_OAuth_Request()
+        {
+            // Arrange
+            var context = new DefaultHttpContext();
+            context.Request.Path = "/data/v3/ed-fi/localEducationAgencies";
+            var next = A.Fake<RequestDelegate>();
+
+            // Act
+            await _middleware.InvokeAsync(context, next);
+
+            // Assert
+            A.CallTo(() => next(context)).MustHaveHappened();
+        }
+    }
+}
