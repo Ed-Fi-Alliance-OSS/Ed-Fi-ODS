71365e6aa7418c78d444b7749467ecbe4ce6c4cf
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2023-10-14 00:55:19 -0500

    [ODS-5987] Relationship Authorization for StudentAssessment based on ReportingSchool (#835)
    * Override for auth by reported school on student assessment (#814)
    
    In order to use role based field for establishing edorg relationship, a custom authorization context data provider implementation is needed to use ReportingSchoolId.  This  follows the pattern established in DisciplineActionRelationshipsAuthorizationContextDataProvider.
    
    * Updated IsEducationOrganizationIdName method to allow for role-named EdOrgId-related identifiers.
    
    * Updated IsPersonIdentifier method to allow for role-named person identifiers.
    
    ---------
    
    Co-authored-by: Audrey Shay <audrey.shay@dpi.wi.gov>

---
+++ Application/EdFi.Ods.Standard/Security/Authorization/Overrides/StudentAssessmentRelationshipsAuthorizationContextDataProvider.cs
@@ -0,0 +1,64 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.Ods.Api.Security.AuthorizationStrategies.Relationships;
+using EdFi.Ods.Entities.Common.EdFi;
+using EdFi.Ods.Entities.NHibernate.StudentAssessmentAggregate.EdFi;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EdFi.Ods.Standard.Security.Authorization.Overrides
+{
+    /// <summary>
+    /// Creates and returns an <see cref="RelationshipsAuthorizationContextData"/> instance for making authorization decisions for access to the edfi.StudentAssessment table of the StudentAssessment aggregate in the Ods Database.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class StudentAssessmentRelationshipsAuthorizationContextDataProvider<TContextData> : IRelationshipsAuthorizationContextDataProvider<IStudentAssessment, TContextData>
+        where TContextData : RelationshipsAuthorizationContextData, new()
+    {
+        /// <summary>
+        /// Creates and returns an <see cref="TContextData"/> instance based on the supplied resource.
+        /// </summary>
+        public TContextData GetContextData(IStudentAssessment resource)
+        {
+            if (resource == null)
+            {
+                throw new ArgumentNullException(nameof(resource), "The 'studentAssessment' resource for obtaining authorization context data cannot be null.");
+            }
+
+            var entity = resource as StudentAssessment;
+
+            var contextData = new TContextData 
+            { 
+                SchoolId = entity.ReportedSchoolId, // Role name applied and not part of primary key
+                StudentUSI = entity.StudentUSI == default ? null : entity.StudentUSI // Primary key property, USI
+            };
+
+            return contextData;
+        }
+
+        /// <summary>
+        ///  Creates and returns a signature key based on the resource, which can then be used to get and instance of IEdFiSignatureAuthorizationProvider
+        /// </summary>
+        public string[] GetAuthorizationContextPropertyNames()
+        {
+            var properties = new[]
+                 {
+                    "ReportedSchoolId",
+                    "StudentUSI",
+                 };
+
+            return properties;
+        }
+
+        /// <summary>
+        /// Creates and returns an <see cref="RelationshipsAuthorizationContextData"/> instance based on the supplied resource.
+        /// </summary>
+        public TContextData GetContextData(object resource)
+        {
+            return GetContextData((StudentAssessment)resource);
+        }
+    }
+}
