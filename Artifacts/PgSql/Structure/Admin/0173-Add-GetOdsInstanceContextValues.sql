63009a2d909c27300728b5a3e1a01241222a5149
Matthew Jackson <mjackson@unicon.net>
2023-11-15 18:39:51 -0600

    Fix postgres function

--- Artifacts/PgSql/Structure/Admin/0173-Add-GetOdsInstanceContextValues.sql
+++ Artifacts/PgSql/Structure/Admin/0173-Add-GetOdsInstanceContextValues.sql
@@ -3,19 +3,19 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-CREATE OR REPLACE FUNCTION dbo.getodsinstancecontextvalues (apikey character varying)
-    RETURNS TABLE(key VARCHAR(50)
-)
+CREATE OR REPLACE FUNCTION dbo.getodsinstancecontextvalues (contextkey VARCHAR(50))
+    RETURNS TABLE(contextvalue VARCHAR(50))
 AS
 $$
+#variable_conflict use_variable
 BEGIN
 	RETURN QUERY
 	SELECT DISTINCT
           ctx.contextvalue
-    FROM dbo.odsoinstances ods
-    INNER JOIN dbo.odsinstanceContexts ctx 
-        ON ods.OdsInstanceId = ctx.OdsInstance_OdsInstanceId
-		WHERE ctx.ContextKey = @ContextKey;
+    FROM dbo.odsinstances ods
+    INNER JOIN dbo.odsinstancecontexts ctx 
+        ON ods.odsinstanceid = ctx.odsinstance_odsinstanceid
+		WHERE ctx.contextkey = contextkey;
 END
 $$
 LANGUAGE plpgsql;
