c0b76618b68cbc50bbf97110f0dcaf7ca0ce3fcf
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2024-01-07 15:55:13 -0600

    Refinements made while working through Profile Postman test failures.

--- Application/EdFi.Ods.Common/Extensions/ValidationExtensions.cs
+++ Application/EdFi.Ods.Common/Extensions/ValidationExtensions.cs
@@ -40,7 +40,7 @@ public static class ValidationExtensions
                             .Where(kvp => (kvp.Key as string) != ValidationContextKeys.ResourceClass)
                             .Prepend(new KeyValuePair<object, object>(ValidationContextKeys.ResourceClass, collection.ItemType)));
 
-                    // Alternative approach: reuse same Items for every validation, but then reset to containing resource class after each collection/embedded object validation
+                    // Alternative approach: reuse same Items for every validation, but then reset to containing resource class after each collection/embedded object validation tree completes
                     // items[ValidationContextKeys.ResourceClass] = collection.ItemType;
                 }
             }
@@ -64,8 +64,32 @@ public static class ValidationExtensions
                             .Where(kvp => (kvp.Key as string) != ValidationContextKeys.ResourceClass)
                             .Prepend(new KeyValuePair<object, object>(ValidationContextKeys.ResourceClass, embeddedObject.ObjectType)));
 
-                    // Alternative approach: reuse same Items for every validation, but then reset to containing resource class after each collection/embedded object validation
+                    // Alternative approach: reuse same Items for every validation, but then reset to containing resource class after each collection/embedded object validation tree completes
                     // items[ValidationContextKeys.ResourceClass] = embeddedObject.ObjectType;
+                }
+            }
+        }
+
+        return items;
+    }
+
+    public static IDictionary<object, object> ForExtension(
+        this IDictionary<object, object> items,
+        string extensionPropertyName)
+    {
+        if (items.TryGetValue(ValidationContextKeys.ResourceClass, out var resourceClassAsObject))
+        {
+            if (resourceClassAsObject is ResourceClassBase resourceClass)
+            {
+                if (resourceClass.ExtensionByName.TryGetValue(extensionPropertyName, out var extension))
+                {
+                    // Create a new dictionary for each validation context
+                    return new Dictionary<object, object>(items
+                            .Where(kvp => (kvp.Key as string) != ValidationContextKeys.ResourceClass)
+                            .Prepend(new KeyValuePair<object, object>(ValidationContextKeys.ResourceClass, extension.ObjectType)));
+
+                    // Alternative approach: reuse same Items for every validation, but then reset to containing resource class after each collection/embedded object validation tree completes
+                    // items[ValidationContextKeys.ResourceClass] = extension.ObjectType;
                 }
             }
         }
