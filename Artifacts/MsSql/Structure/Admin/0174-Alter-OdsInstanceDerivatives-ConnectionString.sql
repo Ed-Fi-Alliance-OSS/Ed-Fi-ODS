48d55dcf5b997fc54a1cd83a98c08d4266d313c2
Johnny Brenes <103782746+JBrenesSimpat@users.noreply.github.com>
2024-01-08 10:58:05 -0600

    [ODS-6125]  Update ConnectionString length in OdsInstanceDerivatives (#906)
    Co-authored-by: semalaiappan <34613894+semalaiappan@users.noreply.github.com>

---
+++ Artifacts/MsSql/Structure/Admin/0174-Alter-OdsInstanceDerivatives-ConnectionString.sql
@@ -0,0 +1,12 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS c WHERE c.TABLE_NAME = 'OdsInstanceDerivatives' and c.COLUMN_NAME = 'ConnectionString' AND DATA_TYPE = 'NVARCHAR')
+BEGIN
+
+ALTER TABLE dbo.OdsInstanceDerivatives ALTER COLUMN ConnectionString NVARCHAR(1500);
+END
+
+GO
