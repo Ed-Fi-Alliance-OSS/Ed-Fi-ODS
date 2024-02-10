eb052b447d86a0c1982374e60ca27d15b8561729
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2024-02-10 00:01:21 -0600

    Made member inclusion/exclusion filters use case-insensitive comparisons.

--- Application/EdFi.Ods.Common/Models/Resource/IncludeOnlyMemberFilter.cs
+++ Application/EdFi.Ods.Common/Models/Resource/IncludeOnlyMemberFilter.cs
@@ -3,6 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System;
 using System.Linq;
 
 namespace EdFi.Ods.Common.Models.Resource
@@ -20,12 +21,12 @@ namespace EdFi.Ods.Common.Models.Resource
 
         public bool ShouldInclude(string memberName)
         {
-            return memberName == "Id" || _includedMemberNames.Contains(memberName);
+            return memberName == "Id" || _includedMemberNames.Contains(memberName, StringComparer.OrdinalIgnoreCase);
         }
 
         public bool ShouldIncludeExtension(string extensionName)
         {
-            return _includedExtensionNames.Contains(extensionName);
+            return _includedExtensionNames.Contains(extensionName, StringComparer.OrdinalIgnoreCase);
         }
     }
 }
