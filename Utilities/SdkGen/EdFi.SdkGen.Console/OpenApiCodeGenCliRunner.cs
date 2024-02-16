2333dce9703a37a6ba846e0a69971462c51aabf1
Matthew Jackson <mjackson@unicon.net>
2024-02-16 04:09:21 -0600

    Switch away from SDK gen cli argument name changes

--- Utilities/SdkGen/EdFi.SdkGen.Console/OpenApiCodeGenCliRunner.cs
+++ Utilities/SdkGen/EdFi.SdkGen.Console/OpenApiCodeGenCliRunner.cs
@@ -70,7 +70,7 @@ namespace EdFi.SdkGen.Console
                     $"-Xms3g -jar {_options.CliExecutableFullName()}", "generate", "-g csharp", $"-i {apiEndpoint.EndpointUri}",
                     $"--api-package {apiPackage}", $"--model-package {modelPackage}", $"-o {_options.OutputFolder}",
                     $"--additional-properties packageName={_options.Namespace},targetFramework=net8.0,netCoreProjectFile=true",
-                    "--global-property modelTests=false --global-property apiTests=false --global-property apiDocs=false --global-property modelDocs=false --skip-validate-spec --reserved-words-mappings=namespace=_namespace"
+                    "--global-property modelTests=false --global-property apiTests=false --global-property apiDocs=false --global-property modelDocs=false --skip-validate-spec"
                 };
 
                 _log.Info($"Generating C# SDK for {apiEndpoint.EndpointUri}");
