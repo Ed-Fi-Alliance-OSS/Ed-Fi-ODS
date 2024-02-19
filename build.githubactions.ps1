8b795127050fde064c0e83de79df09fe9cd0e1ce
Johnny Brenes <johnny.brenes@simpat.tech>
2024-02-19 10:15:02 -0600

    build.githubactions ps1 integration test do not run in GitHub

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -165,7 +165,7 @@ function Publish {
 
 function Test {    
     if(-not $TestFilter) {
-        Invoke-Execute { dotnet test $solution  -c $Configuration --no-build -v normal --filter 'FullyQualifiedName!~EdFi.Admin.DataAccess.IntegrationTests'}
+        Invoke-Execute { dotnet test $solution  -c $Configuration --no-build -v normal --filter 'FullyQualifiedName!~*.DataAccess.IntegrationTests'}
     } else {
         Invoke-Execute { dotnet test $solution  -c $Configuration --no-build -v normal --filter TestCategory!~"$TestFilter" }
     }
