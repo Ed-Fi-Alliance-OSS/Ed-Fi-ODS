63e14d39af0159a8e712e8bc7b8e9a1d9c71cb1f
Johnny Brenes <johnny.brenes@simpat.tech>
2024-02-20 16:35:48 -0600

    change system to microsoft data sql client

--- Application/EdFi.Ods.Api/Security/AuthorizationStrategies/Relationships/Filters/SqlServerViewBasedSingleItemAuthorizationQuerySupport.cs
+++ Application/EdFi.Ods.Api/Security/AuthorizationStrategies/Relationships/Filters/SqlServerViewBasedSingleItemAuthorizationQuerySupport.cs
@@ -5,7 +5,7 @@
 
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using EdFi.Ods.Common.Security.Authorization;
 using EdFi.Ods.Common.Security.Claims;
