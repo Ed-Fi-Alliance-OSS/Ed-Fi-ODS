509f74374fcddd5d8161b6c2619ff2db5d2ca8e5
Eric Stockdale <ericstockdale@gmail.com>
2024-02-24 12:21:00 -0700

    Updated Edi.Ods.Api from FluentValidation 8.6.3 to 10.4.0

--- Application/EdFi.Ods.Api/Validation/DescriptorNamespaceValidator.cs
+++ Application/EdFi.Ods.Api/Validation/DescriptorNamespaceValidator.cs
@@ -33,19 +33,19 @@ namespace EdFi.Ods.Api.Validation
 
         public DescriptorNamespaceValidator()
         {
-            ValidatorOptions.CascadeMode = CascadeMode.StopOnFirstFailure;
-
             RuleFor(x => x.Namespace)
-               .Must(NotBeNullOrWhitespace)
-               .WithMessage(RequiredMessage)
-               .Must(BeValidNamespaceFormat)
-               .WithMessage(InvalidFormatMessage + ValidNamespaceFormatMessage);
+                .Cascade(CascadeMode.Stop)
+                .Must(NotBeNullOrWhitespace)
+                .WithMessage(RequiredMessage)
+                .Must(BeValidNamespaceFormat)
+                .WithMessage(InvalidFormatMessage + ValidNamespaceFormatMessage);
 
             RuleFor(
                     x => CaptureNamespaceGroups.Match(x.Namespace)
                                                .Groups["scheme"]
                                                .Value)
-               .Must(NotBeNullOrWhitespace)
+                .Cascade(CascadeMode.Stop)
+                .Must(NotBeNullOrWhitespace)
                .When(IsValidDescriptorNamespaceFormat)
                .WithMessage(RequiredMessage + ValidNamespaceFormatMessage)
                .Must(x => ValidScheme.IsMatch(x))
@@ -57,7 +57,8 @@ namespace EdFi.Ods.Api.Validation
                     x => CaptureNamespaceGroups.Match(x.Namespace)
                                                .Groups["organizationName"]
                                                .Value)
-               .Must(NotBeNullOrWhitespace)
+                .Cascade(CascadeMode.Stop)
+                .Must(NotBeNullOrWhitespace)
                .When(IsValidDescriptorNamespaceFormat)
                .WithMessage(RequiredMessage + ValidNamespaceFormatMessage)
                .Must(x => !InvalidOrganizationNameCharacters.IsMatch(x))
@@ -69,7 +70,8 @@ namespace EdFi.Ods.Api.Validation
                     x => CaptureNamespaceGroups.Match(x.Namespace)
                                                .Groups["descriptorName"]
                                                .Value)
-               .Must(NotBeNullOrWhitespace)
+                .Cascade(CascadeMode.Stop)
+                .Must(NotBeNullOrWhitespace)
                .When(IsValidDescriptorNamespaceFormat)
                .WithMessage(RequiredMessage + ValidNamespaceFormatMessage)
                .Must(x => !InvalidDescriptorNameCharacters.IsMatch(x))
@@ -78,6 +80,7 @@ namespace EdFi.Ods.Api.Validation
                .WithMessage(InvalidDescriptorNameMessage);
 
             RuleFor(x => x.CodeValue)
+                .Cascade(CascadeMode.Stop)
                .Must(NotBeNullOrWhitespace)
                .WithMessage(RequiredMessage)
                .Must(x => !InvalidCodeValueCharacters.IsMatch(x))
