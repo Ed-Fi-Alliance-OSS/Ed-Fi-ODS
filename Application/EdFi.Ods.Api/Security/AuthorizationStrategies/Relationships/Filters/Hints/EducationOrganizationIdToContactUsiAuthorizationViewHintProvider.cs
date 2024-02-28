16df015e73ad34ce702a38af974d72031fd15900
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2024-02-27 20:09:30 -0600

    Implemented relationship-based auth view failure hints, and made authorization failure messages returned when a referenced person exists indistinguishable from when it does not (missed ODS-6031 requirement).

---
+++ Application/EdFi.Ods.Api/Security/AuthorizationStrategies/Relationships/Filters/Hints/EducationOrganizationIdToContactUsiAuthorizationViewHintProvider.cs
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+namespace EdFi.Ods.Api.Security.AuthorizationStrategies.Relationships.Filters.Hints;
+
+public class EducationOrganizationIdToContactUsiAuthorizationViewHintProvider : IAuthorizationViewHintProvider
+{
+    public string GetFailureHint(string viewName)
+    {
+        if (viewName.StartsWith("EducationOrganizationIdToContactUSI"))
+        {
+            return "Ensure that the corresponding 'StudentSchoolAssociation' and 'StudentContactAssociation' resource items have been created.";
+        }
+
+        return null;
+    }
+}
