f3a4ef42a23dac0e259632cfeda96e1c2bb33ce5
Matthew Jackson <mjackson@unicon.net>
2024-04-03 10:49:40 -0500

    Test updates

--- Application/EdFi.Ods.Tests/EdFi.Ods.Api/Helpers/AssemblyLoaderHelperTests.cs
+++ Application/EdFi.Ods.Tests/EdFi.Ods.Api/Helpers/AssemblyLoaderHelperTests.cs
@@ -12,7 +12,7 @@ using EdFi.Ods.Api.Helpers;
 using NUnit.Framework;
 using Shouldly;
 
-namespace EdFi.Ods.Tests.EdFi.Ods.Api.Utilities;
+namespace EdFi.Ods.Tests.EdFi.Ods.Api.Helpers;
 
 [TestFixture]
 public class AssemblyLoaderHelperTests
@@ -21,30 +21,75 @@ public class AssemblyLoaderHelperTests
     [TestFixture]
     public class When_finding_plugin_assemblies
     {
-        private const string UnitTestPluginFolder = "UnitTestPluginFolder";
-        private const string EmptyUnitTestPluginFolder = "UnitTestEmptyPlugins";
+        private const string OriginalPluginFolder = "../../../../../../Ed-Fi-Ods-Implementation/Plugin";
+        private const string InvalidPluginAssemblyFileName = "InvalidPluginAssembly.dll";
+
+        // Contains the default plugin assemblies that are copied from the original plugin folder
+        private const string UnitTestPluginFolder = "UnitTestPlugins";
+
+        // Contains the default plugin assemblies that are copied from the original plugin folder
+        // and a non-plugin assembly that is a copy of the currently executing assembly dll
+        private const string UnitTestPluginWithNonPluginAssemblyFolder = "UnitTestPluginsWithNonPluginAssembly";
+
+        // Contains the default plugin assemblies that are copied from the original plugin folder
+        // and an invalid plugin assembly that is copied from the embedded resources
+        private const string UnitTestInvalidPluginFolder = "UnitTestPluginsWithInvalidAssembly";
+
+        // Folder that remains empty
+        private const string UnitTestEmptyPluginFolder = "UnitTestPluginsEmpty";
+
+        private readonly string _nonPluginAssemblyFileName;
+        private readonly string _nonPluginAssemblyLocation;
+
+        public When_finding_plugin_assemblies()
+        {
+            _nonPluginAssemblyLocation = Assembly.GetCallingAssembly().Location;
+            _nonPluginAssemblyFileName = Path.GetFileName(_nonPluginAssemblyLocation);
+        }
 
         [OneTimeSetUp]
         public void Setup()
         {
             Directory.CreateDirectory(UnitTestPluginFolder);
-            Directory.CreateDirectory(EmptyUnitTestPluginFolder);
+            Directory.CreateDirectory(UnitTestEmptyPluginFolder);
+            Directory.CreateDirectory(UnitTestPluginWithNonPluginAssemblyFolder);
+            Directory.CreateDirectory(UnitTestInvalidPluginFolder);
 
-            var originalPluginFolder = "../../../../../../Ed-Fi-Ods-Implementation/Plugin";
-            CopyDirectory(originalPluginFolder, UnitTestPluginFolder, true, true);
+            // Save the InvalidPluginAssembly.dll EmbeddedResource as a file in the UnitTestInvalidPlugins Folder
+            using (var fileStream = File.Create(Path.Combine(UnitTestInvalidPluginFolder, InvalidPluginAssemblyFileName)))
+            {
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("EdFi.Ods.Tests._EmbeddedResources.InvalidPluginAssembly.dll")!.CopyTo(fileStream);
+            }
+
+
+            // Copy a non-plugin dll into the folder to ensure that the method does not return it
+            File.Copy(_nonPluginAssemblyLocation ?? "", Path.Combine(UnitTestPluginWithNonPluginAssemblyFolder, _nonPluginAssemblyFileName), true);
+
+
+            CopyDirectory(OriginalPluginFolder, UnitTestPluginFolder, true, true);
+            CopyDirectory(OriginalPluginFolder, UnitTestPluginWithNonPluginAssemblyFolder, true, true);
+            CopyDirectory(OriginalPluginFolder, UnitTestInvalidPluginFolder, true, true);
+
         }
 
         [OneTimeTearDown]
-        public void TearDown()
+        public void OneTimeTearDown()
         {
-            if (Directory.Exists(UnitTestPluginFolder))
+            var foldersToDelete = new List<string>
             {
-                Directory.Delete(UnitTestPluginFolder, true);
-            }
+                UnitTestPluginFolder,
+                UnitTestEmptyPluginFolder,
+                UnitTestPluginWithNonPluginAssemblyFolder,
+                UnitTestInvalidPluginFolder
+            };
 
-            if (Directory.Exists(EmptyUnitTestPluginFolder))
+            // Delete the folders created for the tests
+            foreach (var folder in foldersToDelete)
             {
-                Directory.Delete(EmptyUnitTestPluginFolder, true);
+                if (Directory.Exists(folder))
+                {
+                    Directory.Delete(folder, true);
+                }
             }
         }
 
@@ -76,6 +121,8 @@ public class AssemblyLoaderHelperTests
             var result = AssemblyLoaderHelper.FindPluginAssemblies(UnitTestPluginFolder, includeExtensionAssemblies: false);
 
             // Assert
+            FileExistsInDirectory("EdFi.Ods.Extensions.Sample.dll", UnitTestPluginFolder, true).ShouldBeTrue(); 
+
             result.Select(s => s.Split("\\").Last()).ToList()
                 .ShouldBeEquivalentTo(new List<string>
                 {
@@ -87,17 +134,12 @@ public class AssemblyLoaderHelperTests
         public void Should_not_include_non_plugin_assemblies_located_in_plugin_folder()
         {
             // Arrange
-            var nonPluginAssemblyLocation = Assembly.GetCallingAssembly().Location;
-            var nonPluginAssemblyFileName = Path.GetFileName(nonPluginAssemblyLocation);
-
-            // Copy a non-plugin dll into the folder to ensure that the method does not return it
-            File.Copy(nonPluginAssemblyLocation, Path.Combine(UnitTestPluginFolder, nonPluginAssemblyFileName), true);
 
             // Act
             var result = AssemblyLoaderHelper.FindPluginAssemblies(UnitTestPluginFolder);
 
             // Assert
-            Assert.That(File.Exists(Path.Combine(UnitTestPluginFolder, nonPluginAssemblyFileName)));
+            File.Exists(Path.Combine(UnitTestPluginWithNonPluginAssemblyFolder, _nonPluginAssemblyFileName)).ShouldBeTrue();
 
             result.Select(s => s.Split("\\").Last()).ToList()
                 .ShouldBeEquivalentTo(new List<string>
@@ -115,7 +157,7 @@ public class AssemblyLoaderHelperTests
             // Arrange
 
             // Act
-            var result = AssemblyLoaderHelper.FindPluginAssemblies(EmptyUnitTestPluginFolder);
+            var result = AssemblyLoaderHelper.FindPluginAssemblies(UnitTestEmptyPluginFolder);
 
             // Assert
             result.ShouldBeEmpty();
@@ -125,10 +167,10 @@ public class AssemblyLoaderHelperTests
         public void Should_return_empty_list_when_plugin_folder_does_not_exist()
         {
             // Arrange
-            var nonExistantPluginFolder = "ThisPluginFolderDoesNotExist";
+            var nonExistentPluginFolder = "ThisPluginFolderDoesNotExist";
 
             // Act
-            var result = AssemblyLoaderHelper.FindPluginAssemblies(nonExistantPluginFolder);
+            var result = AssemblyLoaderHelper.FindPluginAssemblies(nonExistentPluginFolder);
 
             // Assert
             result.ShouldBeEmpty();
@@ -173,6 +215,15 @@ public class AssemblyLoaderHelperTests
             result.ShouldBeEmpty();
         }
 
+        [Test]
+        public void Should_throw_exception_when_an_invalid_plugin_assembly_is_in_plugin_folder()
+        {
+            // Arrange
+
+            // Act & Assert
+            Should.Throw<Exception>(() => AssemblyLoaderHelper.FindPluginAssemblies(UnitTestInvalidPluginFolder).ToList()).Message.ShouldBe("No plugin artifacts were found in assembly 'InvalidPluginAssembly.dll'. Expected an IPluginMarker implementation and assembly metadata embedded resource 'assemblyMetadata.json' (for Profiles or Extensions plugins), or implementations of IPlugin and/or IPlugModule (for custom application plugins).");
+        }
+
         // This method is from https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
         // With some modifications
         private static void CopyDirectory(string sourceDir, string destinationDir, bool recursive, bool overwrite)
@@ -206,6 +257,42 @@ public class AssemblyLoaderHelperTests
                     CopyDirectory(subDir.FullName, newDestinationDir, true, overwrite);
                 }
             }
+        }
+
+        private static bool FileExistsInDirectory(string fileName, string directory, bool recursive)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(directory);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                if (file.Name.Equals(fileName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    if (FileExistsInDirectory(fileName, subDir.FullName, true))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
