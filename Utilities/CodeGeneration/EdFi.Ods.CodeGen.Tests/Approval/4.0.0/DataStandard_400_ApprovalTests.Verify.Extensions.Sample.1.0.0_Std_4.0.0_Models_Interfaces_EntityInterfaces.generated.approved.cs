ab5682819f8a9719cb6cdb7ed4ba8dc530874512
Vinaya Mayya <vinaya.mayya@ed-fi.org>
2024-05-10 14:19:25 -0500

    [ODS-6372] Update npgsql package (#1045)

--- Utilities/CodeGeneration/EdFi.Ods.CodeGen.Tests/Approval/4.0.0/DataStandard_400_ApprovalTests.Verify.Extensions.Sample.1.0.0_Std_4.0.0_Models_Interfaces_EntityInterfaces.generated.approved.cs
+++ Utilities/CodeGeneration/EdFi.Ods.CodeGen.Tests/Approval/4.0.0/DataStandard_400_ApprovalTests.Verify.Extensions.Sample.1.0.0_Std_4.0.0_Models_Interfaces_EntityInterfaces.generated.approved.cs
@@ -2039,6 +2039,8 @@ namespace EdFi.Ods.Entities.Common.Sample
 
         // One-to-one relationships
 
+        IStudentArtProgramAssociationFavoriteBook StudentArtProgramAssociationFavoriteBook { get; set; }
+
         // Lists
         ICollection<IStudentArtProgramAssociationArtMedium> StudentArtProgramAssociationArtMedia { get; set; }
         ICollection<IStudentArtProgramAssociationPortfolioYears> StudentArtProgramAssociationPortfolioYears { get; set; }
@@ -2074,11 +2076,13 @@ namespace EdFi.Ods.Entities.Common.Sample
             bool isReasonExitedDescriptorSupported,
             bool isServedOutsideOfRegularSessionSupported,
             bool isStudentArtProgramAssociationArtMediaSupported,
+            bool isStudentArtProgramAssociationFavoriteBookSupported,
             bool isStudentArtProgramAssociationPortfolioYearsSupported,
             bool isStudentArtProgramAssociationServicesSupported,
             bool isStudentArtProgramAssociationStylesSupported,
             bool isStudentReferenceSupported,
             bool isGeneralStudentProgramAssociationParticipationStatusCreatable,
+            bool isStudentArtProgramAssociationFavoriteBookCreatable,
             bool isGeneralStudentProgramAssociationProgramParticipationStatusesItemCreatable,
             Func<IGeneralStudentProgramAssociationProgramParticipationStatus, bool> isGeneralStudentProgramAssociationProgramParticipationStatusIncluded,
             bool isStudentArtProgramAssociationArtMediaItemCreatable,
@@ -2110,11 +2114,13 @@ namespace EdFi.Ods.Entities.Common.Sample
             IsReasonExitedDescriptorSupported = isReasonExitedDescriptorSupported;
             IsServedOutsideOfRegularSessionSupported = isServedOutsideOfRegularSessionSupported;
             IsStudentArtProgramAssociationArtMediaSupported = isStudentArtProgramAssociationArtMediaSupported;
+            IsStudentArtProgramAssociationFavoriteBookSupported = isStudentArtProgramAssociationFavoriteBookSupported;
             IsStudentArtProgramAssociationPortfolioYearsSupported = isStudentArtProgramAssociationPortfolioYearsSupported;
             IsStudentArtProgramAssociationServicesSupported = isStudentArtProgramAssociationServicesSupported;
             IsStudentArtProgramAssociationStylesSupported = isStudentArtProgramAssociationStylesSupported;
             IsStudentReferenceSupported = isStudentReferenceSupported;
             IsGeneralStudentProgramAssociationParticipationStatusCreatable = isGeneralStudentProgramAssociationParticipationStatusCreatable;
+            IsStudentArtProgramAssociationFavoriteBookCreatable = isStudentArtProgramAssociationFavoriteBookCreatable;
             IsGeneralStudentProgramAssociationProgramParticipationStatusesItemCreatable = isGeneralStudentProgramAssociationProgramParticipationStatusesItemCreatable;
             IsGeneralStudentProgramAssociationProgramParticipationStatusIncluded = isGeneralStudentProgramAssociationProgramParticipationStatusIncluded;
             IsStudentArtProgramAssociationArtMediaItemCreatable = isStudentArtProgramAssociationArtMediaItemCreatable;
@@ -2146,11 +2152,13 @@ namespace EdFi.Ods.Entities.Common.Sample
         public bool IsReasonExitedDescriptorSupported { get; }
         public bool IsServedOutsideOfRegularSessionSupported { get; }
         public bool IsStudentArtProgramAssociationArtMediaSupported { get; }
+        public bool IsStudentArtProgramAssociationFavoriteBookSupported { get; }
         public bool IsStudentArtProgramAssociationPortfolioYearsSupported { get; }
         public bool IsStudentArtProgramAssociationServicesSupported { get; }
         public bool IsStudentArtProgramAssociationStylesSupported { get; }
         public bool IsStudentReferenceSupported { get; }
         public bool IsGeneralStudentProgramAssociationParticipationStatusCreatable { get; }
+        public bool IsStudentArtProgramAssociationFavoriteBookCreatable { get; }
         public bool IsGeneralStudentProgramAssociationProgramParticipationStatusesItemCreatable { get; }
         public Func<IGeneralStudentProgramAssociationProgramParticipationStatus, bool> IsGeneralStudentProgramAssociationProgramParticipationStatusIncluded { get; }
         public bool IsStudentArtProgramAssociationArtMediaItemCreatable { get; }
@@ -2204,6 +2212,8 @@ namespace EdFi.Ods.Entities.Common.Sample
                     return IsServedOutsideOfRegularSessionSupported;
                 case "StudentArtProgramAssociationArtMedia":
                     return IsStudentArtProgramAssociationArtMediaSupported;
+                case "StudentArtProgramAssociationFavoriteBook":
+                    return IsStudentArtProgramAssociationFavoriteBookSupported;
                 case "StudentArtProgramAssociationPortfolioYears":
                     return IsStudentArtProgramAssociationPortfolioYearsSupported;
                 case "StudentArtProgramAssociationServices":
@@ -2236,6 +2246,8 @@ namespace EdFi.Ods.Entities.Common.Sample
             {
                 case "GeneralStudentProgramAssociationParticipationStatus":
                     return IsGeneralStudentProgramAssociationParticipationStatusCreatable;
+                case "StudentArtProgramAssociationFavoriteBook":
+                    return IsStudentArtProgramAssociationFavoriteBookCreatable;
                 case "GeneralStudentProgramAssociationProgramParticipationStatuses":
                     return IsGeneralStudentProgramAssociationProgramParticipationStatusesItemCreatable;
                 case "StudentArtProgramAssociationArtMedia":
@@ -2288,6 +2300,135 @@ namespace EdFi.Ods.Entities.Common.Sample
         {
             switch (memberName)
             {
+                // Additional inspection support for identifying properties (which are implicitly supported by Profiles) for use during validation
+                case "ArtMediumDescriptor":
+                    return true;
+                default:
+                    throw new Exception($"Unknown member '{memberName}'.");
+            }
+        }
+
+        bool IMappingContract.IsItemCreatable(string memberName) => throw new Exception($"Unknown child item member '{memberName}'.");
+
+    }
+
+    /// <summary>
+    /// Defines available properties and methods for the abstraction of the StudentArtProgramAssociationFavoriteBook model.
+    /// </summary>
+    public interface IStudentArtProgramAssociationFavoriteBook : ISynchronizable, IMappable, IGetByExample
+    {
+        // Primary Key properties
+        IStudentArtProgramAssociation StudentArtProgramAssociation { get; set; }
+
+        // Non-PK properties
+        string BookTitle { get; set; }
+        string FavoriteBookCategoryDescriptor { get; set; }
+
+        // One-to-one relationships
+
+        // Lists
+        ICollection<IStudentArtProgramAssociationFavoriteBookArtMedium> StudentArtProgramAssociationFavoriteBookArtMedia { get; set; }
+
+        // Resource reference data
+    }
+
+    /// <summary>
+    /// Defines a mapping contract appropriate for a particular context when data is either being mapped or synchronized
+    /// between entities/resources during API request processing.
+    /// </summary>
+    public class StudentArtProgramAssociationFavoriteBookMappingContract : IMappingContract
+    {
+        public StudentArtProgramAssociationFavoriteBookMappingContract(
+            bool isBookTitleSupported,
+            bool isFavoriteBookCategoryDescriptorSupported,
+            bool isStudentArtProgramAssociationFavoriteBookArtMediaSupported,
+            bool isStudentArtProgramAssociationFavoriteBookArtMediaItemCreatable,
+            Func<IStudentArtProgramAssociationFavoriteBookArtMedium, bool> isStudentArtProgramAssociationFavoriteBookArtMediumIncluded
+            )
+        {
+            IsBookTitleSupported = isBookTitleSupported;
+            IsFavoriteBookCategoryDescriptorSupported = isFavoriteBookCategoryDescriptorSupported;
+            IsStudentArtProgramAssociationFavoriteBookArtMediaSupported = isStudentArtProgramAssociationFavoriteBookArtMediaSupported;
+            IsStudentArtProgramAssociationFavoriteBookArtMediaItemCreatable = isStudentArtProgramAssociationFavoriteBookArtMediaItemCreatable;
+            IsStudentArtProgramAssociationFavoriteBookArtMediumIncluded = isStudentArtProgramAssociationFavoriteBookArtMediumIncluded;
+        }
+
+        public bool IsBookTitleSupported { get; }
+        public bool IsFavoriteBookCategoryDescriptorSupported { get; }
+        public bool IsStudentArtProgramAssociationFavoriteBookArtMediaSupported { get; }
+        public bool IsStudentArtProgramAssociationFavoriteBookArtMediaItemCreatable { get; }
+        public Func<IStudentArtProgramAssociationFavoriteBookArtMedium, bool> IsStudentArtProgramAssociationFavoriteBookArtMediumIncluded { get; }
+
+        bool IMappingContract.IsMemberSupported(string memberName)
+        {
+            switch (memberName)
+            {
+                case "BookTitle":
+                    return IsBookTitleSupported;
+                case "FavoriteBookCategoryDescriptor":
+                    return IsFavoriteBookCategoryDescriptorSupported;
+                case "StudentArtProgramAssociationFavoriteBookArtMedia":
+                    return IsStudentArtProgramAssociationFavoriteBookArtMediaSupported;
+                // Additional inspection support for identifying properties (which are implicitly supported by Profiles) for use during validation
+                default:
+                    throw new Exception($"Unknown member '{memberName}'.");
+            }
+        }
+
+        bool IMappingContract.IsItemCreatable(string memberName)
+        {
+            switch (memberName)
+            {
+                case "StudentArtProgramAssociationFavoriteBookArtMedia":
+                    return IsStudentArtProgramAssociationFavoriteBookArtMediaItemCreatable;
+                default:
+                    throw new Exception($"Unknown child item '{memberName}'.");
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Defines available properties and methods for the abstraction of the StudentArtProgramAssociationFavoriteBookArtMedium model.
+    /// </summary>
+    public interface IStudentArtProgramAssociationFavoriteBookArtMedium : ISynchronizable, IMappable, IGetByExample
+    {
+        // Primary Key properties
+        IStudentArtProgramAssociationFavoriteBook StudentArtProgramAssociationFavoriteBook { get; set; }
+        
+        string ArtMediumDescriptor { get; set; }
+
+        // Non-PK properties
+        int? ArtPieces { get; set; }
+
+        // One-to-one relationships
+
+        // Lists
+
+        // Resource reference data
+    }
+
+    /// <summary>
+    /// Defines a mapping contract appropriate for a particular context when data is either being mapped or synchronized
+    /// between entities/resources during API request processing.
+    /// </summary>
+    public class StudentArtProgramAssociationFavoriteBookArtMediumMappingContract : IMappingContract
+    {
+        public StudentArtProgramAssociationFavoriteBookArtMediumMappingContract(
+            bool isArtPiecesSupported
+            )
+        {
+            IsArtPiecesSupported = isArtPiecesSupported;
+        }
+
+        public bool IsArtPiecesSupported { get; }
+
+        bool IMappingContract.IsMemberSupported(string memberName)
+        {
+            switch (memberName)
+            {
+                case "ArtPieces":
+                    return IsArtPiecesSupported;
                 // Additional inspection support for identifying properties (which are implicitly supported by Profiles) for use during validation
                 case "ArtMediumDescriptor":
                     return true;
