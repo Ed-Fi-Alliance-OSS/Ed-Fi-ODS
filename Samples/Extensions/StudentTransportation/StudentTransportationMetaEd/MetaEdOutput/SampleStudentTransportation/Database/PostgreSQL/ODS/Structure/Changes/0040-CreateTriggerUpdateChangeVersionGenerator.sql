bd762a0dd5b53d447a30add6597b9079c3e4fec7
Jesus Flores <jesus@simpat.tech>
2024-05-30 22:25:12 -0600

    Replace obsolete sample

--- Samples/Extensions/StudentTransportation/StudentTransportationMetaEd/MetaEdOutput/SampleStudentTransportation/Database/PostgreSQL/ODS/Structure/Changes/0040-CreateTriggerUpdateChangeVersionGenerator.sql
+++
@@ -1,8 +0,0 @@
--- SPDX-License-Identifier: Apache-2.0
--- Licensed to the Ed-Fi Alliance under one or more agreements.
--- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
--- See the LICENSE and NOTICES files in the project root for more information.
-
-CREATE TRIGGER UpdateChangeVersion BEFORE UPDATE ON samplestudenttransportation.StudentTransportation
-    FOR EACH ROW EXECUTE PROCEDURE changes.UpdateChangeVersion();
-
