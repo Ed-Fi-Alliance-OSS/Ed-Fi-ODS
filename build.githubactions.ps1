5528bb9d7a5f48712ba690ff96cd14677a1a4644
Vinaya Mayya <vinaya.mayya@ed-fi.org>
2024-07-02 08:42:15 -0500

    github action PowerShell update

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -7,7 +7,7 @@
 param(
     # Command to execute, defaults to "Build".
     [string]
-    [ValidateSet("DotnetClean", "Build", "Test", "Pack", "Publish", "CheckoutBranch")]
+    [ValidateSet("DotnetClean", "Build", "Test", "Pack", "Publish", "CheckoutBranch", "InstallCredentialHandler")]
     $Command = "Build",
 
     [switch] $SelfContained,
@@ -207,6 +207,35 @@ function CheckoutBranch {
     }
 }
 
+function InstallCredentialHandler {
+    if (Get-IsWindows -and -not Get-InstalledModule | Where-Object -Property Name -eq "7Zip4Powershell") {
+         Install-Module -Force -Scope CurrentUser -Name 7Zip4Powershell
+         Write-Host "Installed 7Zip4Powershell."
+    }
+     # using WebClient is faster then Invoke-WebRequest but shows no progress
+     $sourceUrl = ' https://github.com/microsoft/artifacts-credprovider/releases/download/v1.0.0/Microsoft.NuGet.CredentialProvider.zip'
+     $fileName = 'Microsoft.NuGet.CredentialProvider.zip'
+     $zipFilePath = Join-Path ([IO.Path]::GetTempPath()) $fileName
+     Write-Host "Downloading file from $sourceUrl..."
+     $webClient = New-Object System.Net.WebClient
+     $webClient.DownloadFile($sourceUrl, $zipFilePath)
+     Write-Host "Download complete." 
+     if (-not (Test-Path $zipFilePath)) {
+         Write-Warning "Microsoft.NuGet.CredentialProvider file '$fileName' not found."
+         exit 0
+     }
+     $packageFolder = Join-Path ([IO.Path]::GetTempPath()) 'Microsoft.NuGet.CredentialProvider/'
+     if ($fileName.EndsWith('.zip')) {
+         Write-Host "Extracting $fileName..."
+         
+         if (Test-Path $zipFilePath) { Expand-Archive -Force -Path $zipFilePath -DestinationPath $packageFolder }
+         Copy-Item -Path $packageFolder\* -Destination "~/.nuget/" -Recurse -Force
+         Write-Host "Extracted to: ~\.nuget\plugins\" -ForegroundColor Green
+     }
+
+}
+
+
 function Invoke-Build {
     Write-Host "Building Version $version" -ForegroundColor Cyan
     Invoke-Step { DotnetClean }
@@ -231,6 +260,9 @@ function Invoke-Pack {
 
 function Invoke-CheckoutBranch {
     Invoke-Step { CheckoutBranch }
+}
+function Invoke-InstallCredentialHandler {
+    Invoke-Step { InstallCredentialHandler }
 }
 
 Invoke-Main {
