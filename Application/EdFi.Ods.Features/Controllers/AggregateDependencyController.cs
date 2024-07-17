c41e8dd90c30a22cf8e61e57c86b0b7cb1586d2a
Axel Marquez <158314644+axelmarquezh@users.noreply.github.com>
2024-07-17 00:26:59 -0600

    [ODS-6443] Dependency order endpoints don't list update order - v5.4 (#1104)
    Co-authored-by: Ben Tillman <ben@headspring.com>

--- Application/EdFi.Ods.Features/Controllers/AggregateDependencyController.cs
+++ Application/EdFi.Ods.Features/Controllers/AggregateDependencyController.cs
@@ -49,10 +49,12 @@ namespace EdFi.Ods.Features.Controllers
 
             try
             {
+                var groupedLoadOrder = GetGroupedLoadOrder(_resourceLoadGraphFactory.CreateResourceLoadGraph()).ToList();
+                ModifyLoadOrderForAuthorizationConcerns(groupedLoadOrder);
                 return Request.GetTypedHeaders().Accept != null
                     && Request.GetTypedHeaders().Accept.Any(a => a.MediaType.Value.EqualsIgnoreCase(CustomMediaContentTypes.GraphML))
                 ? Ok(CreateGraphML(_resourceLoadGraphFactory.CreateResourceLoadGraph()))
-                : Ok(GetGroupedLoadOrder(_resourceLoadGraphFactory.CreateResourceLoadGraph()));
+                : Ok(groupedLoadOrder);
             }
             catch (NonAcyclicGraphException e)
             {
@@ -120,5 +122,113 @@ namespace EdFi.Ods.Features.Controllers
 
         private static string GetNodeId(Resource resource)
             => $"/{resource.SchemaUriSegment()}/{resource.PluralName.ToCamelCase()}";
+
+        private static void ModifyLoadOrderForAuthorizationConcerns(IList<ResourceLoadOrder> resources)
+        {
+            ApplyStudentTransformation(resources);
+            ApplyStaffTransformation(resources);
+            ApplyParentTransformation(resources);
+        }
+
+        private static void ApplyParentTransformation(IList<ResourceLoadOrder> resources)
+        {
+            // StudentParentAssociation must be created before Parents can be updated.
+            // StudentSchoolAssociation must be created before Parents can be updated.
+            // StudentSchoolAssociation must be created before ParentAssociations can be updated.
+            var parentCreate = resources
+                .SingleOrDefault(r => r.Resource == "/ed-fi/parents");
+
+            if (parentCreate == null)
+            {
+                return;
+            }
+
+            var studentParentAssociation = resources
+                .Single(r => r.Resource == "/ed-fi/studentParentAssociations");
+
+            var studentSchoolAssociation = resources
+                .Single(r => r.Resource == "/ed-fi/studentSchoolAssociations");
+
+            var higherOrder = Math.Max(studentParentAssociation.Order, studentSchoolAssociation.Order);
+
+            var parentUpdate = new ResourceLoadOrder
+            {
+                Resource = parentCreate.Resource,
+                Order = higherOrder + 1,
+                Operations = new List<string> { "Update" }
+            };
+
+            parentCreate.Operations.Remove("Update");
+
+            resources.Insert(
+                resources.IndexOf(resources.FirstOrDefault(r => r.Order == parentUpdate.Order) ?? resources[^1])
+                , parentUpdate
+            );
+        }
+
+        private static void ApplyStaffTransformation(IList<ResourceLoadOrder> resources)
+        {
+            // StaffEducationOrganizationEmploymentAssociation or StaffEducationOrganizationAssignmentAssociation
+            //must be created before Staff can be updated.
+            var staffCreate = resources
+                .SingleOrDefault(r => r.Resource == "/ed-fi/staffs");
+
+            if (staffCreate == null)
+            {
+                return;
+            }
+
+            var staffEducationOrganizationEmploymentAssociation = resources
+                .Single(r => r.Resource == "/ed-fi/staffEducationOrganizationEmploymentAssociations");
+
+            var staffEducationOrganizationAssignmentAssociation = resources
+                .Single(r => r.Resource == "/ed-fi/staffEducationOrganizationAssignmentAssociations");
+
+            var highestOrder = Math.Max(
+                staffEducationOrganizationAssignmentAssociation.Order, staffEducationOrganizationEmploymentAssociation.Order);
+
+            var staffUpdate = new ResourceLoadOrder()
+            {
+                Resource = staffCreate.Resource,
+                Order = highestOrder + 1,
+                Operations = new List<string> { "Update" }
+            };
+
+            staffCreate.Operations.Remove("Update");
+
+            resources.Insert(
+                resources.IndexOf(resources.FirstOrDefault(r => r.Order == staffUpdate.Order) ?? resources[^1])
+                , staffUpdate
+            );
+        }
+
+        private static void ApplyStudentTransformation(IList<ResourceLoadOrder> resources)
+        {
+            var studentCreate = resources
+                .SingleOrDefault(r => r.Resource == "/ed-fi/students");
+
+            if (studentCreate == null)
+            {
+                return;
+            }
+
+
+            var studentSchoolAssociation = resources
+                .Single(r => r.Resource == "/ed-fi/studentSchoolAssociations");
+
+            var studentUpdate = new ResourceLoadOrder()
+            {
+                Resource = studentCreate.Resource,
+                Order = studentSchoolAssociation.Order + 1,
+                Operations = new List<string> { "Update" }
+            };
+
+            studentCreate.Operations.Remove("Update");
+
+            resources.Insert(
+                resources.IndexOf(resources.FirstOrDefault(r => r.Order == studentUpdate.Order) ?? resources[^1])
+                , studentUpdate
+            );
+        }
     }
 }
