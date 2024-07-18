02f060491765e2b4af6bb69b4619157de3ca65d3
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2024-07-17 22:46:56 -0500

    Added logic to transform the SQL generated before NHibernate prior to execution, substituting in the custom view name appropriately.

--- Application/EdFi.Ods.Common/Infrastructure/Interceptors/CreateDateBasedTransientInterceptor.cs
+++ Application/EdFi.Ods.Common/Infrastructure/Interceptors/CreateDateBasedTransientInterceptor.cs
@@ -4,12 +4,22 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 using System;
+using System.Text.RegularExpressions;
+using EdFi.Ods.Common.Conventions;
+using EdFi.Ods.Common.Security.CustomViewBased;
 using NHibernate;
+using NHibernate.SqlCommand;
 
 namespace EdFi.Ods.Common.Infrastructure.Interceptors
 {
     public class CreateDateBasedTransientInterceptor : EmptyInterceptor
     {
+        private const string SpaceLiteral = " ";
+
+        private static readonly Regex regex = new(
+            @$"(?<join>inner join\s+)\(?(?<basisEntity>\w+\.\w+) (?<alias>(?<aliasPrefix>{CustomViewHelpers.CustomViewAliasPrefixBase}[a-f\d]{{4}}_).*?\d_).*?(?<onClause>on\s+\(\s+this_\d+_)",
+            RegexOptions.Compiled);
+
         public override bool? IsTransient(object entity)
         {
             var property = entity.GetType()
@@ -23,6 +33,37 @@ namespace EdFi.Ods.Common.Infrastructure.Interceptors
             }
 
             return base.IsTransient(entity);
+        }
+
+        public override SqlString OnPrepareStatement(SqlString sql)
+        {
+            // Determine if we need to perform custom authorization view SQL manipulation
+            if (sql.IndexOf(CustomViewHelpers.CustomViewAliasPrefixBase, 0, sql.Length, StringComparison.Ordinal) >= 0)
+            {
+                var matches = regex.Matches(sql.ToString());
+
+                for (int i = matches.Count - 1; i >= 0; i--)
+                {
+                    var match = matches[i];
+                    
+                    var preamble = sql.Substring(0, match.Groups["join"].Index + match.Groups["join"].Length);
+                    var viewName = CustomViewHelpers.GetViewName(match.Groups["aliasPrefix"].Value);
+
+                    var final = new SqlString(
+                        preamble,
+                        SystemConventions.AuthSchema,
+                        ".",
+                        viewName,
+                        SpaceLiteral,
+                        match.Groups["alias"].Value,
+                        SpaceLiteral,
+                        sql.Substring(match.Groups["onClause"].Index));
+                    
+                    sql = final;
+                }
+            }
+
+            return sql;
         }
     }
 }
