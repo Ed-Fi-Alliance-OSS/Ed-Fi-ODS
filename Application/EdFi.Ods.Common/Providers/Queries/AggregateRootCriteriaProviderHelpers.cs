9687799ae6c20129432d42cee1d7b35d48781e83
Matthew Jackson <mjackson@unicon.net>
2024-10-09 22:40:20 -0500

    Apply IdentificationCode query parameters to QueryBuilder

--- Application/EdFi.Ods.Common/Providers/Queries/AggregateRootCriteriaProviderHelpers.cs
+++ Application/EdFi.Ods.Common/Providers/Queries/AggregateRootCriteriaProviderHelpers.cs
@@ -3,7 +3,10 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
+using EdFi.Ods.Common.Models.Domain;
 using EdFi.Ods.Common.Specifications;
 
 namespace EdFi.Ods.Common.Providers.Criteria;
@@ -11,6 +14,7 @@ namespace EdFi.Ods.Common.Providers.Criteria;
 internal static class AggregateRootCriteriaProviderHelpers
 {
     private static string[] _uniqueIds;
+    private static readonly ConcurrentDictionary<string, EntityProperty> _entityIdentificationCodePropertyCache = [];
 
     public static string[] GetUniqueIdProperties(IPersonTypesProvider personTypesProvider)
     {
@@ -26,4 +30,36 @@ internal static class AggregateRootCriteriaProviderHelpers
         "SortBy",
         "SortDirection"
     };
+    
+    public static EntityProperty FindIdentificationCodeProperty(Entity entity)
+    {
+        var entityFullName = entity.FullName.Name;
+        var identificationCodeEntityProperty = _entityIdentificationCodePropertyCache.GetValueOrDefault(entityFullName);
+
+        if (identificationCodeEntityProperty != null)
+            return identificationCodeEntityProperty;
+
+        while (entity != null)
+        {
+            identificationCodeEntityProperty = FindIdentificationCodePropertyInAssociations(entity);
+
+            if (identificationCodeEntityProperty != null)
+            {
+                _entityIdentificationCodePropertyCache[entityFullName] = identificationCodeEntityProperty;
+                return identificationCodeEntityProperty;
+            }
+
+            entity = entity.BaseEntity;
+        }
+
+        return null;
+
+        EntityProperty FindIdentificationCodePropertyInAssociations(Entity entity)
+        {
+            return entity.OutgoingAssociations
+                .Select(association => association.OtherEntity.PropertyByName)
+                .Select(p => p.GetValueOrDefault("IdentificationCode"))
+                .FirstOrDefault(x => x != null);
+        }
+    }
 }
