541b87ebf598460227f6c8d89669ee5a7bf0b07e
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2024-10-19 02:33:17 -0500

    Added support for generating key indexes for MessagePack serialization of resource classes.

--- Application/EdFi.Ods.Api/Models/Link.cs
+++ Application/EdFi.Ods.Api/Models/Link.cs
@@ -3,6 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using MessagePack;
 using System.Runtime.Serialization;
 
 namespace EdFi.Ods.Api.Models
@@ -11,17 +12,20 @@ namespace EdFi.Ods.Api.Models
     /// Contains hyperlink details for a related resource.
     /// </summary>
     [DataContract]
+    [MessagePackObject]
     public class Link
     {
         /// <summary>
         /// Gets or sets the description of the relationship of the link to the containing resource.
         /// </summary>
+        [Key(0)]
         [DataMember(Name = "rel")]
         public string Rel { get; set; }
 
         /// <summary>
         /// Gets or sets the hyperlink to the related resource.
         /// </summary>
+        [Key(1)]
         [DataMember(Name = "href")]
         public string Href { get; set; }
     }
