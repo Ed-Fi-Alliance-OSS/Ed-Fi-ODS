c7d91d5a058abe37d9bcdd693870c8e15d732c6f
Matthew Jackson <mjackson@unicon.net>
2024-11-06 20:15:00 -0600

    Change database package creation to using dotnet cli

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -153,7 +153,19 @@ function Pack {
         }
     }
     if ($NuspecFilePath -Like "*.nuspec" -and $null -ne $PackageName){
-       nuget pack $NuspecFilePath -OutputDirectory $packageOutput -Version $version -Properties configuration=$Configuration -Properties id=$PackageName -NoPackageAnalysis -NoDefaultExcludes
+        $params = @{
+            PackageDefinitionFile = $NuspecFilePath
+            Version               = $version
+            Properties            = @{
+                configuration = $Configuration
+                id = $PackageName
+            }
+            OutputDirectory       = $packageOutput
+        }
+
+        & "$PSScriptRoot/../Ed-Fi-ODS-Implementation/Initialize-PowershellForDevelopment.ps1"
+
+        New-Package @params | Out-Host    
     }
     if ([string]::IsNullOrWhiteSpace($NuspecFilePath) -and $null -ne $PackageName){
         Invoke-Execute {
