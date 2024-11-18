fcd758e58939912991a6b8a3472815498fd71f55
m.semalaiappan <m.semalaiappan@gmail.com>
2024-11-18 17:58:50 -0600

    Pack Function updated

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -161,7 +161,24 @@ function Pack {
         $xml.package.metadata.id = $PackageName
         $xml.package.metadata.description = $PackageName
         $xml.Save($NuspecFilePath)
-        nuget pack $NuspecFilePath -OutputDirectory $packageOutput -Version $version -Properties configuration=$Configuration -Properties copyright=$Copyright  -NoPackageAnalysis -NoDefaultExcludes
+
+        if (Get-IsWindows){
+            nuget pack $NuspecFilePath -OutputDirectory $packageOutput -Version $version -Properties configuration=$Configuration -Properties copyright=$Copyright  -NoPackageAnalysis -NoDefaultExcludes
+        }
+        else{
+            $params = @{
+                PackageDefinitionFile = $NuspecFilePath
+                Version               = $version
+                PackageId             = $PackageName
+                OutputDirectory       = $packageOutput
+                BuildConfiguration    = $Configuration
+                ProjectFile           = $ProjectFile
+            }
+
+            & "$PSScriptRoot/../Ed-Fi-ODS-Implementation/Initialize-PowershellForDevelopment.ps1"
+
+            New-Package @params | Out-Host
+        }
 
     }
     if ([string]::IsNullOrWhiteSpace($NuspecFilePath) -and $null -ne $PackageName){
