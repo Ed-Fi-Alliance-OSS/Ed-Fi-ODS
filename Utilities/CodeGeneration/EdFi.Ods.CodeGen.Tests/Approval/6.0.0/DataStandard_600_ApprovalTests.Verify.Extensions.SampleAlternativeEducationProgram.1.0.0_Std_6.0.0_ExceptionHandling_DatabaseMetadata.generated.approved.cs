1cb329db0544acdc10407cf314571402b07050eb
m.semalaiappan <m.semalaiappan@gmail.com>
2025-07-08 21:48:38 -0500

    CodeGen Approval Test CodeFix

---
+++ Utilities/CodeGeneration/EdFi.Ods.CodeGen.Tests/Approval/6.0.0/DataStandard_600_ApprovalTests.Verify.Extensions.SampleAlternativeEducationProgram.1.0.0_Std_6.0.0_ExceptionHandling_DatabaseMetadata.generated.approved.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using EdFi.Ods.Api.ExceptionHandling;
+using EdFi.Ods.Api.Providers;
+
+namespace EdFi.Ods.Api.Common.ExceptionHandling.SampleAlternativeEducationProgram
+{
+    [ExcludeFromCodeCoverage]
+    public class DatabaseMetadataProvider : IDatabaseMetadataProvider
+    {
+        public IndexDetails GetIndexDetails(string indexName)
+        {
+            IndexDetails indexDetails = null;
+
+            IndexDetailsByName.TryGetValue(indexName, out indexDetails);
+
+            return indexDetails;
+        }
+
+        private static readonly Dictionary<string, IndexDetails> IndexDetailsByName = new Dictionary<string, IndexDetails>(StringComparer.InvariantCultureIgnoreCase) {
+            { "AlternativeEducationEligibilityReasonDescriptor_PK", new IndexDetails { IndexName = "AlternativeEducationEligibilityReasonDescriptor_PK", TableName = "AlternativeEducationEligibilityReasonDescriptor", ColumnNames = new List<string> { "AlternativeEducationEligibilityReasonDescriptorId" } } },
+            { "FK_StudentAlternativeEducationProgramAssociation_AlternativeEducationEligibilityReasonDescriptor", new IndexDetails { IndexName = "FK_StudentAlternativeEducationProgramAssociation_AlternativeEducationEligibilityReasonDescriptor", TableName = "StudentAlternativeEducationProgramAssociation", ColumnNames = new List<string> { "AlternativeEducationEligibilityReasonDescriptorId" } } },
+            { "StudentAlternativeEducationProgramAssociation_PK", new IndexDetails { IndexName = "StudentAlternativeEducationProgramAssociation_PK", TableName = "StudentAlternativeEducationProgramAssociation", ColumnNames = new List<string> { "BeginDate", "EducationOrganizationId", "ProgramEducationOrganizationId", "ProgramName", "ProgramTypeDescriptorId", "StudentUSI" } } },
+            { "FK_StudentAlternativeEducationProgramAssociationMeetingTime_StudentAlternativeEducationProgramAssociation", new IndexDetails { IndexName = "FK_StudentAlternativeEducationProgramAssociationMeetingTime_StudentAlternativeEducationProgramAssociation", TableName = "StudentAlternativeEducationProgramAssociationMeetingTime", ColumnNames = new List<string> { "BeginDate", "EducationOrganizationId", "ProgramEducationOrganizationId", "ProgramName", "ProgramTypeDescriptorId", "StudentUSI" } } },
+            { "StudentAlternativeEducationProgramAssociationMeetingTime_PK", new IndexDetails { IndexName = "StudentAlternativeEducationProgramAssociationMeetingTime_PK", TableName = "StudentAlternativeEducationProgramAssociationMeetingTime", ColumnNames = new List<string> { "BeginDate", "EducationOrganizationId", "ProgramEducationOrganizationId", "ProgramName", "ProgramTypeDescriptorId", "StudentUSI", "EndTime", "StartTime" } } },
+        };
+    }
+}
