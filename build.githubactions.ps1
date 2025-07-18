aef6456002d90caa6edf9c332d209be9313ce45a
Axel Marquez <axel.marqz@gmail.com>
2025-07-18 13:40:37 -0600

    [ODS-6653] Add DS6 as a copy of DS5.2 - Attempt 4

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -268,7 +268,7 @@ function InstallCredentialHandler {
          Write-Host "Installed 7Zip4Powershell."
     }
      # using WebClient is faster then Invoke-WebRequest but shows no progress
-     $sourceUrl = ' https://github.com/microsoft/artifacts-credprovider/releases/download/v1.0.0/Microsoft.NuGet.CredentialProvider.zip'
+     $sourceUrl = 'https://github.com/microsoft/artifacts-credprovider/releases/download/v1.4.1/Microsoft.NuGet.CredentialProvider.zip'
      $fileName = 'Microsoft.NuGet.CredentialProvider.zip'
      $zipFilePath = Join-Path ([IO.Path]::GetTempPath()) $fileName
      Write-Host "Downloading file from $sourceUrl..."
