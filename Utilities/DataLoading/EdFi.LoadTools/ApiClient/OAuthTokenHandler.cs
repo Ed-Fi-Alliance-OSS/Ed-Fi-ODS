95fa6f18bdf72dbd6d2c00011d8070a0464f1ef5
Jesus Flores <43448209+simpat-jesus@users.noreply.github.com>
2025-08-28 11:09:05 -0600

    [ODS-6688] Optimize SmokeTests execution time by implementing token caching (#1250)

--- Utilities/DataLoading/EdFi.LoadTools/ApiClient/OAuthTokenHandler.cs
+++ Utilities/DataLoading/EdFi.LoadTools/ApiClient/OAuthTokenHandler.cs
@@ -3,21 +3,30 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using System;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 
 namespace EdFi.LoadTools.ApiClient
 {
-    public interface IOAuthTokenHandler
+    public interface IOAuthTokenHandler : IDisposable
     {
         string GetBearerToken();
+        Task<string> GetBearerTokenAsync();
     }
 
-    public class OAuthTokenHandler : IOAuthTokenHandler
+    public class OAuthTokenHandler : IOAuthTokenHandler, IDisposable
     {
         private readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private readonly ITokenRetriever _tokenRetriever;
+        private readonly SemaphoreSlim _semaphore = new(1, 1);
+        private string _cachedToken;
+        private DateTime _tokenExpiration = DateTime.MinValue;
+
+        // Buffer time before expiration to request a new token (1 minute)
+        private static readonly TimeSpan ExpirationBuffer = TimeSpan.FromMinutes(1);
 
         public OAuthTokenHandler(ITokenRetriever tokenRetriever)
         {
@@ -26,13 +35,61 @@ namespace EdFi.LoadTools.ApiClient
 
         public string GetBearerToken()
         {
-            _log.Debug("Getting a new OAuth token");
-            var task = Task.Run(() => _tokenRetriever.ObtainNewBearerToken());
-            task.Wait();
+            return GetBearerTokenAsync().GetAwaiter().GetResult();
+        }
 
-            var bearerToken = task.Result;
+        public async Task<string> GetBearerTokenAsync()
+        {
+            // Check if we have a valid cached token
+            if (IsTokenValid())
+            {
+                _log.Debug("Using cached OAuth token");
+                return _cachedToken;
+            }
 
-            return bearerToken.Access_token;
+            // Use semaphore to ensure only one thread requests a new token at a time
+            await _semaphore.WaitAsync();
+            try
+            {
+                // Double-check pattern: another thread might have refreshed the token while we were waiting
+                if (IsTokenValid())
+                {
+                    _log.Debug("Using cached OAuth token (obtained while waiting)");
+                    return _cachedToken;
+                }
+
+                _log.Debug("Requesting a new OAuth token");
+                var bearerToken = await _tokenRetriever.ObtainNewBearerToken();
+
+                // Cache the new token and calculate expiration time
+                _cachedToken = bearerToken.Access_token;
+                _tokenExpiration = CalculateTokenExpiration(bearerToken.Expires_in);
+
+                _log.Debug($"New OAuth token cached, expires at: {_tokenExpiration:yyyy-MM-dd HH:mm:ss}");
+                return _cachedToken;
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private bool IsTokenValid()
+        {
+            return !string.IsNullOrEmpty(_cachedToken) &&
+                   DateTime.UtcNow < _tokenExpiration.Subtract(ExpirationBuffer);
+        }
+
+        private static DateTime CalculateTokenExpiration(int? expiresInSeconds)
+        {
+            // Default to 1 hour if expires_in is not provided
+            var expirationSeconds = expiresInSeconds ?? 3600;
+            return DateTime.UtcNow.AddSeconds(expirationSeconds);
+        }
+
+        public void Dispose()
+        {
+            _semaphore?.Dispose();
         }
     }
 }
