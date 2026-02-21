3651448970615df7682b3751e6a3d611d954f6c7
Stephen Fuqua <stephen@safnet.com>
2026-02-21 12:27:24 -0600

    Remove the CheckoutBranch function

--- build.githubactions.ps1
+++ build.githubactions.ps1
@@ -14,7 +14,6 @@ param(
         "Test",
         "Pack",
         "Publish",
-        "CheckoutBranch",
         "MaximumPathLengthLimitation",
         "InstallCredentialHandler",
         "ExtensionVersions",
@@ -220,43 +219,6 @@ function Test {
         Invoke-Execute { dotnet test $solution  -c $Configuration --no-build --no-restore -v normal }
     } else {
         Invoke-Execute { dotnet test $solution  -c $Configuration --no-build --no-restore -v normal --filter TestCategory!~"$TestFilter" }
-    }
-}
-
-# SFTODO: can we remove this now?
-function CheckoutBranch {
-    Set-Location $RelativeRepoPath
-    $odsBranch = $Env:REPOSITORY_DISPATCH_BRANCH
-    Write-Output "OdsBranch: $odsBranch"
-    if(![string]::IsNullOrEmpty($odsBranch)){
-        $patternName = "refs/heads/$odsBranch"
-        $does_corresponding_branch_exist = $false
-        $does_corresponding_branch_exist = git ls-remote --heads origin $odsBranch | Select-String -Pattern $patternName -SimpleMatch -Quiet
-        if ($does_corresponding_branch_exist -eq $true) {
-            Write-Output "Corresponding branch for $odsBranch exists in $RelativeRepoPath repo, so checking it out"
-            git fetch origin $odsBranch
-            git checkout $odsBranch
-        } else {
-            Write-Output "Corresponding branch for $odsBranch does not exist in Implementation repo, so not changing branch checked out"
-        }
-    } else {
-        Write-Output "ref_name: $Env:REF_NAME"
-        $current_branch = "$Env:REF_NAME"
-        if ($current_branch -like "*/merge"){
-            Write-Output "ref_name is PR, so using head_ref: $Env:HEAD_REF"
-            $current_branch = "$Env:HEAD_REF"
-        }
-        $patternName = "refs/heads/$current_branch"
-        Write-Output "Pattern Name is $patternName" -fore GREEN
-        $branch_exists = $false
-        $branch_exists = git ls-remote --heads origin $current_branch | Select-String -Pattern $patternName -SimpleMatch -Quiet
-        if ($branch_exists -eq $true) {
-            Write-Output "Current branch exists, so setting to $current_branch"
-            git fetch origin $current_branch
-            git checkout $current_branch
-        } else {
-            Write-Output "did not match on any results for changing ODS checkout branch"
-        }
     }
 }
 
@@ -488,10 +450,6 @@ function Invoke-Pack {
     Invoke-Step { Pack }
 }
 
-function Invoke-CheckoutBranch {
-    Invoke-Step { CheckoutBranch }
-}
-
 function Invoke-InstallCredentialHandler {
     Invoke-Step { InstallCredentialHandler }
 }
@@ -524,7 +482,6 @@ Invoke-Main {
         Test { Invoke-Tests }
         Pack { Invoke-Pack }
         Publish { Invoke-Publish }
-        CheckoutBranch { Invoke-CheckoutBranch }
         MaximumPathLengthLimitation { Invoke-MaximumPathLengthLimitation }
         InstallCredentialHandler { Invoke-InstallCredentialHandler }
         ExtensionVersions { Invoke-ExtensionVersions }
