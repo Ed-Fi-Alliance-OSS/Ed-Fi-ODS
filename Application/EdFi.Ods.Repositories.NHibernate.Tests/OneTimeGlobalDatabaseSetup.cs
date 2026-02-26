1b2f068c6faa16255fdfaec2d25524c431111180
m.semalaiappan <m.semalaiappan@gmail.com>
2026-02-25 23:25:32 -0600

    Configure Npgsql for .NET 10+ DateOnly/TimeOnly compatibility

--- Application/EdFi.Ods.Repositories.NHibernate.Tests/OneTimeGlobalDatabaseSetup.cs
+++ Application/EdFi.Ods.Repositories.NHibernate.Tests/OneTimeGlobalDatabaseSetup.cs
@@ -3,6 +3,8 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
+using EdFi.Ods.Common.Infrastructure.Compatibility;
+using Npgsql;
 using NUnit.Framework;
 using Test.Common;
 
@@ -21,6 +23,20 @@ namespace EdFi.Ods.Repositories.NHibernate.Tests
         protected override string DatabaseCopyPrefix
         {
             get => "EdFiOdsRepositoriesNHibernateTests";
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // Configure Npgsql for .NET 10+ DateOnly/TimeOnly compatibility
+            // Npgsql 10.0+ maps SQL DATE to DateOnly by default, but NHibernate expects DateTime
+            // See: https://www.npgsql.org/doc/release-notes/10.0.html
+            #pragma warning disable CS0618 // GlobalTypeMapper is obsolete but required for NHibernate compatibility
+            #pragma warning disable NPG9001 // Type is for evaluation purposes only and is subject to change or removal in future updates
+            NpgsqlConnection.GlobalTypeMapper.AddTypeInfoResolverFactory(new LegacyDateAndTimeResolverFactory());
+            #pragma warning restore NPG9001
+            #pragma warning restore CS0618
+
         }
     }
 }
