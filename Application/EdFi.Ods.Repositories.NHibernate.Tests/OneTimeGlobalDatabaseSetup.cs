643c9a8ed618d0893ac8f9dc927f2e28d4d7de1c
m.semalaiappan <m.semalaiappan@gmail.com>
2026-03-05 17:40:57 -0600

    Duplicate LegacyDateAndTimeResolverFactory file Removed

--- Application/EdFi.Ods.Repositories.NHibernate.Tests/OneTimeGlobalDatabaseSetup.cs
+++ Application/EdFi.Ods.Repositories.NHibernate.Tests/OneTimeGlobalDatabaseSetup.cs
@@ -3,7 +3,7 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-using EdFi.Ods.Common.Infrastructure.Compatibility;
+using EdFi.Ods.Common.Infrastructure.Configuration;
 using Npgsql;
 using NUnit.Framework;
 using Test.Common;
